20abc29f3f0dc5197e4e6231e714e59bb52a3dc4
unknown <nicolas.bessi@camptocamp.com>
2012-04-02 08:59:30 +0200

    [MRG] from trunk (lp:c2c-addons/6.1  rev 28.1.7)

--- delivery_carrier_file_laposte/generator/laposte_generator.py
+++ delivery_carrier_file_laposte/generator/laposte_generator.py
@@ -1,29 +1,21 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2012 Camptocamp SA (http://www.camptocamp.com)
-#   @author Guewen Baconnier
+#    Author: Guewen Baconnier
+#    Copyright 2012 Camptocamp SA
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
@@ -47,7 +39,7 @@ class LaPosteLine(BaseLine):
               'city',
               'country_code',
               'phone',
-              '',
+              'mail',
               '',
               '',
               '',
@@ -86,12 +78,13 @@ class LaPosteFileGenerator(CarrierFileGenerator):
             # if a company, put company name
             if picking.address_id.partner_id.title:
                 line.company_name = address.partner_id.name
-            line.street1 = address.street or ''
-            line.street2 = address.street2 or ''
-            line.zip = address.zip or ''
-            line.city = address.city or ''
-            line.country = address.country_id.code or ''
-            line.phone = address.phone or picking.address_id.mobile or ''
+            line.street1 = address.street
+            line.street2 = address.street2
+            line.zip = address.zip
+            line.city = address.city
+            line.country = address.country_id.code
+            line.phone = address.phone or picking.address_id.mobile
+            line.mail = address.email
         line.weight = "%.2f" % (picking.weight,)
         return [line.get_fields()]
 
