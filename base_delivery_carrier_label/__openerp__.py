10905c75aa8e092ae49ae89c8dbb09dc962a58d9
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-11-26 17:51:04 +0100

    [ADD] base_delivery_carrier_label portage of Akretion delivery_shipping_label

---
+++ base_delivery_carrier_label/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Base module for carrier labels',
+ 'version': '1.0',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'contributors': ['Yannick Vaucher <yannick.vaucher@camptocamp.com'],
+ 'category': 'version',
+ 'complexity': 'normal',
+ 'depends': ['stock', 'delivery'],
+ 'description': """
+Base module for carrier labels
+==============================
+
+This module adds a button on delivery order to generate a label as an attachement.
+
+.. tip::
+   It doesn't implement a label. To add a default label, you can install
+   module `delivery_carrier_label_default_webkit`
+
+It can be used print specific labels per carrier.
+
+.. note::
+   Inspired by Akretion module delivery_base and delivery_shipping_label
+ """,
+ 'website': 'http://www.camptocamp.com/',
+ 'data': [
+     'delivery_view.xml',
+     'stock_view.xml',
+     ],
+ 'tests': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'license': 'AGPL-3',
+ 'application': True}
