fe85fa6bcbf45a645a8b96ae2d757627fac938af
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-12-17 08:37:04 +0100

    [IMP] add tracking_id on shipping label to be able to create a label per pack (stock.tracking)
    [IMP] set tracking_id as an optional key of dict returned by label generation and improve doc string
    
    [IMP] rename generate_pack_labels in generate_shipping_labels as it can also generate a single label for a picking without pack notion
    
    [IMP] set labels on stock.picking instead of stock.picking.out in order to let the creation of return labels to provide to the customer
    
    [FIX] xml view and copy what is needed on stock.picking.out and stock.picking.in as it won't be inherited from stock.picking.out

--- base_delivery_carrier_label/stock.py
+++ base_delivery_carrier_label/stock.py
@@ -26,17 +26,7 @@ from openerp.tools.translate import _
 
 
 class stock_picking(orm.Model):
-    """ Define m2m field due to inheritance to have it in stock.picking.out """
     _inherit = 'stock.picking'
-
-    _columns = {
-        'option_ids': fields.many2many('delivery.carrier.option',
-                                       string='Options'),
-    }
-
-
-class stock_picking_out(orm.Model):
-    _inherit = 'stock.picking.out'
 
     def _get_carrier_type_selection(self, cr, uid, context=None):
         carrier_obj = self.pool.get('delivery.carrier')
@@ -71,13 +61,24 @@ class stock_picking_out(orm.Model):
         """
         return NotImplementedError
 
-    def generate_single_label(self, cr, uid, ids, context=None):
-        """Generate a the single label by default
+    def generate_shipping_labels(self, cr, uid, ids, context=None):
+        """Generate a shipping label by default
 
-        :return: (file_binary, file_type)
+        This method can be inherited to create specific shipping labels
+        a list of label must be return as we can have multiple
+        stock.tracking for a single picking representing packs
+
+        :return: list of dict containing
+           name: name to give to the attachement
+           file: file as string
+           file_type: string of file type like 'PDF'
+           (optional)
+           tracking_id: tracking_id if picking lines have tracking_id and
+                        if label generator creates shipping label per
+                        pack
 
         """
-        return self.generate_default_label(cr, uid, ids, context=None)
+        return [self.generate_default_label(cr, uid, ids, context=None)]
 
     def action_generate_carrier_label(self, cr, uid, ids, context=None):
         shipping_label_obj = self.pool.get('shipping.label')
@@ -85,20 +86,29 @@ class stock_picking_out(orm.Model):
         pickings = self.browse(cr, uid, ids, context=context)
 
         for pick in pickings:
-            label, file_type = pick.generate_single_label()
-            data = {
-                'name': pick.name,
-                'res_id': pick.id,
-                'res_model': 'stock.picking.out',
-                'datas': label.encode('base64'),
-                'file_type': file_type,
-            }
-            context_attachment = context.copy()
-            # remove default_type setted for stock_picking
-            # as it would try to define default value of attachement
-            if 'default_type' in context_attachment:
-                del context_attachment['default_type']
-            shipping_label_obj.create(cr, uid, data, context=context_attachment)
+            shipping_labels = pick.generate_shipping_labels()
+            for label in shipping_labels:
+                # map types with models
+                types = {'in': 'stock.picking.in',
+                         'out': 'stock.picking.out',
+                         'internal': 'stock.picking',
+                         }
+                res_model = types[pick.type]
+                data = {
+                    'name': label['name'],
+                    'res_id': pick.id,
+                    'res_model': res_model,
+                    'datas': label['file'].encode('base64'),
+                    'file_type': label['file_type'],
+                }
+                if label.get('tracking_id'):
+                    data['tracking_id'] = label['tracking_id']
+                context_attachment = context.copy()
+                # remove default_type setted for stock_picking
+                # as it would try to define default value of attachement
+                if 'default_type' in context_attachment:
+                    del context_attachment['default_type']
+                shipping_label_obj.create(cr, uid, data, context=context_attachment)
         return True
 
     def carrier_id_change(self, cr, uid, ids, carrier_id, context=None):
@@ -150,6 +160,101 @@ class stock_picking_out(orm.Model):
         return res
 
 
+class stock_picking_in(orm.Model):
+    """ Add what isn't inherited from stock.picking """
+    _inherit = 'stock.picking.in'
+
+    def _get_carrier_type_selection(self, cr, uid, context=None):
+        carrier_obj = self.pool.get('delivery.carrier')
+        return carrier_obj._get_carrier_type_selection(cr, uid, context=context)
+
+    _columns = {
+        'carrier_id': fields.many2one(
+            'delivery.carrier', 'Carrier',
+            states={'done': [('readonly', True)]}),
+        'carrier_type': fields.related(
+            'carrier_id', 'type',
+            string='Carrier type',
+            readonly=True,
+            type='selection',
+            selection=_get_carrier_type_selection,
+            help="Carrier type ('group')"),
+        'carrier_code': fields.related(
+            'carrier_id', 'code',
+            string='Delivery Method Code',
+            readonly=True,
+            type='char',
+            help="Delivery Method Code (from carrier)"),
+        'option_ids': fields.many2many('delivery.carrier.option',
+                                       string='Options'),
+    }
+
+    def action_generate_carrier_label(self, cr, uid, ids, context=None):
+        picking_obj = self.pool.get('stock.picking')
+        return picking_obj.action_generate_carrier_label(cr, uid, ids,
+                                                         context=context)
+
+    def carrier_id_change(self, cr, uid, ids, carrier_id, context=None):
+        """ Call stock.picking carrier_id_change """
+        picking_obj = self.pool.get('stock.picking')
+        return picking_obj.carrier_id_change(cr, uid, ids,
+                                             carrier_id, context=context)
+
+    def option_ids_change(self, cr, uid, ids,
+                          option_ids, carrier_id, context=None):
+        """ Call stock.picking option_ids_change """
+        picking_obj = self.pool.get('stock.picking')
+        return picking_obj.option_ids_change(cr, uid, ids,
+                                             option_ids, carrier_id,
+                                             context=context)
+
+
+class stock_picking_out(orm.Model):
+    """ Add what isn't inherited from stock.picking """
+    _inherit = 'stock.picking.out'
+
+    def _get_carrier_type_selection(self, cr, uid, context=None):
+        carrier_obj = self.pool.get('delivery.carrier')
+        return carrier_obj._get_carrier_type_selection(cr, uid, context=context)
+
+    _columns = {
+        'carrier_id': fields.many2one(
+            'delivery.carrier', 'Carrier',
+            states={'done': [('readonly', True)]}),
+        'carrier_type': fields.related(
+            'carrier_id', 'type',
+            string='Carrier type',
+            readonly=True,
+            type='selection',
+            selection=_get_carrier_type_selection,
+            help="Carrier type ('group')"),
+        'carrier_code': fields.related(
+            'carrier_id', 'code',
+            string='Delivery Method Code',
+            readonly=True,
+            type='char',
+            help="Delivery Method Code (from carrier)"),
+        'option_ids': fields.many2many('delivery.carrier.option',
+                                       string='Options'),
+    }
+
+    def action_generate_carrier_label(self, cr, uid, ids, context=None):
+        picking_obj = self.pool.get('stock.picking')
+        return picking_obj.action_generate_carrier_label(cr, uid, ids,
+                                                         context=context)
+
+    def carrier_id_change(self, cr, uid, ids, carrier_id, context=None):
+        """ Inherit this method in your module """
+        picking_obj = self.pool.get('stock.picking')
+        return picking_obj.carrier_id_change(cr, uid, ids, carrier_id, context=context)
+
+    def option_ids_change(self, cr, uid, ids, option_ids, carrier_id, context=None):
+        picking_obj = self.pool.get('stock.picking')
+        return picking_obj.option_ids_change(cr, uid, ids,
+                                             option_ids, carrier_id,
+                                             context=context)
+
+
 class ShippingLabel(orm.Model):
     """ Child class of ir attachment to identify which are labels """
     _inherits = {'ir.attachment': 'attachment_id'}
@@ -160,7 +265,8 @@ class ShippingLabel(orm.Model):
         return [('pdf', 'PDF')]
 
     _columns = {
-        'file_type': fields.selection(_get_file_type_selection, 'File type')
+        'file_type': fields.selection(_get_file_type_selection, 'File type'),
+        'tracking_id': fields.many2one('stock.tracking', 'Pack'),
     }
 
     _defaults = {
