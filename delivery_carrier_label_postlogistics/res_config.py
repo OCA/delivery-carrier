9c5c336f550196fd3a0ec2098dbd7c426606bdf6
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-01-23 11:38:43 +0100

    [ADD] security groups for postlogistics licenses
    [IMP] PEP8

--- delivery_carrier_label_postlogistics/res_config.py
+++ delivery_carrier_label_postlogistics/res_config.py
@@ -66,10 +66,11 @@ class PostlogisticsConfigSettings(orm.TransientModel):
                  "– File size: max. 30 kb\n"
                  "– File format: GIF or PNG\n"
                  "– Colour table: indexed colours, max. 200 colours\n"
-                 "– The logo will be printed rotated counter-clockwise by 90°\n"
-                 "We recommend using a black and white logo for printing in the\n"
-                 "ZPL2 format."
-            ),
+                 "– The logo will be printed rotated counter-clockwise by 90°"
+                 "\n"
+                 "We recommend using a black and white logo for printing in "
+                 " the ZPL2 format."
+        ),
         'office': fields.related(
             'company_id', 'postlogistics_office',
             string='Domicile Post office', type='char',
@@ -143,7 +144,8 @@ class PostlogisticsConfigSettings(orm.TransientModel):
 
         lang = context.get('lang', 'en')
         service_code_list = service_code.split(',')
-        res = web_service.read_delivery_instructions(company, service_code_list, lang)
+        res = web_service.read_delivery_instructions(
+            company, service_code_list, lang)
         if 'errors' in res:
             errors = '\n'.join(res['errors'])
             error_message = ('Could not retrieve Postlogistics delivery instructions:\n%s'
@@ -184,14 +186,15 @@ class PostlogisticsConfigSettings(orm.TransientModel):
                 ], context=context)
 
             if option_ids:
-                carrier_option_obj.write(cr, uid, option_ids, data, context=context)
+                carrier_option_obj.write(cr, uid, option_ids, data,
+                                         context=context)
             else:
                 data.update(code=service_code,
                             postlogistics_type='delivery',
                             partner_id=postlogistics_partner.id)
                 carrier_option_obj.create(cr, uid, data, context=context)
         lang = context.get('lang', 'en')
-        _logger.info("Updated delivery instrutions. [%s]" %(lang))
+        _logger.info("Updated delivery instrutions. [%s]" % (lang))
 
     def _get_additional_services(self, cr, uid, ids, web_service,
                                  company, service_code, context=None):
@@ -200,7 +203,8 @@ class PostlogisticsConfigSettings(orm.TransientModel):
 
         lang = context.get('lang', 'en')
         service_code_list = service_code.split(',')
-        res = web_service.read_additional_services(company, service_code_list, lang)
+        res = web_service.read_additional_services(company, service_code_list,
+                                                   lang)
         if 'errors' in res:
             errors = '\n'.join(res['errors'])
             error_message = 'Could not retrieve Postlogistics base services:\n%s' % (errors,)
@@ -229,7 +233,8 @@ class PostlogisticsConfigSettings(orm.TransientModel):
         carrier_option_obj = self.pool.get('delivery.carrier.template.option')
 
         postlogistics_partner = ir_model_data_obj.get_object(
-            cr, uid, 'delivery_carrier_label_postlogistics', 'postlogistics', context=context)
+            cr, uid, 'delivery_carrier_label_postlogistics', 'postlogistics',
+            context=context)
 
         for service_code, data in additional_services.iteritems():
 
@@ -239,7 +244,8 @@ class PostlogisticsConfigSettings(orm.TransientModel):
                 ], context=context)
 
             if option_ids:
-                carrier_option_obj.write(cr, uid, option_ids, data, context=context)
+                carrier_option_obj.write(cr, uid, option_ids, data,
+                                         context=context)
             else:
                 data.update(code=service_code,
                             postlogistics_type='additional',
@@ -248,9 +254,10 @@ class PostlogisticsConfigSettings(orm.TransientModel):
         lang = context.get('lang', 'en')
         _logger.info("Updated additional services [%s]" % (lang))
 
-    def _update_basic_services(self, cr, uid, ids, web_service, company, group_id, context=None):
-        """
-        Update of basic services
+    def _update_basic_services(self, cr, uid, ids, web_service, company,
+                               group_id, context=None):
+        """ Update of basic services
+
         A basic service can be part only of one service group
         :return: {additional_services: {<service_code>: service_data}
                   delivery_instructions: {<service_code>: service_data}
@@ -287,43 +294,57 @@ class PostlogisticsConfigSettings(orm.TransientModel):
                 ], context=context)
             data = {'name': service.Description}
             if option_ids:
-                carrier_option_obj.write(cr, uid, option_ids, data, context=context)
+                carrier_option_obj.write(cr, uid, option_ids, data,
+                                         context=context)
                 option_id = option_ids[0]
             else:
                 data.update(code=service_code,
                             postlogistics_service_group_id=group_id,
                             partner_id=postlogistics_partner.id,
                             postlogistics_type='basic')
-                option_id = carrier_option_obj.create(cr, uid, data, context=context)
+                option_id = carrier_option_obj.create(cr, uid, data,
+                                                      context=context)
 
             # Get related services
             allowed_services = self._get_additional_services(
-                cr, uid, ids, web_service, company, service_code, context=context)
+                cr, uid, ids, web_service, company, service_code,
+                context=context)
             for key, value in additional_services.iteritems():
                 if key in allowed_services:
-                    additional_services[key]['postlogistics_basic_service_ids'][0][2].append(option_id)
+                    (additional_services[key]
+                                        ['postlogistics_basic_service_ids']
+                                        [0]
+                                        [2]).append(option_id)
                     del allowed_services[key]
             for key, value in allowed_services.iteritems():
+                value['postlogistics_basic_service_ids'] = [
+                    (6, 0, [option_id])]
                 additional_services[key] = value
-                additional_services[key]['postlogistics_basic_service_ids'] = [(6, 0, [option_id])]
 
             allowed_services = self._get_delivery_instructions(
-                cr, uid, ids, web_service, company, service_code, context=context)
+                cr, uid, ids, web_service, company, service_code,
+                context=context)
             for key, value in delivery_instructions.iteritems():
                 if key in allowed_services:
-                    delivery_instructions[key]['postlogistics_basic_service_ids'][0][2].append(option_id)
+                    (delivery_instructions[key]
+                                          ['postlogistics_basic_service_ids']
+                                          [0]
+                                          [2]).append(option_id)
                     del allowed_services[key]
             for key, value in allowed_services.iteritems():
+                value['postlogistics_basic_service_ids'] = [
+                    (6, 0, [option_id])]
                 delivery_instructions[key] = value
-                delivery_instructions[key]['postlogistics_basic_service_ids'] = [(6, 0, [option_id])]
 
         _logger.info("Updated '%s' basic service [%s]." % (group.name, lang))
         return {'additional_services': additional_services,
                 'delivery_instructions': delivery_instructions}
 
-    def _update_service_groups(self, cr, uid, ids, web_service, company, context=None):
-        """
-        Also updates additional services and delivery instructions as they are shared between groups
+    def _update_service_groups(self, cr, uid, ids, web_service, company,
+                               context=None):
+        """ Also updates additional services and delivery instructions
+        as they are shared between groups
+
         """
         if context is None:
             context = {}
@@ -347,29 +368,40 @@ class PostlogisticsConfigSettings(orm.TransientModel):
                 cr, uid, [('group_extid', '=', group_extid)], context=context)
             data = {'name': group.Description}
             if group_ids:
-                service_group_obj.write(cr, uid, group_ids, data, context=context)
+                service_group_obj.write(cr, uid, group_ids, data,
+                                        context=context)
                 group_id = group_ids[0]
             else:
                 data['group_extid'] = group_extid
-                group_id = service_group_obj.create(cr, uid, data, context=context)
+                group_id = service_group_obj.create(cr, uid, data,
+                                                    context=context)
 
             # Get related services for all basic services of this group
             res = self._update_basic_services(cr, uid, ids, web_service,
-                                              company, group_id, context=context)
+                                              company, group_id,
+                                              context=context)
 
             allowed_services = res.get('additional_services', {})
             for key, value in additional_services.iteritems():
                 if key in allowed_services:
-                    option_ids = allowed_services[key]['postlogistics_basic_service_ids'][0][2]
-                    additional_services[key]['postlogistics_basic_service_ids'][0][2].extend(option_ids)
+                    a = allowed_services[key]
+                    option_ids = a['postlogistics_basic_service_ids'][0][2]
+                    (additional_services[key]
+                                        ['postlogistics_basic_service_ids']
+                                        [0]
+                                        [2]).extend(option_ids)
                     del allowed_services[key]
             additional_services.update(allowed_services)
 
             allowed_services = res.get('delivery_instructions', {})
             for key, value in delivery_instructions.iteritems():
                 if key in allowed_services:
-                    option_ids = allowed_services[key]['postlogistics_basic_service_ids'][0][2]
-                    delivery_instructions[key]['postlogistics_basic_service_ids'][0][2].extend(option_ids)
+                    a = allowed_services[key]
+                    option_ids = a['postlogistics_basic_service_ids'][0][2]
+                    (delivery_instructions[key]
+                                          ['postlogistics_basic_service_ids']
+                                          [0]
+                                          [2]).extend(option_ids)
                     del allowed_services[key]
             delivery_instructions.update(allowed_services)
 
@@ -377,7 +409,8 @@ class PostlogisticsConfigSettings(orm.TransientModel):
         self._update_additional_services(cr, uid, ids, web_service,
                                          additional_services, context=context)
         self._update_delivery_instructions(cr, uid, ids, web_service,
-                                           delivery_instructions, context=context)
+                                           delivery_instructions,
+                                           context=context)
 
     def update_postlogistics_options(self, cr, uid, ids, context=None):
         """
@@ -388,7 +421,6 @@ class PostlogisticsConfigSettings(orm.TransientModel):
         """
         if context is None:
             context = {}
-        user_obj = self.pool.get('res.users')
         for config in self.browse(cr, uid, ids, context=context):
             company = config.company_id
             web_service = PostlogisticsWebService(company)
@@ -396,12 +428,14 @@ class PostlogisticsConfigSettings(orm.TransientModel):
             # make sure we create source text in en_US
             ctx = context.copy()
             ctx['lang'] = 'en_US'
-            self._update_service_groups(cr, uid, ids, web_service, company, context=ctx)
+            self._update_service_groups(cr, uid, ids, web_service, company,
+                                        context=ctx)
 
             language_obj = self.pool.get('res.lang')
             language_ids = language_obj.search(cr, uid, [], context=context)
 
-            languages = language_obj.browse(cr, uid, language_ids, context=context)
+            languages = language_obj.browse(cr, uid, language_ids,
+                                            context=context)
 
             # handle translations
             # we call the same methode with a different language context
@@ -410,11 +444,12 @@ class PostlogisticsConfigSettings(orm.TransientModel):
                 ctx = context.copy()
                 ctx['lang'] = lang_br.code
                 postlogistics_lang = web_service._get_language(lang)
-                # add translations only for languages that exists on postlogistics
-                # english source will be kept for other languages
+                # add translations only for languages that exists on
+                # postlogistics english source will be kept for other languages
                 if postlogistics_lang == 'en':
                     continue
-                self._update_service_groups(cr, uid, ids, web_service, company, context=ctx)
+                self._update_service_groups(cr, uid, ids, web_service, company,
+                                            context=ctx)
         return True
 
     def _get_allowed_service_group_codes(self, web_service, company,
