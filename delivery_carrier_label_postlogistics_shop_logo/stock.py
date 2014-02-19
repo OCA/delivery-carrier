9c299b96d637ea041d265139fe8f318cc92a6654
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-02-19 14:52:56 +0100

    [ADD] an optional parameter 'tracking_ids' so we can decide to generate the label only for a selection of packs in a picking

--- delivery_carrier_label_postlogistics_shop_logo/stock.py
+++ delivery_carrier_label_postlogistics_shop_logo/stock.py
@@ -27,11 +27,14 @@ class stock_picking(orm.Model):
     _inherit = 'stock.picking'
 
     def _generate_postlogistics_label(self, cr, uid, picking,
-                                      webservice_class=None, context=None):
+                                      webservice_class=None,
+                                      tracking_ids=None,
+                                      context=None):
         """ Generate post label using shop label """
         if webservice_class is None:
             webservice_class = PostlogisticsWebServiceShop
         return super(stock_picking, self)._generate_postlogistics_label(
             cr, uid, picking,
             webservice_class=webservice_class,
+            tracking_ids=tracking_ids,
             context=context)
