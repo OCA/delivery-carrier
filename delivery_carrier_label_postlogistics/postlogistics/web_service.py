f1a85decc0e9752db35016b69f34c7406b83b561
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-02-25 15:37:59 +0100

    [FIX] make sure pack number (which can be picking name) has no unallowed character for item id of postlogistics label

--- delivery_carrier_label_postlogistics/postlogistics/web_service.py
+++ delivery_carrier_label_postlogistics/postlogistics/web_service.py
@@ -249,6 +249,8 @@ class PostlogisticsWebService(object):
 
         """
         name = _compile_itemid.sub('', picking.name)
+        if pack_no:
+            pack_no = _compile_itemid.sub('', pack_no)
         codes = [name, pack_no]
         return "+".join(c for c in codes if c)
 
