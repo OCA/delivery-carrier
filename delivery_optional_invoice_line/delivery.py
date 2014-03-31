94df7a745d68c5a7d653202535621bf05afbb6e3
Alex Comba <alex.comba@agilebg.com>
2014-03-31 16:09:29 +0200

    [ADD] module delivery_optional_invoice_line

---
+++ delivery_optional_invoice_line/delivery.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Agile Business Group sagl
+#    (<http://www.agilebg.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class delivery_carrier(orm.Model):
+    _inherit = 'delivery.carrier'
+
+    _columns = {
+        'do_not_create_invoice_line': fields.boolean(
+            'Do not create invoice line'),
+    }
+
+
+class stock_picking(orm.Model):
+    _inherit = "stock.picking"
+
+    def _prepare_shipping_invoice_line(
+        self, cr, uid, picking, invoice, context=None
+    ):
+        res = {}
+        if picking.carrier_id:
+            carrier_obj = self.pool.get('delivery.carrier')
+            carrier = carrier_obj.browse(
+                cr, uid, picking.carrier_id.id, context=context)
+            if carrier.do_not_create_invoice_line:
+                res = None
+            else:
+                res = super(
+                    stock_picking, self)._prepare_shipping_invoice_line(
+                    cr, uid, picking, invoice, context=context
+                )
+        else:
+            res = super(stock_picking, self)._prepare_shipping_invoice_line(
+                cr, uid, picking, invoice, context=context
+            )
+
+        return res
