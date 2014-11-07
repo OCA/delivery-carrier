bb5cb33faa477e9ee7296ac127b20c979ef95385
david.beal@akretion.com <david.beal@akretion.com>
2014-11-07 08:30:18 +0100

    [FIX] installable to True

--- delivery_carrier_b2c/__openerp__.py
+++ delivery_carrier_b2c/__openerp__.py
@@ -52,7 +52,7 @@ Contributors
         'partner_view.xml',
     ],
     'tests': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'license': 'AGPL-3',
     'application': False,
