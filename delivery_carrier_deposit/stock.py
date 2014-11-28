dcbd5ed2f913496ce6196559d9cde22dceb3709f
David Beal <david.beal@akretion.com>
2014-11-28 15:13:37 +0100

    Update stock.py

--- delivery_carrier_deposit/stock.py
+++ delivery_carrier_deposit/stock.py
@@ -61,7 +61,7 @@ class DepositSlip(orm.Model):
     }
 
     def _get_default_company(self, cr, uid, context=None):
-        company_id = self.pool.get('res.users')._get_company(cr, uid, context=context)
+        company_id = self.pool['res.users']._get_company(cr, uid, context=context)
         if not company_id:
             raise orm.except_orm(
                 _('Error!'),
