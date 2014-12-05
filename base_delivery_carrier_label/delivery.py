3791a1ff1559dcd5dec87e93bac8c5628935c7f4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-12-05 10:43:44 +0100

    Migrate to new API
    Remove an unused file 'reports.xml'
    
    Remove the selection wrappers
    
    Use the name of the method as string instead

--- base_delivery_carrier_label/delivery.py
+++ base_delivery_carrier_label/delivery.py
@@ -3,6 +3,7 @@
 #
 #    Author: Sébastien BEAU <sebastien.beau@akretion.com>
 #    Copyright (C) 2012-TODAY Akretion <http://www.akretion.com>.
+#    Copyright 2014 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,35 +19,26 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 
 
-class DeliveryCarrierTemplateOption(orm.Model):
+class DeliveryCarrierTemplateOption(models.Model):
     """ Available options for a carrier (partner) """
     _name = 'delivery.carrier.template.option'
     _description = 'Delivery carrier template option'
 
-    _columns = {
-        'partner_id': fields.many2one(
-            'res.partner',
-            'Partner Carrier'),
-        'name': fields.char(
-            'Name',
-            readonly=True,
-            size=64),
-        'code': fields.char(
-            'Code',
-            readonly=True,
-            size=64),
-        'description': fields.char(
-            'Description',
-            readonly=True,
-            help="Allow to define a more complete description "
-                 "than in the name field."),
-    }
+    partner_id = fields.Many2one(comodel_name='res.partner',
+                                 string='Partner Carrier')
+    name = fields.Char(readonly=True)
+    code = fields.Char(readonly=True)
+    description = fields.Char(
+        readonly=True,
+        help="Allow to define a more complete description "
+             "than in the name field."
+    )
 
 
-class DeliveryCarrierOption(orm.Model):
+class DeliveryCarrierOption(models.Model):
     """ Option selected for a carrier method
 
     Those options define the list of available pre-added and available
@@ -57,51 +49,58 @@ class DeliveryCarrierOption(orm.Model):
     _description = 'Delivery carrier option'
     _inherits = {'delivery.carrier.template.option': 'tmpl_option_id'}
 
-    _columns = {
-        'mandatory': fields.boolean(
-            'Mandatory',
-            help="If checked, this option is necessarily applied "
-                 "to the delivery order"),
-        'by_default': fields.boolean(
-            'Applied by Default',
-            help="By check, user can choose to apply this option "
-            "to each Delivery Order\n using this delivery method"),
-        'tmpl_option_id': fields.many2one(
-            'delivery.carrier.template.option',
-            string='Option', required=True, ondelete="cascade"),
-        'carrier_id': fields.many2one('delivery.carrier', 'Carrier'),
-        'readonly_flag': fields.boolean(
-            'Readonly Flag',
-            help="When True, help to prevent the user to modify some fields "
-                 "option (if attribute is defined in the view)"),
-    }
+    mandatory = fields.Boolean(
+        help="If checked, this option is necessarily applied "
+             "to the delivery order"
+    )
+    by_default = fields.Boolean(
+        string='Applied by Default',
+        help="By check, user can choose to apply this option "
+             "to each Delivery Order\n using this delivery method"
+    )
+    tmpl_option_id = fields.Many2one(
+        comodel_name='delivery.carrier.template.option',
+        string='Option',
+        required=True,
+        ondelete="cascade",
+    )
+    carrier_id = fields.Many2one(comodel_name='delivery.carrier',
+                                 string='Carrier')
+    readonly_flag = fields.Boolean(
+        string='Readonly Flag',
+        help="When True, help to prevent the user to modify some fields "
+             "option (if attribute is defined in the view)"
+    )
 
-    _defaults = {
-        'readonly_flag': False,
-        'by_default': False,
-    }
 
-
-class DeliveryCarrier(orm.Model):
+class DeliveryCarrier(models.Model):
     _inherit = 'delivery.carrier'
 
-    def _get_carrier_type_selection(self, cr, uid, context=None):
+    @api.model
+    def _get_carrier_type_selection(self):
         """ To inherit to add carrier type """
         return []
 
-    def __get_carrier_type_selection(self, cr, uid, context=None):
-        """ Wrapper to preserve inheritance for selection field """
-        return self._get_carrier_type_selection(cr, uid, context=context)
+    type = fields.Selection(
+        selection='_get_carrier_type_selection',
+        string='Type',
+        help="Carrier type (combines several delivery methods)",
+    )
+    code = fields.Char(
+        help="Delivery Method Code (according to carrier)",
+    )
+    description = fields.Text()
+    available_option_ids = fields.One2many(
+        comodel_name='delivery.carrier.option',
+        inverse_name='carrier_id',
+        string='Option',
+    )
 
-    _columns = {
-        'type': fields.selection(
-            __get_carrier_type_selection, 'Type',
-            help="Carrier type (combines several delivery methods)"),
-        'code': fields.char(
-            'Code', size=10,
-            help="Delivery Method Code (according to carrier)"),
-        'description': fields.text('Description'),
-        'available_option_ids': fields.one2many(
-            'delivery.carrier.option',
-            'carrier_id', 'Option'),
-    }
+    @api.multi
+    def default_options(self):
+        """ Returns default and available options for a carrier """
+        options = self.env['delivery.carrier.option'].browse()
+        for available_option in self.available_option_ids:
+            if (available_option.mandatory or available_option.by_default):
+                options |= available_option
+        return options
