1ed60d6c4abd21bc5fe7d372c6330e1042f3cf40
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-13 16:29:31 +0200

    [MOV] move addons out of __unported__ (they remain not installable)

---
+++ base_delivery_carrier_files/csv_writer/unicode_writer.py
@@ -0,0 +1,41 @@
+# CSV writer from python doc
+import csv
+import codecs
+
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+
+
+class UnicodeWriter(object):
+
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = StringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        # we ensure that we do not try to encode none or bool
+        row = [x or u'' for x in row]
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
---
+++ base_delivery_carrier_files/wizard/generate_carrier_files.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2012 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+from tools.translate import _
+
+
+class DeliveryCarrierFileGenerate(orm.TransientModel):
+
+    _name = 'delivery.carrier.file.generate'
+
+    def _get_picking_ids(self, cr, uid, context=None):
+        if context is None:
+            context = {}
+        res = False
+        if (context.get('active_model', False) == 'stock.picking.out' and
+                context.get('active_ids', False)):
+            res = context['active_ids']
+        return res
+
+    _columns = {
+        'picking_ids': fields.many2many('stock.picking.out',
+                                        string='Delivery Orders'),
+        'recreate': fields.boolean(
+            'Recreate files',
+            help="If this option is used, new files will be generated "
+                 "for selected picking even if they already had one.\n"
+                 "By default, delivery orders with existing file will be "
+                 "skipped."),
+    }
+
+    _defaults = {
+        'picking_ids': _get_picking_ids,
+    }
+
+    def action_generate(self, cr, uid, ids, context=None):
+        """
+        Call the creation of the delivery carrier files
+        """
+        context = context or {}
+        form = self.browse(cr, uid, ids, context=context)[0]
+        if not form.picking_ids:
+            raise orm.except_orm(_('Error'), _('No delivery orders selected'))
+
+        picking_obj = self.pool['stock.picking']
+        picking_ids = [picking.id for picking in form.picking_ids]
+        picking_obj.generate_carrier_files(cr, uid,
+                                           picking_ids,
+                                           auto=False,
+                                           recreate=form.recreate,
+                                           context=context)
+
+        return {'type': 'ir.actions.act_window_close'}
