b9d7666e109d7923add3d2e94756396e21933402
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-10-15 14:33:35 +0200

    [PORT] base_delivery_carrier_label port to 9.0 - Reactivate - Move files in views and models - Adapt XML views - Update README.rst
    Move settings menu in Inventory/Configuration
    
    Remove test with Logistic units as it was removed from 9.0
    
    Allow to give a filename to the created label if not defined use the attachment name
    
    Fix weight calcul adding a field for total weight
    This due to removed logistics units
    If set this field will be used to define weight, otherwise we compute
    weight even before quants have been created.
    
    Fix stock picking view some labels were missing and the view was scrambled
    
    Split files by model

---
+++ base_delivery_carrier_label/models/delivery_carrier.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 Akretion <http://www.akretion.com>.
+# Copyright 2013-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import api, fields, models
+
+
+class DeliveryCarrier(models.Model):
+    _inherit = 'delivery.carrier'
+
+    @api.model
+    def _get_carrier_type_selection(self):
+        """ To inherit to add carrier type """
+        return []
+
+    type = fields.Selection(
+        selection='_get_carrier_type_selection',
+        string='Type',
+        help="Carrier type (combines several delivery methods)",
+    )
+    code = fields.Char(
+        help="Delivery Method Code (according to carrier)",
+    )
+    description = fields.Text()
+    available_option_ids = fields.One2many(
+        comodel_name='delivery.carrier.option',
+        inverse_name='carrier_id',
+        string='Option',
+    )
+
+    @api.multi
+    def default_options(self):
+        """ Returns default and available options for a carrier """
+        options = self.env['delivery.carrier.option'].browse()
+        for available_option in self.available_option_ids:
+            if (available_option.mandatory or available_option.by_default):
+                options |= available_option
+        return options
---
+++ base_delivery_carrier_label/models/delivery_carrier_template_option.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 Akretion <http://www.akretion.com>.
+# Copyright 2013-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import fields, models
+
+
+class DeliveryCarrierTemplateOption(models.Model):
+    """ Available options for a carrier (partner) """
+    _name = 'delivery.carrier.template.option'
+    _description = 'Delivery carrier template option'
+
+    partner_id = fields.Many2one(comodel_name='res.partner',
+                                 string='Partner Carrier')
+    name = fields.Char(readonly=True)
+    code = fields.Char(readonly=True)
+    description = fields.Char(
+        readonly=True,
+        help="Allow to define a more complete description "
+             "than in the name field."
+    )
