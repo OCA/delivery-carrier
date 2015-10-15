2ad989f72773eb661085d0ce0296465d2890dae0
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-10-15 14:33:35 +0200

    [PORT] base_delivery_carrier_label port to 9.0 - Reactivate - Move files in views and models - Adapt XML views - Update README.rst
    Move settings menu in Inventory/Configuration
    
    Remove test with Logistic units as it was removed from 9.0
    
    Allow to give a filename to the created label if not defined use the attachment name
    
    Fix weight calcul adding a field for total weight
    This due to removed logistics units
    If set this field will be used to define weight, otherwise we compute
    weight even before quants have been created.
    
    Fix stock picking view some labels were missing and the view was scrambled
    
    Split files by model

---
+++ base_delivery_carrier_label/models/stock_pack_operation.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Hpar
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import api, fields, models
+import openerp.addons.decimal_precision as dp
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class StockPackOperation(models.Model):
+    _inherit = 'stock.pack.operation'
+
+    weight = fields.Float(
+        digits=dp.get_precision('Stock Weight'),
+        help="Weight of the pack_operation"
+    )
+
+    @api.multi
+    def get_weight(self):
+        """Calc and save weight of pack.operations.
+
+        Warning: Type conversion not implemented
+                it will return False if at least one uom or uos not in kg
+        return:
+            the sum of the weight of [self]
+        """
+        total_weight = 0
+        kg = self.env.ref('product.product_uom_kgm').id
+        units = self.env.ref('product.product_uom_unit').id
+        allowed = (False, kg, units)
+        cant_calc_total = False
+        for operation in self:
+            product = operation.product_id
+
+            # if not defined we assume it's in kg
+            if product.uom_id.id not in allowed:
+                _logger.warning(
+                    'Type conversion not implemented for product %s' %
+                    product.id)
+                cant_calc_total = True
+
+            operation.weight = (product.weight * operation.product_qty)
+
+            total_weight += operation.weight
+
+        if cant_calc_total:
+            return False
+        return total_weight
