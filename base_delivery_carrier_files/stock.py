e86b680921ee18ce8a28bb275e8ecf8b66df3a25
Kalantojus Karolis <karolis.kalantojus@gmail.com>
2015-11-11 07:19:30 +0200

    Port base_delivery_carrier_files_migration

--- base_delivery_carrier_files/stock.py
+++ base_delivery_carrier_files/stock.py
@@ -19,22 +19,15 @@
 #
 ##############################################################################
 
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 
 
-class stock_picking(orm.Model):
-    _inherit = 'stock.picking'
+class stock_picking(models.Model):
+    _inherit = "stock.picking"
 
-    _columns = {
-        'carrier_file_generated': fields.boolean('Carrier File Generated',
-                                                 readonly=True,
-                                                 help="The file for the "
-                                                      "delivery carrier "
-                                                      "has been generated."),
-    }
-
-    def generate_carrier_files(self, cr, uid, ids, auto=True,
-                               recreate=False, context=None):
+    @api.multi
+    def generate_carrier_files(self, auto=True,
+                               recreate=False):
         """
         Generates all the files for a list of pickings according to
         their configuration carrier file.
@@ -50,50 +43,52 @@ class stock_picking(orm.Model):
                      are exported
         :return: True if successful
         """
-        carrier_file_obj = self.pool.get('delivery.carrier.file')
         carrier_file_ids = {}
-        for picking in self.browse(cr, uid, ids, context):
-            if picking.type != 'out':
+        for picking in self:
+            if picking.picking_type_id.code != 'outgoing':
                 continue
             if not recreate and picking.carrier_file_generated:
                 continue
             carrier = picking.carrier_id
-            if not carrier or not carrier.carrier_file_id:
+            if not carrier:
+                continue
+            if not carrier.carrier_file_id:
                 continue
             if auto and not carrier.carrier_file_id.auto_export:
                 continue
             p_carrier_file_id = picking.carrier_id.carrier_file_id.id
-            carrier_file_ids.setdefault(p_carrier_file_id, []).\
-                append(picking.id)
+            carrier_file_ids.setdefault(p_carrier_file_id, []).append(
+                picking.id)
 
-        for carrier_file_id, carrier_picking_ids\
-                in carrier_file_ids.iteritems():
-            carrier_file_obj.generate_files(cr, uid, carrier_file_id,
-                                            carrier_picking_ids,
-                                            context=context)
+        carrier_files = self.env["delivery.carrier.file"].browse(
+            carrier_file_ids.keys())
+        for carrier_file in carrier_files:
+            carrier_file.generate_files(carrier_file_ids[carrier_file.id])
         return True
 
-    def action_done(self, cr, uid, ids, context=None):
-        result = super(stock_picking, self).action_done(cr, uid, ids,
-                                                        context=context)
-        self.generate_carrier_files(cr, uid, ids, auto=True, context=context)
+    @api.multi
+    def action_done(self):
+        result = super(stock_picking, self).action_done()
+        self.generate_carrier_files(auto=True)
         return result
 
+    carrier_file_generated = fields.Boolean(
+        'Carrier File Generated', readonly=True, copy=False,
+        help="The file for the delivery carrier has been generated.")
 
-class stock_picking_out(orm.Model):
-    _inherit = 'stock.picking.out'
 
-    _columns = {
-        'carrier_file_generated': fields.boolean('Carrier File Generated',
-                                                 readonly=True,
-                                                 help="The file for "
-                                                 "the delivery carrier "
-                                                 "has been generated."),
-    }
+class stock_move(models.Model):
+    _inherit = 'stock.move'
 
-    def copy(self, cr, uid, rec_id, default=None, context=None):
-        if default is None:
-            default = {}
-        default.update({'carrier_file_generated': False})
-        return super(stock_picking_out, self).copy(cr, uid, rec_id, default,
-                                                   context=context)
+    @api.multi
+    def write(self, values):
+        write_result = super(stock_move, self).write(values)
+        if values.get('state') and values['state'] == 'done':
+            picking_ids = map(lambda p: p.id, self.mapped('picking_id'))
+            done_pickings = self.env['stock.picking'].search([
+                ('id', 'in', picking_ids),
+                ('state', '=', 'done')
+            ])
+            if done_pickings:
+                done_pickings.generate_carrier_files()
+        return write_result
