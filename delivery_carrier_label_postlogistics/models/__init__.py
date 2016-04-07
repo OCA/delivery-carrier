1c8594434a41b7153b6601c4792b6bafb8703c87
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-04-07 14:51:18 +0200

    Use short license header

--- delivery_carrier_label_postlogistics/models/__init__.py
+++ delivery_carrier_label_postlogistics/models/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2013-2015 Yannick Vaucher (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import company
 from . import res_config
 from . import delivery
