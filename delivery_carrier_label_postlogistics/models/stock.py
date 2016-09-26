011eb7ede9c85ada6d98dac46e4a6e123d9ba5e0
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-09-26 16:18:04 +0200

    Rename field type to carrier_type in delivery.carrier
    Missing acl for stock user/manager

--- delivery_carrier_label_postlogistics/models/stock.py
+++ delivery_carrier_label_postlogistics/models/stock.py
@@ -105,7 +105,7 @@ class StockPicking(models.Model):
     def generate_shipping_labels(self, package_ids=None):
         """ Add label generation for Postlogistics """
         self.ensure_one()
-        if self.carrier_id.type == 'postlogistics':
+        if self.carrier_id.carrier_type == 'postlogistics':
             return self._generate_postlogistics_label(package_ids=package_ids)
         _super = super(StockPicking, self)
         return _super.generate_shipping_labels(package_ids=package_ids)
