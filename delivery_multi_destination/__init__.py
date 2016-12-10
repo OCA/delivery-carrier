f6c8d1f52b07ccdeeb29b8762527ef4ef213299e
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-10 22:45:36 +0100

    [ADD] delivery_multi_destination
    ==================================================
    Multiple destinations for the same delivery method
    ==================================================
    
    Module `delivery` in version 8 allows to set different price rules depending
    on the destination. This is what is called a delivery grid.
    
    In version 9, for simplifying delivery methods, Odoo has plained the structure,
    lowering destinations at delivery method level, and removing delivery grid
    model.
    
    This is not usable when you have different prices according the destination
    of your delivery.
    
    This module restores the same concept, reusing the same model for nesting
    several "children" delivery methods, one per possible destination. It has been
    designed to reuse all possible extensions to the base delivery, without the
    need to create a glue module for having multiple destinations.
    
    This module also handles if you're migrating from version 8 and you had
    `delivery` module installed, to keep the delivery grids.
    
    Installation
    ============
    
    If you installed the module on a version 8 migrated database, some operations
    will be done for recovering delivery grids. If so, you need to have
    **openupgradelib** library installed.
    
    Configuration
    =============
    
    To configure delivery methods with multiple destinations:
    
    * Go to Inventory > Configuration > Delivery > Delivery Methods
    * Create or edit an existing record.
    * Select "Destination type" = "Multiple destinations".
    * Introduce a line for each destination in the new tab "Destinations"
    * Lines have priority, so you have to put first the lines with more restricted
      destinations.
    
    Usage
    =====
    
    * When using the delivery method in a Sales order, delivery address will be
      used for computing the delivery price according introduced destinations.

---
+++ delivery_multi_destination/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+from .hooks import post_init_hook
+from .hooks import uninstall_hook
