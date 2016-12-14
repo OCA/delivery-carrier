3398e7a356e96ef5b6fa213b7703504f880a0e46
Raph <raph@poir.es>
2016-12-14 13:48:09 +0100

    Remove trailing whitespace

--- delivery_roulier/models/stock_transfert.py
+++ delivery_roulier/models/stock_transfert.py
@@ -1,18 +1,18 @@
 #  coding: utf-8
 #  © 2016 David BEAL @ Akretion <david.beal@akretion.com>
 #  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# 
+#
 # import logging
-# 
+#
 # from openerp import _, api, models
 # from openerp.exceptions import Warning as UserError
-# 
+#
 # _logger = logging.getLogger(__name__)
-# 
-# 
+#
+#
 # class StockTransferDetails(models.TransientModel):
 #     _inherit = 'stock.transfer_details'
-# 
+#
 #     @api.multi
 #     def do_detailed_transfer(self):
 #         """ All carriers using roulier needs package
@@ -28,4 +28,4 @@
 #                           "must be put in a parcel.")
 #                         % rec.picking_id.carrier_id.name)
 #             return super(StockTransferDetails, self).do_detailed_transfer()
-# 
+#
