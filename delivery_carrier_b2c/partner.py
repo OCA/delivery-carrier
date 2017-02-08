3353810f89a0cde06e440f6eab49c355553a0196
MonsieurB <contact@ebii.fr>
2017-02-08 13:35:14 +0100

    migrate module to V10

--- delivery_carrier_b2c/partner.py
+++ delivery_carrier_b2c/partner.py
@@ -1,44 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
 #    Author: Chafique DELLI <chafique.delli@akretion.com>
-#    Copyright 2014 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import orm, fields
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 
-class res_partner(orm.Model):
+from odoo import models, fields
+
+
+class res_partner(models.Model):
     _inherit = "res.partner"
 
-    _columns = {
-        'use_b2c_info': fields.boolean(
+    use_b2c_info= fields.Boolean(
             'Advanced address',
-            help="Display additional information for home delivery (b2c)"),
-        'door_code': fields.char(
-            'Door Code'),
-        'door_code2': fields.char(
-            'Door Code 2',),
-        'intercom': fields.char(
-            'Intercom',
-            help="Informations for Intercom such as name "
-                 "or number on the intercom"),
-    }
-
-    _defaults = {
-        'use_b2c_info': False,
-    }
+            help="Display additional information for home delivery (b2c)")
+    door_code= fields.Char('Door Code')
+    door_code2= fields.Char('Door Code 2')
+    intercom= fields.Char('Intercom',
+                        help="Informations for Intercom such as name "
+                        "or number on the intercom")
