b04e7ba6692b4b6d56b54f5b91722e6e4a8b8f99
MonsieurB <eric@bouhana.com>
2017-03-06 11:08:00 +0100

    [10] delivery  carrier b2c (#116)
    migrate module to V10

---
+++ delivery_carrier_b2c/README.rst
@@ -0,0 +1,49 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=====================
+Delivery Carrier B2C
+=====================
+
+* This module adds specific fields to partner for delivery purposes. Some carrier services offer to deliver packages to any place in a country (e.g. 'So Colissimo' in France). In this case addition fields are required with a more complete view on partner
+
+.. figure:: path/to/local/image.png
+   :alt: alternative description
+   :width: 600 px
+
+
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/delivery_carrier_b2c/{branch}
+
+
+
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
+
+Contributors
+------------
+* Chafique DELLI <chafique.delli@akretion.com>
+* David BEAL <david.beal@akretion.com>
+* Monsieur B <monsieurb@saaslys.com>
+----
+
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
--- delivery_carrier_b2c/__manifest__.py
+++ delivery_carrier_b2c/__manifest__.py
@@ -1,58 +1,22 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
 #    Author: Chafique DELLI <chafique.delli@akretion.com>
-#    Copyright 2014 Akretion
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Delivery Carrier Business To Customer',
-    'version': '8.0.0.2.0',
+    'version': '10.0.0.1.0',
     'author': "Akretion,Odoo Community Association (OCA)",
     'maintainer': 'Akretion',
     'category': '',
     'depends': [
-        'base_delivery_carrier_label',
-        'partner_helper',
     ],
-    'description': """
-Delivery Carrier Business To Customer
-=====================================
 
-Description
------------
-* Add specific fields to partner for delivery purpose
-
-
-Contributors
-------------
-* Chafique DELLI <chafique.delli@akretion.com>
-* David BEAL <david.beal@akretion.com>
-
-----
-
-
-    """,
     'website': 'http://www.akretion.com/',
     'data': [
         'partner_view.xml',
     ],
     'tests': [],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'license': 'AGPL-3',
     'application': False,
