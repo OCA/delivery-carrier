8931ccaa709571bd96b68a46629f42bde56193d2
Angel Moya Pardo <amoyapardo@gmail.com>
2017-03-29 19:18:35 +0200

    MIG WIP 10.0 delivery carrier label gls

--- delivery_carrier_label_gls/__init__.py
+++ delivery_carrier_label_gls/__init__.py
@@ -1,26 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) All Rights Reserved 2014 Akretion
-#    @author David BEAL <david.beal@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# © 2013-2015 David BEAL <david.beal@akretion.com>
+# © 2017 Angel Moya <angel.moya@pesol.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import company
-from . import config
-from . import delivery
-from . import report
-from . import stock
+from . import models
--- delivery_carrier_label_gls/__manifest__.py
+++ delivery_carrier_label_gls/__manifest__.py
@@ -23,44 +23,22 @@
 {
     'name': 'Delivery Carrier Label GLS',
     'version': '0.1',
-    'author': "Akretion,Odoo Community Association (OCA)",
+    'author': "Akretion,"
+              "PESOL,"
+              "Odoo Community Association (OCA)",
     'maintener': 'Akretion',
     'category': 'Warehouse',
     'summary': "GLS carrier label printing",
     'depends': [
         'base_delivery_carrier_label',
-        'configuration_helper',
-        'partner_helper',
+        # 'configuration_helper',
+        # 'partner_helper',
     ],
-    'description': """
-Delivery Carrier Label GLS (french carrier)
-=============================================
-
-
-GLS Group - Transportation services
--------------------------------------
-
-
-Implémentation du service Unibox:
-- envoi des informations du bon de livraison et des colis au serveur GLS unibox
-- retour par webservice des informations de routage
-- génération des étiquettes GLS Unibox
-
-
-GLS carrier https://gls-group.eu/
-
-
-Contributors
-------------
-* David BEAL <david.beal@akretion.com>
-
-""",
     'website': 'http://www.akretion.com/',
     'data': [
         'data/delivery_carrier.xml',
         'data/sequence.xml',
-        'config_view.xml',
-        # 'security/ir.model.access.csv',
+        'views/config_view.xml',
     ],
     'demo': [
         'demo/res.partner.csv',
