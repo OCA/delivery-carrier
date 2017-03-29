c58f5d9cde96bd11c591556e7aae48467aa0369c
Angel Moya Pardo <amoyapardo@gmail.com>
2017-03-29 15:10:04 +0200

    WIP migraciont label
    MIG 10.0 base_delivery_carrier_label

--- base_delivery_carrier_label/models/stock_quant_package.py
+++ base_delivery_carrier_label/models/stock_quant_package.py
@@ -2,8 +2,8 @@
 # Copyright 2014-2015 Akretion <http://www.akretion.com>
 # Copyright 2014-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import api, fields, models
-import openerp.addons.decimal_precision as dp
+from odoo import api, fields, models
+import odoo.addons.decimal_precision as dp
 
 
 class StockQuantPackage(models.Model):
