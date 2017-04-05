95ad12ae3addb68d45e1ed6fa65980c78c58c465
Raph <raph@hpar.fr>
2017-04-05 16:59:32 +0200

    Remove laposte demo

--- delivery_roulier/__openerp__.py
+++ delivery_roulier/__openerp__.py
@@ -12,25 +12,20 @@
     'depends': [
         'partner_helper',
         'base_phone',  # from oca/telephony
-        'document',
         'keychain',  # from oca/server-tools
         'base_suspend_security',
         'product_harmonized_system',  # from oca/intrastat
-        'delivery_carrier_b2c',
     ],
     'website': 'http://www.akretion.com/',
     'data': [
         'data/delivery.xml',
         'views/stock_quant_package.xml',
     ],
-    'demo': [
-    ],
     'external_dependencies': {
         'python': [
-            'roulier',  # '>0.2'
+            'roulier',  # '>0.1.4'
         ],
     },
-    'tests': [],
     'installable': True,
     'auto_install': False,
     'license': 'AGPL-3',
