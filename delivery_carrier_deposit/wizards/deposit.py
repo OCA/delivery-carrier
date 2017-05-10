bf1e1b232a7a3a3b95b8c6cb9a81d1ec0b668e75
MonsieurB <contact@ebii.fr>
2017-05-10 13:17:55 +0200

    mig V10

--- delivery_carrier_deposit/wizards/deposit.py
+++ delivery_carrier_deposit/wizards/deposit.py
@@ -1,29 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
 #   Copyright (C) 2012-2014 Akretion France (www.akretion.com)
 #   @author: David BEAL <david.beal@akretion.com>
 #   @author: Sebastien BEAU <sebastien.beau@akretion.com>
 #   @author: Chafique DELLI <chafique.delli@akretion.com>
 #   @author: Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models, api, _
-from openerp.exceptions import Warning
+from odoo import fields, models, api, _
+from odoo.exceptions import Warning
 
 
 class DeliveryDepositWizard(models.TransientModel):
