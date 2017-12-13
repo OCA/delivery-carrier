220ef0e8af9ff62f621cea016be8f4e6786abb91
hpar <raph@hpar.fr>
2017-12-13 17:52:46 +0100

    Port laposte to 10

--- delivery_roulier_option/models/stock_picking.py
+++ delivery_roulier_option/models/stock_picking.py
@@ -2,35 +2,11 @@
 #  @author Raphael Reverdy <raphael.reverdy@akretion.com>
 #          David BEAL <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from functools import wraps
 import logging
-from openerp import models, fields, api
+from odoo import models, fields, api
+from odoo.addons.delivery_roulier import implemented_by_carrier
 
 _logger = logging.getLogger(__name__)
-
-
-def implemented_by_carrier(func):
-    """Decorator: call _carrier_prefixed method instead.
-
-    Usage:
-        @implemented_by_carrier
-        def _do_something()
-        def _laposte_do_something()
-        def _gls_do_something()
-
-    At runtime, picking._do_something() will try to call
-    the carrier spectific method or fallback to generic _do_something
-
-    """
-    @wraps(func)
-    def wrapper(cls, *args, **kwargs):
-        fun_name = func.__name__
-        fun = '_%s%s' % (cls.carrier_type, fun_name)
-        if not hasattr(cls, fun):
-            fun = '_roulier%s' % (fun_name)
-            # return func(cls, *args, **kwargs)
-        return getattr(cls, fun)(*args, **kwargs)
-    return wrapper
 
 
 class StockPicking(models.Model):
