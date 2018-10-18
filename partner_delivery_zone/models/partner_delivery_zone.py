70550dca0fbbfbefd4cba8ef914ff795e8e6132a
Sergio Teruel <sergio.teruel@tecnativa.com>
2018-10-18 09:53:32 +0200

    [11.0][NEW] partner_delivery_zone: New module to group partners, orders and pickings by delivery zones

---
+++ partner_delivery_zone/models/partner_delivery_zone.py
@@ -0,0 +1,13 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class PartnerDeliveryZone(models.Model):
+    _name = 'partner.delivery.zone'
+
+    code = fields.Char()
+    name = fields.Char(
+        string='Zone',
+        required=True,
+    )
