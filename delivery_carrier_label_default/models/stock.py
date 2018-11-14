3e23cd09cae260fc12f526fc182c6ed561c26243
sebalix <seb@usr-src.org>
2018-11-14 17:17:44 +0100

    [MIG] delivery_carrier_label_default: migration to 11.0

--- delivery_carrier_label_default/models/stock.py
+++ delivery_carrier_label_default/models/stock.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2013-2016 Camptocamp SA
+# Copyright 2013-2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from openerp import api, models
+from odoo import api, models
 
 
 class StockPicking(models.Model):
@@ -9,19 +8,12 @@ class StockPicking(models.Model):
 
     @api.multi
     def generate_default_label(self, package_ids=None):
-        """
-        Generate a label from a qweb report
-        """
+        """Generate a label from a qweb report."""
         self.ensure_one()
-        report_obj = self.env['ir.actions.report.xml']
-
-        data = {'ids': self.id}
-        report_obj = self.env['report']
-        pdf_report = report_obj.get_pdf(
-            self, 'delivery_carrier_label_default.report_default_label',
-            data=data)
-
-        return {'name': '%s.pdf' % "Shipping Label",  # report.report_name,
-                'file': pdf_report,
-                'file_type': 'pdf',
-                }
+        report = self.env.ref('delivery_carrier_label_default.default_label')
+        file_, file_type = report.render(res_ids=self.ids)
+        return {
+            'name': '%s.%s' % (report.name, file_type),
+            'file': file_,
+            'file_type': file_type,
+        }
