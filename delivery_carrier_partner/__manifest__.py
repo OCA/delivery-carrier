fed3bc5024fdddea110db630b4af98774d39f075
SimoRubi <simone.rubino@agilebg.com>
2018-12-10 11:47:33 +0100

    [ADD] delivery_carrier: Add transporter field

---
+++ delivery_carrier_partner/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Simone Rubino - Agile Business Group
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery carrier partner",
+    "summary": "Add a partner in the delivery carrier",
+    "version": "10.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Delivery",
+    "website": "https://github.com/OCA/delivery-carrier/tree/"
+               "10.0/delivery_carrier_partner",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "installable": True,
+    "license": "AGPL-3",
+    "depends": [
+        "delivery",
+    ],
+    "data": [
+        "views/delivery_carrier_views.xml"
+    ]
+}
