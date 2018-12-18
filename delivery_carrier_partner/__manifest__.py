e8974af66085110c2b7aa375c1ffbaa95ea993b3
SimoRubi <simone.rubino@agilebg.com>
2018-12-18 12:05:28 +0100

    [MIG] delivery_carrier_partner: Migration to 11.0

--- delivery_carrier_partner/__manifest__.py
+++ delivery_carrier_partner/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Delivery carrier partner",
     "summary": "Add a partner in the delivery carrier",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "development_status": "Beta",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier/tree/"
