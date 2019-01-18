f063ee0c697dbe0361fdf7e9f17ba3eab7f708c0
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-01-18 01:20:55 +0100

    [MIG] delivery_multi_destination: Migration to 11.0
    * Standard procedure
    * README by fragments
    * Code changed to follow v11 logic
    * Tests adapted and expanded

--- delivery_multi_destination/models/delivery_carrier.py
+++ delivery_multi_destination/models/delivery_carrier.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016-2019 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -45,11 +44,29 @@ class DeliveryCarrier(models.Model):
             name=name, args=args, operator=operator, limit=limit,
         )
 
-    @api.multi
-    def verify_carrier(self, contact):
+    def available_carriers(self, partner):
+        """Add childrens on the possible list to select carriers. This is
+        used on `website_sale_delivery` module.
+        """
+        candidates = self.env['delivery.carrier']
+        for carrier in self:
+            if self.destination_type == 'one':
+                candidates |= carrier
+            else:
+                carrier = self.with_context(show_children_carriers=True)
+                candidates |= carrier.child_ids
+        return super(DeliveryCarrier, candidates).available_carriers(partner)
+
+    def rate_shipment(self, order):
+        """We have to override this method for getting the proper price
+        according destination on sales orders.
+        """
         if self.destination_type == 'one':
-            return super(DeliveryCarrier, self).verify_carrier(contact)
-        carrier = self.with_context(show_children_carriers=True)
-        for subcarrier in carrier.child_ids:
-            if super(DeliveryCarrier, subcarrier).verify_carrier(contact):
-                return subcarrier
+            return super().rate_shipment(order)
+        else:
+            carrier = self.with_context(show_children_carriers=True)
+            for subcarrier in carrier.child_ids:
+                if subcarrier._match_address(order.partner_shipping_id):
+                    return super(
+                        DeliveryCarrier, subcarrier,
+                    ).rate_shipment(order)
