0aab9949f4889b784b460cf88828a6e2639cd776
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-01-18 16:39:49 +0100

    [11.0][IMP] partner_delivery_schedule: Display delivery schedules for incoming pickings too.
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: delivery-carrier-11.0/delivery-carrier-11.0-partner_delivery_schedule
    Translate-URL: https://translation.odoo-community.org/projects/delivery-carrier-11-0/delivery-carrier-11-0-partner_delivery_schedule/es/
    
    [UPD] README.rst

--- partner_delivery_schedule/__manifest__.py
+++ partner_delivery_schedule/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Partner Delivery Schedule',
     'summary': 'Set on partners a schedule for delivery goods',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'development_status': 'Beta',
     'category': 'Delivery',
     'website': 'https://github.com/OCA/delivery-carrier',
