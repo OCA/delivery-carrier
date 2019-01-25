be660226d743b06a1b822a206058b6d23d62b998
cubells <vicent@vcubells.net>
2019-01-25 10:29:37 +0100

    [ADD] delivery_free_fee_removal: avoid create shipping lines if price is zero

---
+++ delivery_free_fee_removal/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery Free Fee Removal",
+    "summary": "Remove free fee lines from sales order",
+    "version": "11.0.1.0.0",
+    "category": "Delivery",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "installable": True,
+    "license": "AGPL-3",
+    "depends": [
+        "delivery",
+    ],
+}
