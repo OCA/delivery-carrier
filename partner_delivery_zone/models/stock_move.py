d029c7b9cc30d1886bc06880f7cc811d85fd2694
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-03-13 16:54:13 +0100

    [11.0][FIX] partner_delivery_zone: Get delivery zone from original move linked (pick-pack...) and add test

--- partner_delivery_zone/models/stock_move.py
+++ partner_delivery_zone/models/stock_move.py
@@ -6,10 +6,16 @@ from odoo import models
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
+    def get_original_move(self):
+        if self.move_dest_ids:
+            return self.move_dest_ids.get_original_move()
+        return self
+
     def _get_new_picking_values(self):
         vals = super()._get_new_picking_values()
-        zone_id = self.sale_line_id.order_id.delivery_zone_id.id
-        if not self.sale_line_id:
+        original_move = self.get_original_move()
+        zone_id = original_move.sale_line_id.order_id.delivery_zone_id.id
+        if not zone_id:
             zone_id = self.env['res.partner'].browse(
                 vals.get('partner_id', False)).delivery_zone_id.id
         vals['delivery_zone_id'] = zone_id
