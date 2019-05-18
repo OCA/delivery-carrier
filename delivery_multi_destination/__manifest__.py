56131328dbea3302cd7663613adb5f9fafe90ea9
Luis M. Ontalba <luismaront@gmail.com>
2019-05-18 14:02:04 +0200

    [MIG] delivery_multi_destination: Migration to 12.0

--- delivery_multi_destination/__manifest__.py
+++ delivery_multi_destination/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2016-2019 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Multiple destinations for the same delivery method",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
     "author": "Tecnativa, "
