511b5ba682b47f01774d39f4847467179d4ede0a
Luis M. Ontalba <luismaront@gmail.com>
2019-05-18 14:02:04 +0200

    [MIG] delivery_multi_destination: Migration to 12.0

--- delivery_multi_destination/models/delivery_carrier.py
+++ delivery_multi_destination/models/delivery_carrier.py
@@ -1,6 +1,6 @@
 # Copyright 2016-2019 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
