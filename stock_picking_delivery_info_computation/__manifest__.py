740af90047b62365f6fc604134719b34c0198441
Victor M.M. Torres <victor.martin@tecnativa.com>
2019-07-24 19:57:14 +0200

    [ADD] stock_picking_delivery_info_computation: Improve on calculation weight and volume

---
+++ stock_picking_delivery_info_computation/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Tecnativa - Victor M.M. Torres
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Picking Delivery Info Computation',
+    'summary': 'Improve weight and volume calculation',
+    'version': '10.0.1.0.0',
+    'category': 'Sales, Stock, Delivery',
+    'website': 'https://github.com/OCA/delivery-carrier',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'delivery',
+    ],
+    'data': [
+        'views/delivery_view.xml',
+        'report/report_shipping.xml',
+        'report/report_deliveryslip.xml',
+    ],
+}
