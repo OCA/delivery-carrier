2a809fa6d172a78de77081609fbc2f8af98794e6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-28 01:12:16 +0200

    [IMP] stock_picking_delivery_info_computation: Improve code + missing features
    * Better readme
    * Improved tests
    * Compute volume on sales order confirmation
    * Compute volume on backorder creation
    * Better code in some places
    * Improved views

--- stock_picking_delivery_info_computation/models/stock_picking.py
+++ stock_picking_delivery_info_computation/models/stock_picking.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019 Tecnativa - Victor M.M. Torres
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, models
 
@@ -25,12 +26,25 @@ class StockPicking(models.Model):
     @api.multi
     def action_calculate_volume(self):
         for rec in self:
-            has_done = any(rec.mapped('pack_operation_product_ids.qty_done'))
-            field = 'qty_done' if has_done else 'product_qty'
-            rec.volume = sum(rec.pack_operation_product_ids.mapped(
-                lambda x: x[field] * x.product_id.volume
-            ))
-            if rec.move_lines and not has_done:
+            if rec.pack_operation_product_ids:
+                has_done = any(
+                    rec.mapped('pack_operation_product_ids.qty_done')
+                )
+                field = 'qty_done' if has_done else 'product_qty'
+                rec.volume = sum(rec.pack_operation_product_ids.mapped(
+                    lambda x: x[field] * x.product_id.volume
+                ))
+            else:
                 rec.volume = sum(rec.move_lines.mapped(
                     lambda x: x.product_uom_qty * x.product_id.volume
                 ))
+
+    def _create_backorder(self, backorder_moves=None):
+        """Compute volume on newly created backorders."""
+        if backorder_moves is None:
+            backorder_moves = []
+        backorders = super(StockPicking, self)._create_backorder(
+            backorder_moves=backorder_moves,
+        )
+        backorders.action_calculate_volume()
+        return backorders
