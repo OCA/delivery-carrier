b52ba48c61b19e53f3f25499a6ab6d82035a2291
David Beal <david.beal@akretion.com>
2019-08-28 19:52:50 +0200

    MIGR v12 delivery_roulier

--- delivery_roulier/__manifest__.py
+++ delivery_roulier/__manifest__.py
@@ -1,10 +1,9 @@
-# coding: utf-8
 #  @author Raphael Reverdy <raphael.reverdy@akretion.com>
 #          David BEAL <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Delivery Carrier Roulier',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'summary': 'Integration of multiple carriers',
     'maintainer': 'Akretion, Odoo Community Association (OCA)',
@@ -12,13 +11,11 @@
     'depends': [
         'partner_helper',
         'base_phone',  # from oca/telephony
-        'keychain',  # from oca/server-tools
         'base_suspend_security',
         'product_harmonized_system',  # from oca/intrastat
         'base_delivery_carrier_label',
-        'delivery_carrier_deposit',
     ],
-    'website': 'http://www.akretion.com/',
+    'website': 'https://github.com/delivery-carrier',
     'data': [
         'views/stock_quant_package.xml',
     ],
@@ -28,7 +25,5 @@
         ],
     },
     'installable': True,
-    'auto_install': False,
     'license': 'AGPL-3',
-    'application': False,
 }
--- delivery_roulier/models/__init__.py
+++ delivery_roulier/models/__init__.py
@@ -1,3 +1,4 @@
 from . import stock_picking
 from . import stock_quant_package
-from . import stock_pack_operation
+from . import stock_move_line
+from . import carrier_account
