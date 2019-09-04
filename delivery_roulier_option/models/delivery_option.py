83fe197bb2c0da38eac1d86696264cb4769f0c3c
Florian da Costa <florian.dacosta@akretion.com>
2019-09-04 16:45:11 +0200

    Migrate delivery_roulier_option to v12

--- delivery_roulier_option/models/delivery_option.py
+++ delivery_roulier_option/models/delivery_option.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # © 2016 David BEAL @ Akretion <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,7 +5,6 @@ from odoo import models, fields
 
 
 class DeliveryCarrierTemplateOption(models.Model):
-    """ Available options for a carrier (partner) """
     _inherit = 'delivery.carrier.template.option'
 
     name = fields.Char(translate=True)
--- delivery_roulier_option/models/stock_picking.py
+++ delivery_roulier_option/models/stock_picking.py
@@ -3,7 +3,7 @@
 #          David BEAL <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.addons.delivery_roulier import implemented_by_carrier
 
 _logger = logging.getLogger(__name__)
@@ -12,10 +12,17 @@ _logger = logging.getLogger(__name__)
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    display_insurance = fields.Boolean(
-        compute='_compute_check_options',
-        string="Define a condition to display/hide your custom Insurance"
-               "field with a dedicated view")
+    customs_category = fields.Selection(
+        selection=[
+            ('gift', _("Gift")),
+            ('sample', _("Samples")),
+            ('commercial', _("Commercial Goods")),
+            ('document', _("Documents")),
+            ('other', _("Other")),
+            ('return', _("Goods return")),
+        ],
+        default='commercial',
+        help="Type of sending for the customs")
 
     @implemented_by_carrier
     def _map_options(self):
@@ -24,19 +31,6 @@ class StockPicking(models.Model):
     @implemented_by_carrier
     def _get_options(self, package):
         pass
-
-    @api.multi
-    @api.depends('option_ids')
-    def _compute_check_options(self):
-        insurance_opt = self.env.ref(
-            'delivery_roulier_option.carrier_opt_tmpl_INS', False)
-        for rec in self:
-            if insurance_opt in [x.tmpl_option_id for x in rec.option_ids]:
-                rec.display_insurance = True
-            else:
-                rec.display_insurance = False
-                _logger.info("Picking %s display_insurance=%s",
-                             rec.name, rec.display_insurance)
 
     @api.model
     def _roulier_map_options(self):
