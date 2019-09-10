f48f7abded3add3817010546b3d1d10a3310aaf2
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2019-09-10 09:27:12 +0200

    [12.0][MIG] delivery_carrier_label_batch

--- delivery_carrier_label_batch/wizard/apply_carrier.py
+++ delivery_carrier_label_batch/wizard/apply_carrier.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Camptocamp SA
+# Copyright 2016-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from openerp import _, api, exceptions, fields, models
+from odoo import _, api, exceptions, fields, models
 
 
 class PickingBatchApplyCarrier(models.TransientModel):
