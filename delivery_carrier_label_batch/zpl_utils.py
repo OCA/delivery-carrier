a7a7aa3c9a5f96437b465038167296c446f4174f
sebalix <seb@usr-src.org>
2019-09-27 15:04:42 +0200

    [IMP] delivery_carrier_label_batch: add support for ZPL labels

---
+++ delivery_carrier_label_batch/zpl_utils.py
@@ -0,0 +1,7 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+
+def assemble_zpl2(files):
+    """Assemble a list of ZPL2 files."""
+    return "".join(files)
