c8325ec600370c7c169257a49d094e9523143599
sebalix <seb@usr-src.org>
2019-09-27 15:04:42 +0200

    [IMP] delivery_carrier_label_batch: add support for ZPL labels

--- delivery_carrier_label_batch/pdf_utils.py
+++ delivery_carrier_label_batch/pdf_utils.py
@@ -1,7 +1,14 @@
 # Copyright 2013-2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from io import BytesIO, StringIO
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from io import BytesIO
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from PyPDF2 import PdfFileReader, PdfFileWriter
+except ImportError:
+    _logger.debug('Cannot import "PyPDF2". Please make sure it is installed.')
 
 
 def assemble_pdf(pdf_list):
