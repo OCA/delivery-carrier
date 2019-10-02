2ba96d84f3af3417da11bc5ae163d563e14f2927
sergio-teruel <sergio.teruel@tecnativa.com>
2019-10-02 13:54:41 +0200

    [IMP] partner_delivery_zone: Sync delivery zone between order and picking

--- partner_delivery_zone/models/stock_picking.py
+++ partner_delivery_zone/models/stock_picking.py
@@ -24,3 +24,15 @@ class StockPicking(models.Model):
                 else picking.partner_id.commercial_partner_id
             )
             picking.delivery_zone_id = partner.delivery_zone_id
+
+    def write(self, vals):
+        # Update sale order delivery zone if user update it a picking linked
+        # to a sale order,
+        res = super().write(vals)
+        if "delivery_zone_id" in vals and not self.env.context.get(
+            "skip_delivery_zone_update", False
+        ):
+            self.mapped("sale_id").with_context(skip_delivery_zone_update=True).write(
+                {"delivery_zone_id": vals["delivery_zone_id"]}
+            )
+        return res
