45c56e0167eb4e9baed639dcc303fb02a118e8fb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-10-17 20:36:30 +0200

    [FIX] delivery_auto_refresh: Avoid error when confirming sales order + add on create
    As the used method now has a check for not being used when the order is confirmed, so
    it collapses when you want to add the delivery line in that moment.
    
    Previous code didn't take into account the direct creation of the sales order.

--- delivery_auto_refresh/models/stock_picking.py
+++ delivery_auto_refresh/models/stock_picking.py
@@ -19,6 +19,9 @@ class StockPicking(models.Model):
         sale_order = self.sale_id
         if not sale_order or not self.carrier_id:  # pragma: no cover
             return res
+        so_line = sale_order.order_line.filtered(lambda x: x.is_delivery)[:1]
+        if not so_line:  # pragma: no cover
+            return res
         total = weight = volume = quantity = 0
         for move_line in self.move_line_ids.filtered('qty_done'):
             if not move_line.product_id:
@@ -31,12 +34,10 @@ class StockPicking(models.Model):
             volume += (move_line.product_id.volume or 0.0) * qty
             quantity += qty
             total += move.sale_line_id.price_unit * qty
-        total = sale_order.currency_id.with_context(
-            date=sale_order.date_order
-        ).compute(total, sale_order.company_id.currency_id)
-        so_line = sale_order.order_line.filtered(lambda x: x.is_delivery)[:1]
-        if so_line:
-            so_line.price_unit = self.carrier_id._get_price_from_picking(
-                total, weight, volume, quantity,
-            )
+        total = sale_order.currency_id._convert(
+            total, sale_order.company_id.currency_id, sale_order.company_id,
+            sale_order.date_order)
+        so_line.price_unit = self.carrier_id._get_price_from_picking(
+            total, weight, volume, quantity,
+        )
         return res
