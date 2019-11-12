0ebc49fb225df2eb8a12dcbcf6eff254187d1cbe
Timon Tschanz <timon.tschanz@gmail.com>
2019-11-12 11:18:18 +0100

    fixes for cahnges in migration of base_deliver_carrier_label (#1)

--- delivery_carrier_label_batch/pdf_utils.py
+++ delivery_carrier_label_batch/pdf_utils.py
@@ -21,9 +21,10 @@ def assemble_pdf(pdf_list):
 
     output = PdfFileWriter()
     for pdf in pdf_list:
+
         if not pdf:
             continue
-        reader = PdfFileReader(StringIO(pdf))
+        reader = PdfFileReader(BytesIO(pdf))
 
         for page in range(reader.getNumPages()):
             output.addPage(reader.getPage(page))
