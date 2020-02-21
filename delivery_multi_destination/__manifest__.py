8ca37250dbdded474d04b725185457f64b3a1a9b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-02-21 19:51:28 +0100

    [FIX] delivery_multi_destination: Make delivery shipping properly with multi
    Handle the step of sending the shipping (triggered on picking validation) for
    multi destination delivery. This means to manually check for fixing prices (as
    the implementation doesn't check this part and always take the main carrier price,
    not the subcarrier one.
    
    Test for this use case done, although the rest of the cases are not covered by
    tests yet.

--- delivery_multi_destination/__manifest__.py
+++ delivery_multi_destination/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Multiple destinations for the same delivery method",
-    "version": "12.0.1.0.1",
+    "version": "12.0.1.1.0",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
     "author": "Tecnativa, "
