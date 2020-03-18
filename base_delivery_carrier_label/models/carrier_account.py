6a232957f157cec5d9c45c6870ef1f61e6f6a930
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-18 17:08:14 +0700

    [IMP] base_delivery_carrier_label: black, isort

--- base_delivery_carrier_label/models/carrier_account.py
+++ base_delivery_carrier_label/models/carrier_account.py
@@ -2,30 +2,30 @@
 # Copyright 2014 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class CarrierAccount(models.Model):
-    _name = 'carrier.account'
-    _description = 'Base account datas'
+    _name = "carrier.account"
+    _description = "Base account datas"
 
     @api.model
     def _selection_file_format(self):
         """ To inherit to add label file types"""
-        return [('PDF', 'PDF'),
-                ('ZPL', 'ZPL'),
-                ('XML', 'XML')]
+        return [("PDF", "PDF"), ("ZPL", "ZPL"), ("XML", "XML")]
 
     name = fields.Char(required=True)
     delivery_type = fields.Selection(
         selection=lambda self: self.env["delivery.carrier"]
-        ._fields["delivery_type"].selection,
-        help="This field may be used to link an account to a carrier")
-    account = fields.Char(string='Account Number', required=True)
-    password = fields.Char(string='Account Password', required=True)
+        ._fields["delivery_type"]
+        .selection,
+        help="This field may be used to link an account to a carrier",
+    )
+    account = fields.Char(string="Account Number", required=True)
+    password = fields.Char(string="Account Password", required=True)
     company_id = fields.Many2one(comodel_name="res.company", string="Company")
     file_format = fields.Selection(
-        selection='_selection_file_format',
-        string='File Format',
-        help="Default format of the carrier's label you want to print"
+        selection="_selection_file_format",
+        string="File Format",
+        help="Default format of the carrier's label you want to print",
     )
