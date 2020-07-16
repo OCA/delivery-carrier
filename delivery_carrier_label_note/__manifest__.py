905f0730b7c14378191231c03e3a3ea3309856fd
victoralmau <victoralmau@gmail.com>
2020-07-16 11:15:46 +0200

    travis errors

--- delivery_carrier_label_note/__manifest__.py
+++ delivery_carrier_label_note/__manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Delivery carrier label note",
     "version": "12.0.1.0.0",
-    "author": "Odoo Nodriza Tech (ONT)",
+    "author": "Odoo Nodriza Tech (ONT), Odoo Community Association (OCA)",
     "website": "https://nodrizatech.com/",
     "category": "Delivery",
     "license": "AGPL-3",
