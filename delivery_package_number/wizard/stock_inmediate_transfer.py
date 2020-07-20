b089590746ea8b9e0df3c2c264209cad71b34a44
david <david.vidal@tecnativa.com>
2020-07-20 09:49:31 +0200

    [ADD] delivery_package_number: New module

---
+++ delivery_package_number/wizard/stock_inmediate_transfer.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class StockImmediateTransfer(models.TransientModel):
+    _inherit = 'stock.immediate.transfer'
+
+    number_of_packages = fields.Integer(
+        help="Set the number of packages for this picking(s)",
+    )
+
+    def process(self):
+        if self.number_of_packages:
+            self.pick_ids.write(
+                {"number_of_packages": self.number_of_packages})
+        return super().process()
