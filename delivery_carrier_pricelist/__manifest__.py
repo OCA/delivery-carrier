50d5f4f8d4061a99aaf07dd412ba30f6706af5c2
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-08-18 10:37:59 +0200

    Add delivery_carrier_pricelist
    New module allowing to compute a delivery cost based on the sales order
    pricelist.
    
    Most of the code of the module is to update the many 'attrs' of the
    'delivery' module which have domains based on the "delivery_type" field
    and cannot be extended in XML without breaking compatibility.
    
    When using an external provider (such as DHL, UPS), the "Pricelist"
    provider cannot be used. In this case, the invoice policy, which is by
    default "Estimate" or "Real" has a third option "Pricelist Cost". This
    option would not make sense with "Estimate" or "Real", which is why this
    field is used.

---
+++ delivery_carrier_pricelist/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Shipping Method Pricelist",
+    "summary": "Compute method method fees based on the product's pricelist.",
+    "version": "13.0.1.0.0",
+    "category": "Delivery",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "installable": True,
+    "license": "AGPL-3",
+    "depends": ["delivery"],
+    "data": [],
+}
