e45d09ac57a80e68a278122a9e3c139a55235b9b
Sébastien BEAU <sebastien.beau@akretion.com>
2020-08-30 10:58:44 +0200

    [12.0][REF] extract delivery_carrier_info from base_delivery_carrier_label

---
+++ delivery_carrier_info/__manifest__.py
@@ -0,0 +1,30 @@
+# Copyright 2020 Akretion (https://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+{
+    "name": "Delivery Carrier Info",
+    "summary": "Add code and description on carrier",
+    "version": "12.0.1.0.0",
+    "category": "Delivery",
+    "website": "www.akretion.com",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "delivery",
+    ],
+    "data": [
+        "views/delivery_view.xml"
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
