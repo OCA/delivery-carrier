82da2d1c1d77c98540644411e17fab564e765bc7
Marçal Isern <marsal.isern@qubiq.es>
2020-09-16 09:17:38 +0200

    [IMP] delivery_state: black, isort, prettier

--- delivery_state/tests/test_delivery_state.py
+++ delivery_state/tests/test_delivery_state.py
@@ -6,46 +6,56 @@ from odoo.tests.common import TransactionCase
 
 
 class TestDeliveryState(TransactionCase):
-
     def setUp(self):
         super().setUp()
-        product_shipping_cost = self.env['product.product'].create({
-            'type': 'service',
-            'name': 'Shipping costs',
-            'standard_price': 10,
-            'list_price': 100,
-        })
-        self.carrier = self.env['delivery.carrier'].create({
-            'name': 'Test carrier',
-            'delivery_type': 'fixed',
-            'product_id': product_shipping_cost.id,
-            'fixed_price': 99.99,
-        })
-        self.product = self.env["product.product"].create({
-            "name": "Test product",
-            "type": "product",
-        })
-        self.partner = self.env["res.partner"].create({
-            "name": "Mr. Odoo",
-        })
-        self.pricelist = self.env['product.pricelist'].create({
-            'name': 'Test pricelist',
-            'item_ids': [(0, 0, {
-                'applied_on': '3_global',
-                'compute_price': 'formula',
-                'base': 'list_price',
-            })]
-        })
+        product_shipping_cost = self.env["product.product"].create(
+            {
+                "type": "service",
+                "name": "Shipping costs",
+                "standard_price": 10,
+                "list_price": 100,
+            }
+        )
+        self.carrier = self.env["delivery.carrier"].create(
+            {
+                "name": "Test carrier",
+                "delivery_type": "fixed",
+                "product_id": product_shipping_cost.id,
+                "fixed_price": 99.99,
+            }
+        )
+        self.product = self.env["product.product"].create(
+            {"name": "Test product", "type": "product"}
+        )
+        self.partner = self.env["res.partner"].create({"name": "Mr. Odoo"})
+        self.pricelist = self.env["product.pricelist"].create(
+            {
+                "name": "Test pricelist",
+                "item_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "applied_on": "3_global",
+                            "compute_price": "formula",
+                            "base": "list_price",
+                        },
+                    )
+                ],
+            }
+        )
 
     def test_delivery_state(self):
-        sale = self.env['sale.order'].create({
-            'partner_id': self.partner.id,
-            'carrier_id': self.carrier.id,
-            'pricelist_id': self.pricelist.id,
-            'order_line': [(0, 0, {
-                'product_id': self.product.id,
-                'product_uom_qty': 1})]
-        })
+        sale = self.env["sale.order"].create(
+            {
+                "partner_id": self.partner.id,
+                "carrier_id": self.carrier.id,
+                "pricelist_id": self.pricelist.id,
+                "order_line": [
+                    (0, 0, {"product_id": self.product.id, "product_uom_qty": 1})
+                ],
+            }
+        )
         sale.get_delivery_price()
         self.assertEquals(sale.delivery_price, 99.99)
         sale.set_delivery_line()
@@ -57,17 +67,17 @@ class TestDeliveryState(TransactionCase):
         picking.action_confirm()
         picking.action_assign()
         picking.send_to_shipper()
-        self.assertEquals(
-            picking.delivery_state, 'shipping_recorded_in_carrier')
+        self.assertEquals(picking.delivery_state, "shipping_recorded_in_carrier")
         self.assertTrue(picking.date_shipped)
         self.assertFalse(picking.tracking_state_history)
         picking.tracking_state_update()
         picking.date_delivered = fields.Datetime.now()
         with self.assertRaises(NotImplementedError):
             picking.cancel_shipment()
-        self.env['delivery.carrier']._patch_method(
-            'fixed_cancel_shipment', lambda *args: True)
+        self.env["delivery.carrier"]._patch_method(
+            "fixed_cancel_shipment", lambda *args: True
+        )
         picking.cancel_shipment()
-        self.assertEquals(picking.delivery_state, 'canceled_shipment')
+        self.assertEquals(picking.delivery_state, "canceled_shipment")
         self.assertFalse(picking.date_shipped)
         self.assertFalse(picking.date_delivered)
