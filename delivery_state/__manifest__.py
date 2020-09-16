43a10f7467ff7d73e2631cfa470706b0e987b384
Marçal Isern <marsal.isern@qubiq.es>
2020-09-16 09:17:38 +0200

    [IMP] delivery_state: black, isort, prettier

--- delivery_state/__manifest__.py
+++ delivery_state/__manifest__.py
@@ -3,22 +3,17 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Delivery State',
-    'summary': 'Provides fields to be able to contemplate the tracking states'
-               'and also adds a global fields',
-    'author': 'Trey (www.trey.es), '
-              'FactorLibre, '
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/delivery-carrier',
-    'license': 'AGPL-3',
-    'category': 'Delivery',
-    'version': '12.0.1.1.0',
-    'depends': [
-        'delivery',
-    ],
-    'data': [
-        'data/ir_cron_data.xml',
-        'views/stock_picking_views.xml',
-    ],
+    "name": "Delivery State",
+    "summary": "Provides fields to be able to contemplate the tracking states"
+    "and also adds a global fields",
+    "author": "Trey (www.trey.es), "
+    "FactorLibre, "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "license": "AGPL-3",
+    "category": "Delivery",
+    "version": "12.0.1.1.0",
+    "depends": ["delivery"],
+    "data": ["data/ir_cron_data.xml", "views/stock_picking_views.xml"],
 }
--- delivery_state/models/stock_picking.py
+++ delivery_state/models/stock_picking.py
@@ -6,37 +6,27 @@ from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
-    date_shipped = fields.Date(
-        string='Shipment Date',
-        readonly=True,
-    )
-    date_delivered = fields.Datetime(
-        string='Delivery Date',
-        readonly=True,
-    )
+    date_shipped = fields.Date(string="Shipment Date", readonly=True,)
+    date_delivered = fields.Datetime(string="Delivery Date", readonly=True,)
     tracking_state = fields.Char(
-        string='Tracking state',
-        readonly=True,
-        index=True,
-        track_visibility='always',
+        string="Tracking state", readonly=True, index=True, track_visibility="always",
     )
     tracking_state_history = fields.Text(
-        string='Tracking state history',
-        readonly=True,
+        string="Tracking state history", readonly=True,
     )
     delivery_state = fields.Selection(
         selection=[
-            ('shipping_recorded_in_carrier', 'Shipping recorded in carrier'),
-            ('in_transit', 'In transit'),
-            ('canceled_shipment', 'Canceled shipment'),
-            ('incidence', 'Incidence'),
-            ('customer_delivered', 'Customer delivered'),
-            ('warehouse_delivered', 'Warehouse delivered'),
+            ("shipping_recorded_in_carrier", "Shipping recorded in carrier"),
+            ("in_transit", "In transit"),
+            ("canceled_shipment", "Canceled shipment"),
+            ("incidence", "Incidence"),
+            ("customer_delivered", "Customer delivered"),
+            ("warehouse_delivered", "Warehouse delivered"),
         ],
-        string='Carrier State',
-        track_visibility='onchange',
+        string="Carrier State",
+        track_visibility="onchange",
         readonly=True,
     )
 
@@ -46,7 +36,7 @@ class StockPicking(models.Model):
             <my_provider>_tracking_state_update
         It can be triggered manually or by the cron."""
         for picking in self.filtered("carrier_id"):
-            method = '%s_tracking_state_update' % picking.delivery_type
+            method = "%s_tracking_state_update" % picking.delivery_type
             if hasattr(picking.carrier_id, method):
                 getattr(picking.carrier_id, method)(picking)
 
@@ -55,13 +45,18 @@ class StockPicking(models.Model):
         """Automated action to query the delivery states to the carriers API.
         every carrier should implement it 's own method. We split them by
         delivery type so only those carries with the method update"""
-        pickings = self.search([
-            ("state", "=", "done"),
-            ("delivery_state", "not in",
-                ["customer_delivered", "canceled_shipment"]),
-            # These won't ever autoupdate, so we don't want to evaluate them
-            ("delivery_type", "not in", [False, "fixed", "base_one_rule"]),
-        ])
+        pickings = self.search(
+            [
+                ("state", "=", "done"),
+                (
+                    "delivery_state",
+                    "not in",
+                    ["customer_delivered", "canceled_shipment"],
+                ),
+                # These won't ever autoupdate, so we don't want to evaluate them
+                ("delivery_type", "not in", [False, "fixed", "base_one_rule"]),
+            ]
+        )
         delivery_types = self.mapped("delivery_type")
         # Split them by delivery type so we can ignore those without the
         # proper method.
@@ -70,6 +65,7 @@ class StockPicking(models.Model):
             if not hasattr(self.env["delivery.carrier"], method):
                 continue
             delivery_type_pickings = pickings.filtered(
-                lambda x: x.delivery_type == delivery_type)
+                lambda x: x.delivery_type == delivery_type
+            )
             for picking in delivery_type_pickings:
                 getattr(picking.carrier_id, method)(picking)
