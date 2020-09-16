d8fa7c79e70b9d4ac793904730b8220eb6c81f96
Marçal Isern <marsal.isern@qubiq.es>
2020-09-16 09:17:38 +0200

    [IMP] delivery_state: black, isort, prettier

--- delivery_state/models/delivery_carrier.py
+++ delivery_state/models/delivery_carrier.py
@@ -5,20 +5,24 @@ from odoo import fields, models
 
 
 class DeliveryCarrier(models.Model):
-    _inherit = 'delivery.carrier'
+    _inherit = "delivery.carrier"
 
     def send_shipping(self, pickings):
         res = super().send_shipping(pickings)
-        pickings.write({
-            'delivery_state': 'shipping_recorded_in_carrier',
-            'date_shipped': fields.Date.today(),
-        })
+        pickings.write(
+            {
+                "delivery_state": "shipping_recorded_in_carrier",
+                "date_shipped": fields.Date.today(),
+            }
+        )
         return res
 
     def cancel_shipment(self, pickings):
         super().cancel_shipment(pickings)
-        pickings.write({
-            'delivery_state': 'canceled_shipment',
-            'date_delivered': False,
-            'date_shipped': False,
-        })
+        pickings.write(
+            {
+                "delivery_state": "canceled_shipment",
+                "date_delivered": False,
+                "date_shipped": False,
+            }
+        )
