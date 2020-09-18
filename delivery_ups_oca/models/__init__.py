ddd1caf7025321321de784a47eb79d597052560a
Holger Brunn <mail@hunki-enterprises.com>
2020-09-18 13:13:55 +0200

    delivery_carrier_label_ups

---
+++ delivery_ups_oca/models/__init__.py
@@ -0,0 +1,6 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import delivery_carrier
+from . import stock_picking
+from . import carrier_account
+from . import product_packaging
---
+++ delivery_ups_oca/readme/CONFIGURE.rst
@@ -0,0 +1,4 @@
+To configure this module, you need to:
+
+#. Add options to your UPS carrier
+#. Add a carrier account with delivery type ``ups`` and fill in your credentials
