da37d82df310c7aa9555e70ac7613a36a3ee3b03
Marçal Isern <marsal.isern@qubiq.es>
2020-09-20 10:39:20 +0200

    [MIG] delivery_price_method: Migration to 13.0
    [UPD] Update delivery_price_method.pot
    
    [UPD] README.rst

--- delivery_price_method/__manifest__.py
+++ delivery_price_method/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/OCA/delivery-carrier",
     "license": "AGPL-3",
     "category": "Delivery",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "depends": ["delivery"],
     "data": ["views/delivery_carrier_views.xml"],
 }
