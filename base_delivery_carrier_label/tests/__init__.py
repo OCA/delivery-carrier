0ddfc33ae8a8fe5dbdc4577f6e8d0f67ccf84dc7
Holger Brunn <mail@hunki-enterprises.com>
2020-09-24 15:15:18 +0200

    [ADD] test helper functions

--- base_delivery_carrier_label/tests/__init__.py
+++ base_delivery_carrier_label/tests/__init__.py
@@ -1,2 +1,4 @@
 from . import test_get_weight
 from . import test_manifest_wizard
+from . import test_helper_functions
+from . import test_send
---
+++ base_delivery_carrier_label/tests/test_send.py
@@ -0,0 +1,37 @@
+# Copyright 2020 Hunki Enterprises BV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import base64
+
+from odoo.tests.common import Form, TransactionCase
+
+
+class TestSend(TransactionCase):
+    """Test sending a picking"""
+
+    def test_send(self):
+        """Test if the module picks up labels returned from delivery.carrier#send"""
+        carrier = self.env.ref("delivery.normal_delivery_carrier")
+        picking_form = Form(
+            self.env["stock.picking"].with_context(
+                default_picking_type_id=self.env.ref("stock.picking_type_out").id,
+            )
+        )
+        picking_form.carrier_id = carrier
+        picking = picking_form.save()
+        carrier.fixed_send_shipping = lambda *args: [
+            dict(
+                labels=[
+                    dict(
+                        name="Hello",
+                        filename="hello_world.pdf",
+                        file=base64.b64encode(bytes("hello world", "utf8")),
+                        file_type="pdf",
+                    ),
+                ]
+            )
+        ]
+        labels_before = self.env["shipping.label"].search([])
+        carrier.send_shipping(picking)
+        label = self.env["shipping.label"].search([]) - labels_before
+        self.assertTrue(label, "No label created")
+        self.assertEqual(label.mimetype, "application/pdf", "Wrong attachment created")
