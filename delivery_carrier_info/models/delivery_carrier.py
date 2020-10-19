ed91b2ed76defa86aab94e9d1110e56c0975d658
Sébastien BEAU <sebastien.beau@akretion.com>
2020-10-19 16:32:30 +0200

    [REF] run black and precommit

--- delivery_carrier_info/models/delivery_carrier.py
+++ delivery_carrier_info/models/delivery_carrier.py
@@ -6,7 +6,7 @@ from odoo import fields, models
 
 
 class DeliveryCarrier(models.Model):
-    _inherit = 'delivery.carrier'
+    _inherit = "delivery.carrier"
 
     code = fields.Char(
         help="Delivery Method Code (according to carrier)",
