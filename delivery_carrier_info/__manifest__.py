2bcdf09ed4188b4cdb5ba3456d25489c14dcd836
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-10-21 10:46:25 +0200

    [FIX] ceci n'est pas une URL

--- delivery_carrier_info/__manifest__.py
+++ delivery_carrier_info/__manifest__.py
@@ -8,7 +8,7 @@
     "summary": "Add code and description on carrier",
     "version": "14.0.1.0.0",
     "category": "Delivery",
-    "website": "www.akretion.com",
+    "website": "https://github.com/OCA/delivery-carrier",
     "author": "Akretion,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
