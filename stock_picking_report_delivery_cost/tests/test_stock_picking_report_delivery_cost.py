59b51832ab4f14c6435cbf7a7f92348e3fe0184d
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-27 09:40:25 +0100

    [IMP] stock_picking_report_delivery_cost: black, isort, prettier

--- stock_picking_report_delivery_cost/tests/test_stock_picking_report_delivery_cost.py
+++ stock_picking_report_delivery_cost/tests/test_stock_picking_report_delivery_cost.py
@@ -8,39 +8,46 @@ class TestStockPickingReportDeliveryCost(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.partner = cls.env['res.partner'].create({'name': 'Test contact'})
-        cls.product = cls.env['product.product'].create({
-            'name': 'Test product',
-            'type': 'consu',
-        })
-        cls.carrier_product = cls.env['product.product'].create({
-            'name': 'Test product',
-            'type': 'service',
-            'list_price': 5,
-            'taxes_id': False,
-        })
-        cls.carrier = cls.env['delivery.carrier'].create({
-            'name': 'Test carrier',
-            'delivery_type': 'fixed',
-            'product_id': cls.carrier_product.id,
-        })
-        cls.pricelist = cls.env['product.pricelist'].create({
-            'name': 'Test pricelist',
-        })
-        cls.order = cls.env['sale.order'].create({
-            'partner_id': cls.partner.id,
-            'carrier_id': cls.carrier.id,
-            'pricelist_id': cls.pricelist.id,
-            'order_line': [
-                (0, 0, {
-                    'name': cls.product.name,
-                    'product_id': cls.product.id,
-                    'product_uom_qty': 2,
-                    'product_uom': cls.product.uom_id.id,
-                    'price_unit': 300.00,
-                }),
-            ],
-        })
+        cls.partner = cls.env["res.partner"].create({"name": "Test contact"})
+        cls.product = cls.env["product.product"].create(
+            {"name": "Test product", "type": "consu"}
+        )
+        cls.carrier_product = cls.env["product.product"].create(
+            {
+                "name": "Test product",
+                "type": "service",
+                "list_price": 5,
+                "taxes_id": False,
+            }
+        )
+        cls.carrier = cls.env["delivery.carrier"].create(
+            {
+                "name": "Test carrier",
+                "delivery_type": "fixed",
+                "product_id": cls.carrier_product.id,
+            }
+        )
+        cls.pricelist = cls.env["product.pricelist"].create({"name": "Test pricelist"})
+        cls.order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "carrier_id": cls.carrier.id,
+                "pricelist_id": cls.pricelist.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 2,
+                            "product_uom": cls.product.uom_id.id,
+                            "price_unit": 300.00,
+                        },
+                    ),
+                ],
+            }
+        )
 
     def test_carrier_price_for_report_before(self):
         self.order.get_delivery_price()
