ad39675e928d42a1b8970adccd47f317701e8ab0
Florian da Costa <florian.dacosta@akretion.com>
2020-12-08 19:02:51 +0100

    [MIG] delivery_roulier_option: Migration to 14.0

--- delivery_roulier_option/__manifest__.py
+++ delivery_roulier_option/__manifest__.py
@@ -1,18 +1,15 @@
-# © 2016 Raphael REVERDY <raphael.reverdy@akretion.com>
-#        David BEAL <david.beal@akretion.com>
-#        Sébastien BEAU <sebastien.beau@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Delivery Roulier Option",
-    "version": "12.0.1.0.0",
-    "author": "Akretion",
+    "version": "14.0.1.0.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
     "summary": "Add options to roulier modules",
-    "maintainer": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["florian-dacosta"],
     "category": "Warehouse",
     "depends": [
         "delivery_roulier",
-        "product_harmonized_system",  # from oca/intrastat
+        "product_harmonized_system",  # from OCA/intrastat-extrastat
     ],
     "website": "https://github.com/OCA/delivery-carrier",
     "data": [
