43bbfe5a6f216663ef41f330d0744c7610cd5e2d
Florian da Costa <florian.dacosta@akretion.com>
2020-12-08 19:02:51 +0100

    [IMP] delivery_roulier_option: black, isort, prettier

--- delivery_roulier_option/__manifest__.py
+++ delivery_roulier_option/__manifest__.py
@@ -4,20 +4,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Delivery Roulier Option',
-    'version': '12.0.1.0.0',
-    'author': 'Akretion',
-    'summary': 'Add options to roulier modules',
-    'maintainer': 'Akretion, Odoo Community Association (OCA)',
-    'category': 'Warehouse',
-    'depends': [
-        'delivery_roulier',
-        'product_harmonized_system',  # from oca/intrastat
+    "name": "Delivery Roulier Option",
+    "version": "12.0.1.0.0",
+    "author": "Akretion",
+    "summary": "Add options to roulier modules",
+    "maintainer": "Akretion, Odoo Community Association (OCA)",
+    "category": "Warehouse",
+    "depends": [
+        "delivery_roulier",
+        "product_harmonized_system",  # from oca/intrastat
     ],
-    'website': 'https://github.com/OCA/delivery-carrier',
-    'data': [
-        'data/delivery.xml',
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "data/delivery.xml",
     ],
-    'installable': True,
-    'license': 'AGPL-3',
+    "installable": True,
+    "license": "AGPL-3",
 }
--- delivery_roulier_option/models/stock_picking.py
+++ delivery_roulier_option/models/stock_picking.py
@@ -1,28 +1,30 @@
-# coding: utf-8
 #  @author Raphael Reverdy <raphael.reverdy@akretion.com>
 #          David BEAL <david.beal@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
-from odoo import models, fields, api, _
+
+from odoo import _, api, fields, models
+
 from odoo.addons.delivery_roulier import implemented_by_carrier
 
 _logger = logging.getLogger(__name__)
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     customs_category = fields.Selection(
         selection=[
-            ('gift', _("Gift")),
-            ('sample', _("Samples")),
-            ('commercial', _("Commercial Goods")),
-            ('document', _("Documents")),
-            ('other', _("Other")),
-            ('return', _("Goods return")),
+            ("gift", _("Gift")),
+            ("sample", _("Samples")),
+            ("commercial", _("Commercial Goods")),
+            ("document", _("Documents")),
+            ("other", _("Other")),
+            ("return", _("Goods return")),
         ],
-        default='commercial',
-        help="Type of sending for the customs")
+        default="commercial",
+        help="Type of sending for the customs",
+    )
 
     @implemented_by_carrier
     def _map_options(self):
@@ -50,7 +52,7 @@ class StockPicking(models.Model):
         options = {}
         if self.option_ids:
             for opt in self.option_ids:
-                opt_key = str(opt.tmpl_option_id['code'])
+                opt_key = str(opt.tmpl_option_id["code"])
                 if opt_key in mapping_options:
                     options[mapping_options[opt_key]] = True
                 else:
