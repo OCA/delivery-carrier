43cc7f5f674dd8a172090f1e1d499cd6bd9d8bf1
Florian da Costa <florian.dacosta@akretion.com>
2021-01-03 00:34:08 +0100

    [IMP] delivery_roulier_chronopost_fr: black, isort, prettier

--- delivery_roulier_chronopost_fr/__manifest__.py
+++ delivery_roulier_chronopost_fr/__manifest__.py
@@ -1,19 +1,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-{'name': 'Delivery Chronopost France',
- 'version': '12.0.1.0.1',
- 'author': "Akretion,Odoo Community Association (OCA)",
- 'category': 'Delivery',
- 'complexity': 'normal',
- 'depends': [
-     'delivery_roulier_option',
- ],
- 'website': 'https://github.com/OCA/delivery-carrier',
- 'data': [
-     'views/carrier_account.xml',
-     'data/product.xml',
-     'data/delivery.xml',
- ],
- 'installable': True,
- 'license': 'AGPL-3',
- }
+{
+    "name": "Delivery Chronopost France",
+    "version": "12.0.1.0.1",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "category": "Delivery",
+    "complexity": "normal",
+    "depends": [
+        "delivery_roulier_option",
+    ],
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "views/carrier_account.xml",
+        "data/product.xml",
+        "data/delivery.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
--- delivery_roulier_chronopost_fr/models/carrier_account.py
+++ delivery_roulier_chronopost_fr/models/carrier_account.py
@@ -4,13 +4,21 @@ from odoo import fields, models
 
 
 class CarrierAccount(models.Model):
-    _inherit = 'carrier.account'
+    _inherit = "carrier.account"
 
     chronopost_fr_subaccount = fields.Char("Chronopost Sub Account", size=3)
     chronopost_fr_file_format = fields.Selection(
-        selection=[('PDF', 'PDF'), ('PPR', 'PPR'), ('SPD', 'SPD'),
-                   ('THE', 'THE'), ('SER', 'SER'), ('Z2D', 'Z2D'),
-                   ('XML', 'XML'), ('THEPSG', 'THEPSG'), ('Z2DPSG', 'Z2DPSG')],
-        string='Chronopost File Format',
-        help="Default format of the carrier's label you want to print"
+        selection=[
+            ("PDF", "PDF"),
+            ("PPR", "PPR"),
+            ("SPD", "SPD"),
+            ("THE", "THE"),
+            ("SER", "SER"),
+            ("Z2D", "Z2D"),
+            ("XML", "XML"),
+            ("THEPSG", "THEPSG"),
+            ("Z2DPSG", "Z2DPSG"),
+        ],
+        string="Chronopost File Format",
+        help="Default format of the carrier's label you want to print",
     )
