7628759cd8092ce9cc76fae98ae0bf186bee740e
Carlos Roca <carlos.roca@tecnativa.com>
2021-01-26 14:24:04 +0100

    [MIG] delivery_multi_destination: Migration to v13.0
    [UPD] Update delivery_multi_destination.pot
    
    [UPD] README.rst

--- delivery_multi_destination/__manifest__.py
+++ delivery_multi_destination/__manifest__.py
@@ -4,13 +4,13 @@
 
 {
     "name": "Multiple destinations for the same delivery method",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.0",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["delivery",],
-    "demo": ["demo/delivery_carrier_demo.xml",],
-    "data": ["views/delivery_carrier_view.xml",],
+    "depends": ["delivery"],
+    "demo": ["demo/delivery_carrier_demo.xml"],
+    "data": ["views/delivery_carrier_view.xml"],
 }
