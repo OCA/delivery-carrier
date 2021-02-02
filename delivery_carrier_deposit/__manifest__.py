de2d47c0d466d2dd08bd79067e58c35a96d4da75
Florian da Costa <florian.dacosta@akretion.com>
2021-02-02 10:43:47 +0100

    [IMP] delivery_carrier_deposit: black, isort, prettier

--- delivery_carrier_deposit/__manifest__.py
+++ delivery_carrier_deposit/__manifest__.py
@@ -1,28 +1,27 @@
-# -*- coding: utf-8 -*-
 #    Author: David BEAL <david.beal@akretion.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Delivery Deposit',
-    'version': '10.0.0.1.2',
-    'category': 'Delivery',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'maintainer': 'Akretion',
-    'summary': 'Create deposit slips',
-    'depends': [
-        'base_delivery_carrier_label',
+    "name": "Delivery Deposit",
+    "version": "10.0.0.1.2",
+    "category": "Delivery",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainer": "Akretion",
+    "summary": "Create deposit slips",
+    "depends": [
+        "base_delivery_carrier_label",
     ],
-    'website': 'https://github.com/OCA/delivery-carrier',
-    'data': [
-        'views/stock_view.xml',
-        'wizards/deposit.xml',
-        'data/ir_sequence_data.xml',
-        'report/report.xml',
-        'report/deposit_slip.xml',
-        'security/ir.model.access.csv',
-        'security/model_security.xml',
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "views/stock_view.xml",
+        "wizards/deposit.xml",
+        "data/ir_sequence_data.xml",
+        "report/report.xml",
+        "report/deposit_slip.xml",
+        "security/ir.model.access.csv",
+        "security/model_security.xml",
     ],
-    'demo': [],
-    'installable': True,
-    'license': 'AGPL-3',
+    "demo": [],
+    "installable": True,
+    "license": "AGPL-3",
 }
