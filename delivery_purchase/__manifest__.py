1afded30b817c143da065e6535b559fabe0cdfbb
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-02-05 01:16:10 -0500

    [ADD] delivery_purchase: New module
    Co-authored-by: Pedro M. Baeza <pedro.baeza@gmail.com>
    Co-authored-by: cubells <vicent@vcubells.net>
    
    [UPD] Update delivery_purchase.pot

---
+++ delivery_purchase/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# Copyright 2016-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Delivery costs in purchases",
+    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "Operations/Purchase",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["purchase", "delivery"],
+    "data": ["views/purchase_order_view.xml"],
+}
