0e3fed7553528b421ecd11fe3d4ed475a8c01c08
gael.torrecillas <gael.torrecillas@mind-and-go.com>
2021-02-10 11:01:48 +0100

    fix error in python (oca bot)
    remove api.one

--- delivery_carrier_default_tracking_url/__manifest__.py
+++ delivery_carrier_default_tracking_url/__manifest__.py
@@ -9,7 +9,7 @@
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'maintainers': ['rousseldenis'],
-    'development_status': 'alpha',
+    'development_status': 'Alpha',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/delivery-carrier',
     'depends': [
