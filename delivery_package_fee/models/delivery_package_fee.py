13c86548151a136d8761e3bf64393883cf2a3a82
dzung.tran <dungtd@trobz.com>
2021-02-27 15:51:31 +0700

    [IMP] : black, isort, prettier

--- delivery_package_fee/models/delivery_package_fee.py
+++ delivery_package_fee/models/delivery_package_fee.py
@@ -8,8 +8,12 @@ class DeliveryPackageFee(models.Model):
     _description = "Delivery Package Fees"
 
     carrier_id = fields.Many2one(
-        comodel_name="delivery.carrier", required=True, ondelete="cascade",
+        comodel_name="delivery.carrier",
+        required=True,
+        ondelete="cascade",
     )
     product_id = fields.Many2one(
-        comodel_name="product.product", required=True, ondelete="restrict",
+        comodel_name="product.product",
+        required=True,
+        ondelete="restrict",
     )
