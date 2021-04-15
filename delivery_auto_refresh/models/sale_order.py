ee836cea3f02e3c947df173714e9f8d09183a0c0
Carlos Roca <carlos.roca@tecnativa.com>
2021-04-15 13:51:09 +0200

    [MIG] delivery_auto_refresh: Migration to v13.0

--- delivery_auto_refresh/models/sale_order.py
+++ delivery_auto_refresh/models/sale_order.py
@@ -1,22 +1,41 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, fields, models
 from odoo.tools import safe_eval
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    carrier_id = fields.Many2one(
+        compute="_compute_carrier_id", store=True, readonly=False
+    )
+
+    @api.depends("partner_id")
+    def _compute_carrier_id(self):
+        if hasattr(super(), "_compute_carrier_id"):
+            super()._compute_carrier_id()
+        for rec in self:
+            if rec.partner_id:
+                if rec.partner_id.property_delivery_carrier_id:
+                    rec.carrier_id = rec.partner_id.property_delivery_carrier_id
+
+    def _get_param_auto_add_delivery_line(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        param = "delivery_auto_refresh.auto_add_delivery_line"
+        return safe_eval(get_param(param, "0"))
+
     def _auto_refresh_delivery(self):
         self.ensure_one()
         # Make sure that if you have removed the carrier, the line is gone
         if self.state in {"draft", "sent"}:
-            self._remove_delivery_line()
-        get_param = self.env["ir.config_parameter"].sudo().get_param
-        param = "delivery_auto_refresh.auto_add_delivery_line"
-        if safe_eval(get_param(param, "0")) and self.carrier_id:
-            if self.state in {"draft", "sent"} or self.invoice_shipping_on_delivery:
+            # Context added to avoid the recursive calls and save the new
+            # value of carrier_id
+            self.with_context(auto_refresh_delivery=True)._remove_delivery_line()
+        if self._get_param_auto_add_delivery_line() and self.carrier_id:
+            if self.state in {"draft", "sent"}:
                 price_unit = self.carrier_id.rate_shipment(self)["price"]
                 self._create_delivery_line(self.carrier_id, price_unit)
 
@@ -29,14 +48,15 @@ class SaleOrder(models.Model):
 
     def write(self, vals):
         """Create or refresh delivery line after saving."""
-        res = super(SaleOrder, self).write(vals)
-        for order in self:
-            delivery_line = order.order_line.filtered("is_delivery")
-            if len(delivery_line) > 1:
-                continue
-            order.with_context(
-                delivery_discount=delivery_line.discount,
-            )._auto_refresh_delivery()
+        res = super().write(vals)
+        if self._get_param_auto_add_delivery_line() and not self.env.context.get(
+            "auto_refresh_delivery"
+        ):
+            for order in self:
+                delivery_line = order.order_line.filtered("is_delivery")
+                order.with_context(
+                    delivery_discount=delivery_line.discount,
+                )._auto_refresh_delivery()
         return res
 
     def _create_delivery_line(self, carrier, price_unit):
@@ -47,3 +67,14 @@ class SaleOrder(models.Model):
         if discount and sol:
             sol.discount = discount
         return sol
+
+    def set_delivery_line(self, carrier, amount):
+        if self._get_param_auto_add_delivery_line() and self.state in {"draft", "sent"}:
+            self.carrier_id = carrier.id
+        else:
+            return super().set_delivery_line(carrier, amount)
+
+    def _remove_delivery_line(self):
+        current_carrier = self.carrier_id
+        super()._remove_delivery_line()
+        self.carrier_id = current_carrier
