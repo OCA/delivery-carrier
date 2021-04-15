e426d273955efa1634ad29fc9a68024030ac734a
Ethan Hildick <ethan@studio73.es>
2021-04-15 18:10:26 +0200

    [14.0][MIG] delivery_package_number: Migration to 14.0
    [UPD] Update delivery_package_number.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: delivery-carrier-14.0/delivery-carrier-14.0-delivery_package_number
    Translate-URL: https://translation.odoo-community.org/projects/delivery-carrier-14-0/delivery-carrier-14-0-delivery_package_number/

--- delivery_package_number/tests/test_delivery_package_number.py
+++ delivery_package_number/tests/test_delivery_package_number.py
@@ -42,14 +42,13 @@ class TestDeliveryPackageNumber(SavepointCase):
         picking_form.number_of_packages = 3
         picking_form.save()
         self.assertEqual(self.picking.number_of_packages, 3)
-        # Now we put in pack some quantities and the number of packages is set
-        # as the number of packs is added to the number already set
-        self.picking.put_in_pack()
-        self.assertEqual(self.picking.number_of_packages, 4)
+        # We add a package and it recalculates
+        self.picking.action_put_in_pack()
+        self.assertEqual(self.picking.number_of_packages, 1)
         self.ml2.qty_done = 5
-        self.picking.put_in_pack()
-        self.assertEqual(self.picking.number_of_packages, 5)
-        # We can later set it manually if we want to
-        picking_form.number_of_packages = 2
-        picking_form.save()
+        self.picking.action_put_in_pack()
         self.assertEqual(self.picking.number_of_packages, 2)
+        # We can later set it manually if we want to
+        picking_form.number_of_packages = 3
+        picking_form.save()
+        self.assertEqual(self.picking.number_of_packages, 3)
