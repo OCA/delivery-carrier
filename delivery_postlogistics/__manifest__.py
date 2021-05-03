7450afd4aef55989b8f7fbcffad62ee44e85651c
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-05-03 09:56:11 +0200

    delivery_postlogistics: several fixes
    * fix 'PostLogistics' terms
    * make 'Package Code' required for PostLogistics delivery packaging
    * show a link listing available package codes for PostLogistics delivery
      packaging
    * update README

--- delivery_postlogistics/__manifest__.py
+++ delivery_postlogistics/__manifest__.py
@@ -1,8 +1,8 @@
 # © 2013-2016 Yannick Vaucher (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Postlogistics Shipping",
-    "summary": "Print postlogistics shipping labels",
+    "name": "PostLogistics Shipping - “Barcode” web service",
+    "summary": "Print PostLogistics shipping labels using the Barcode web service",
     "version": "13.0.1.1.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
@@ -16,6 +16,7 @@
         "data/product.xml",
         "data/delivery.xml",
         "views/delivery.xml",
+        "views/product_packaging.xml",
         "views/stock_quant_package_view.xml",
         "views/postlogistics_license.xml",
         "views/res_partner_view.xml",
