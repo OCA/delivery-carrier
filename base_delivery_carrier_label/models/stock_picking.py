2714b148c05251871fba6ee88bb3e9242f30962a
Florian da Costa <florian.dacosta@akretion.com>
2021-06-07 20:04:04 +0200

    Allow to have multiple carrier accounts for a same delivery type and still be able to select the right account depending on delivery methods

--- base_delivery_carrier_label/models/stock_picking.py
+++ base_delivery_carrier_label/models/stock_picking.py
@@ -147,15 +147,22 @@ class StockPicking(models.Model):
         vals = self._values_with_carrier_options(vals)
         return super().create(vals)
 
+    def _get_carrier_account_domain(self):
+        return [
+            ("delivery_type", "in", self.mapped("delivery_type")),
+            "|",
+            ("company_id", "=", False),
+            ("company_id", "in", self.mapped("company_id.id")),
+            "|",
+            ("carrier_ids", "=", False),
+            ("carrier_ids", "in", self.mapped("carrier_id").ids),
+        ]
+
     def _get_carrier_account(self):
         """ Return a carrier suitable for the current picking """
+        domain = self._get_carrier_account_domain()
         return self.env["carrier.account"].search(
-            [
-                ("delivery_type", "in", self.mapped("delivery_type")),
-                "|",
-                ("company_id", "=", False),
-                ("company_id", "in", self.mapped("company_id.id")),
-            ],
+            domain,
             limit=1,
             order="company_id asc, sequence asc",
         )
