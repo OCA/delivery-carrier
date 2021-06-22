160fde9ef3478668c5e95aed3adefd5b98badc7b
nans <nans.lefebvre@acsone.eu>
2021-06-22 10:24:03 +0200

    [16.0][ADD] delivery_carrier_max_weight_constraint

---
+++ delivery_carrier_max_weight_constraint/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Constrain package maximum weight",
+    "version": "16.0.1.0.1",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "category": "Delivery",
+    "depends": ["delivery"],
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "views/res_config_settings_views.xml",
+        "views/delivery_carrier_views.xml",
+        "views/stock_package_type_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
---
+++ delivery_carrier_max_weight_constraint/readme/USAGE.rst
@@ -0,0 +1,8 @@
+The constraint can be enabled at multiple levels:
+
+- Company: *Settings* - *Inventory* - *Shipping*
+- Carrier with company level as default
+- Package type with carrier level as default
+
+If the constraint is disabled at the package type level
+Odoo behaves the usual way.
