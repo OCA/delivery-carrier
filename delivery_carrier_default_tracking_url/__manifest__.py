1e8aa349c116f9d4c26e84cbcb7ab04411254ab4
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-06-24 12:02:45 +0200

    [14.0][MIG] delivery_carrier_default_tracking_url
    url removed as in 14 the carrier informations are no more on sale_order view but in sale_order_line -> not as usefull anymore

--- delivery_carrier_default_tracking_url/__manifest__.py
+++ delivery_carrier_default_tracking_url/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2019 ACSONE SA/NV
+# Copyright 2019-2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Delivery Carrier Default Tracking Url",
     "summary": """
         Adds the default tracking url on delivery carrier""",
-    "version": "10.0.1.1.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "maintainers": ["rousseldenis"],
-    "development_status": "Alpha",
+    "development_status": "Production/Stable",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/delivery-carrier",
     "depends": [
@@ -16,7 +16,6 @@
     ],
     "data": [
         "views/delivery_carrier.xml",
-        "views/sale_order.xml",
         "views/stock_picking.xml",
     ],
 }
--- delivery_carrier_default_tracking_url/models/__init__.py
+++ delivery_carrier_default_tracking_url/models/__init__.py
@@ -1,3 +1,2 @@
 from . import delivery_carrier
-from . import sale_order
 from . import stock_picking
