8c29a3a5f483ab83a21c19bd8df41440a7499cf4
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-06-24 10:46:29 +0200

    [IMP] delivery_carrier_default_tracking_url: black, isort, prettier

--- delivery_carrier_default_tracking_url/models/stock_picking.py
+++ delivery_carrier_default_tracking_url/models/stock_picking.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,9 +6,9 @@ from odoo import fields, models
 
 class StockPicking(models.Model):
 
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     default_tracking_url = fields.Char(
-        related='carrier_id.default_tracking_url',
+        related="carrier_id.default_tracking_url",
         readonly=True,
     )
