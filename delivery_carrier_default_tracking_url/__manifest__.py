566b71c5484542a88aebef1d76635287de40a6f4
sbejaoui <souheil.bejaoui@acsone.eu>
2021-07-12 16:03:54 +0200

    [14.0][MIG] - delivery_carrier_default_tracking_url

--- delivery_carrier_default_tracking_url/__manifest__.py
+++ delivery_carrier_default_tracking_url/__manifest__.py
@@ -1,23 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Delivery Carrier Default Tracking Url',
-    'summary': """
+    "name": "Delivery Carrier Default Tracking Url",
+    "summary": """
         Adds the default tracking url on delivery carrier""",
-    'version': '10.0.1.1.0',
-    'license': 'AGPL-3',
-    'maintainers': ['rousseldenis'],
-    'development_status': 'Alpha',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/delivery-carrier',
-    'depends': [
-        'delivery',
-    ],
-    'data': [
-        'views/delivery_carrier.xml',
-        'views/sale_order.xml',
-        'views/stock_picking.xml',
-    ],
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "development_status": "Alpha",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "depends": ["delivery"],
+    "data": ["views/delivery_carrier.xml", "views/stock_picking.xml"],
 }
