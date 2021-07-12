01a6bfaff95e82ab8e3c60d33522fb2def3c15fc
sbejaoui <souheil.bejaoui@acsone.eu>
2021-07-12 16:03:54 +0200

    [14.0][MIG] - delivery_carrier_default_tracking_url

--- delivery_carrier_default_tracking_url/models/delivery_carrier.py
+++ delivery_carrier_default_tracking_url/models/delivery_carrier.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,7 +6,7 @@ from odoo import fields, models
 
 class DeliveryCarrier(models.Model):
 
-    _inherit = 'delivery.carrier'
+    _inherit = "delivery.carrier"
 
     default_tracking_url = fields.Char(
         help="This is the default tracking url for this carrier."
