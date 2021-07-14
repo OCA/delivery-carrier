b1b7c5e8cc7b7d8bda9a8111577878849429a2d4
sbejaoui <souheil.bejaoui@acsone.eu>
2021-07-14 11:08:36 +0200

    [IMP] - change development_status to Beta

--- delivery_carrier_default_tracking_url/__manifest__.py
+++ delivery_carrier_default_tracking_url/__manifest__.py
@@ -8,7 +8,6 @@
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "maintainers": ["rousseldenis"],
-    "development_status": "Alpha",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/delivery-carrier",
     "depends": ["delivery"],
