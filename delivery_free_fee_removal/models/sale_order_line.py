86f55995d365ceacb64fb116c3cbdf6ceaf7ab94
sonhd <sonhd@trobz.com>
2021-08-30 21:58:45 +0700

    [IMP] delivery_free_fee_removal: black, isort, prettier

--- delivery_free_fee_removal/models/sale_order_line.py
+++ delivery_free_fee_removal/models/sale_order_line.py
@@ -7,7 +7,10 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    is_free_delivery = fields.Boolean(compute="_compute_is_free_delivery", store=True,)
+    is_free_delivery = fields.Boolean(
+        compute="_compute_is_free_delivery",
+        store=True,
+    )
 
     @api.depends("is_delivery", "currency_id", "price_total")
     def _compute_is_free_delivery(self):
