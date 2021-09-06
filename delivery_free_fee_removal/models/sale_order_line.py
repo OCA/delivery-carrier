d6bf63755077248d73fd8fdffb92ba81ce49c9aa
sonhd <sonhd@trobz.com>
2021-09-06 17:32:58 +0700

    [MIG] delivery_free_fee_removal: Migration to 14.0
    [UPD] Update delivery_free_fee_removal.pot
    
    [UPD] README.rst

--- delivery_free_fee_removal/models/sale_order_line.py
+++ delivery_free_fee_removal/models/sale_order_line.py
@@ -7,10 +7,7 @@ from odoo import api, fields, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    is_free_delivery = fields.Boolean(
-        compute="_compute_is_free_delivery",
-        store=True,
-    )
+    is_free_delivery = fields.Boolean(compute="_compute_is_free_delivery", store=True)
 
     @api.depends("is_delivery", "currency_id", "price_total")
     def _compute_is_free_delivery(self):
