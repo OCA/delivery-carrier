7c3a18b52667dc0ba74cd7795a4f51b776a54882
KV <khoivha@trobz.com>
2021-10-11 10:17:40 +0700

    [MIG] stock_picking_carrier_from_rule: Migration to 14.0

--- stock_picking_carrier_from_rule/__manifest__.py
+++ stock_picking_carrier_from_rule/__manifest__.py
@@ -4,7 +4,7 @@
     Set the carrier on picking if the stock rule used has a partner
     address set with a delivery method.
     """,
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/delivery-carrier",
