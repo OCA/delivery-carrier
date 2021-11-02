03796db8e607c958f6eb9d04ca03eeb83fcfcc5e
Florian da Costa <florian.dacosta@akretion.com>
2021-11-02 15:03:47 +0100

    Migrate DPD to v14

--- delivery_roulier_dpd_fr/models/stock_picking.py
+++ delivery_roulier_dpd_fr/models/stock_picking.py
@@ -1,40 +1,26 @@
-#  @author Raphael Reverdy <raphael.reverdy@akretion.com>
-#          EBII MonsieurB <monsieurb@saaslys.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import logging
-from datetime import datetime, timedelta
-
 from odoo import models
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-
-_logger = logging.getLogger(__name__)
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def _dpd_get_shipping_date(self, package_id):
-        """Estimate shipping date."""
+    def _dpd_fr_soap_get_service(self, account, package=None):
+        service = self._roulier_get_service(account, package=package)
+        service.update(
+            {
+                "customerCountry": account.dpd_fr_soap_customer_country,
+                "customerId": account.dpd_fr_soap_customer_id,
+                "agencyId": account.dpd_fr_soap_agency_id,
+                "reference1": self.sale_id.name or self.origin,
+            }
+        )
+        if self.carrier_code == "DPD_Relais":
+            service["dropOffLocation"] = self._dpd_dropoff_site()
+            service["notifications"] = "No"
+        return service
+
+    def _dpd_dropoff_site(self):
         self.ensure_one()
-        shipping_date = self.min_date
-        if self.date_done:
-            shipping_date = self.date_done
-
-        shipping_date = datetime.strptime(shipping_date, DEFAULT_SERVER_DATETIME_FORMAT)
-
-        tomorrow = datetime.now() + timedelta(1)
-        if shipping_date < tomorrow:
-            # don't send in the past
-            shipping_date = tomorrow
-        return shipping_date.strftime("%Y/%m/%d")
-
-    def _dpd_get_auth(self, package):
-        account = self._get_account(package)
-        service = account.get_data()
-        auth = {
-            "login": account.login,
-            "password": account._get_password(),
-            "isTest": service.get("isTest"),
-        }
-        return auth
+        return ""  # like P22895 TODO implement this
