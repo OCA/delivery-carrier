6c591ede9676a842019fde3d0a3aa00ab7fd1784
Ethan Hildick <ethan@studio73.es>
2021-12-13 11:56:50 +0100

    [MIG] delivery_schenker: Migration to 14.0

--- delivery_schenker/__manifest__.py
+++ delivery_schenker/__manifest__.py
@@ -1,12 +1,13 @@
 # Copyright 2021 Tecnativa - David Vidal
+# Copyright 2021 Studio73 - Ethan Hildick <ethan@studio73.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Delivery Schenker",
     "summary": "Delivery Carrier implementation for DB Schenker API",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/delivery-carrier",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "author": "Tecnativa, Studio73, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
