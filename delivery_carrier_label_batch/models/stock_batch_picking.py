0e59ecafca95756c6bbd6b5679dd6628c82edc70
KV <khoivha@trobz.com>
2021-12-21 22:49:55 +0700

    [IMP] delivery_carrier_label_batch: black, isort, prettier

--- delivery_carrier_label_batch/models/stock_batch_picking.py
+++ delivery_carrier_label_batch/models/stock_batch_picking.py
@@ -5,31 +5,30 @@ from odoo import _, api, fields, models
 
 class StockBatchPicking(models.Model):
 
-    """ Add carrier and carrier options on batch
+    """Add carrier and carrier options on batch
 
     to be able to massively set those options on related picking.
 
     """
+
     _inherit = "stock.picking.batch"
 
     carrier_id = fields.Many2one(
-        'delivery.carrier', 'Carrier',
-        states={'done': [('readonly', True)]})
-    option_ids = fields.Many2many(
-        'delivery.carrier.option',
-        string='Options')
+        "delivery.carrier", "Carrier", states={"done": [("readonly", True)]}
+    )
+    option_ids = fields.Many2many("delivery.carrier.option", string="Options")
 
     @api.multi
     def action_set_options(self):
-        """ Apply options to picking of the batch
+        """Apply options to picking of the batch
 
         This will replace all carrier options in picking
 
         """
         for rec in self:
             options_datas = {
-                'carrier_id': rec.carrier_id.id,
-                'option_ids': [(6, 0, rec.option_ids.ids)],
+                "carrier_id": rec.carrier_id.id,
+                "option_ids": [(6, 0, rec.option_ids.ids)],
             }
             rec.picking_ids.write(options_datas)
 
@@ -39,7 +38,7 @@ class StockBatchPicking(models.Model):
         options = carrier.available_option_ids
         return options.filtered(lambda rec: rec.mandatory or rec.by_default)
 
-    @api.onchange('carrier_id')
+    @api.onchange("carrier_id")
     def carrier_id_change(self):
         """ Inherit this method in your module """
         if self.carrier_id:
@@ -54,23 +53,26 @@ class StockBatchPicking(models.Model):
             self.option_ids = [(6, 0, default_options.ids)]
             self.carrier_type = self.carrier_id.delivery_type
             self.carrier_code = self.carrier_id.code
-            return {'domain': {
-                'option_ids': [('id', 'in', available_options.ids)],
-            }}
+            return {
+                "domain": {
+                    "option_ids": [("id", "in", available_options.ids)],
+                }
+            }
         return {}
 
-    @api.onchange('option_ids')
+    @api.onchange("option_ids")
     def option_ids_change(self):
         res = {}
         if not self.carrier_id:
             return res
         for available_option in self.carrier_id.available_option_ids:
-            if (available_option.mandatory and
-                    available_option not in self.option_ids):
-                res['warning'] = {
-                    'title': _('User Error !'),
-                    'message': _("You can not remove a mandatory option."
-                                 "\nPlease reset options to default.")
+            if available_option.mandatory and available_option not in self.option_ids:
+                res["warning"] = {
+                    "title": _("User Error !"),
+                    "message": _(
+                        "You can not remove a mandatory option."
+                        "\nPlease reset options to default."
+                    ),
                 }
                 # Due to https://github.com/odoo/odoo/issues/2693 we cannot
                 # reset options
@@ -80,10 +82,10 @@ class StockBatchPicking(models.Model):
     @api.multi
     def _values_with_carrier_options(self, values):
         values = values.copy()
-        carrier_id = values.get('carrier_id')
-        option_ids = values.get('option_ids')
+        carrier_id = values.get("carrier_id")
+        option_ids = values.get("option_ids")
         if carrier_id and not option_ids:
-            carrier = self.env['delivery.carrier'].browse(carrier_id)
+            carrier = self.env["delivery.carrier"].browse(carrier_id)
             options = self._get_options_to_add(carrier)
             if options:
                 values.update(option_ids=[(6, 0, options.ids)])
@@ -91,7 +93,7 @@ class StockBatchPicking(models.Model):
 
     @api.multi
     def write(self, values):
-        """ Set the default options when the delivery method is changed.
+        """Set the default options when the delivery method is changed.
 
         So we are sure that the options are always in line with the
         current delivery method.
@@ -102,7 +104,7 @@ class StockBatchPicking(models.Model):
 
     @api.model
     def create(self, values):
-        """ Set the default options when the delivery method is set on creation
+        """Set the default options when the delivery method is set on creation
 
         So we are sure that the options are always in line with the
         current delivery method.
