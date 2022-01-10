34d2eb3ae17c70a78fb2a3ab832953221b6c28cd
sonhd <sonhd@trobz.com>
2022-01-10 11:09:55 +0700

    [14.0] base_delivery_carrier_label: add check when no carrier_id assigned and fix import mock bug

--- base_delivery_carrier_label/tests/test_send.py
+++ base_delivery_carrier_label/tests/test_send.py
@@ -1,8 +1,7 @@
 # Copyright 2020 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import base64
-
-import mock
+from unittest import mock
 
 from odoo.tests.common import Form, TransactionCase
 
