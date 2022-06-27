b6b358753a9a0f8f33e690b561f42e05325e5a6c
Víctor Martínez <victor.martinez@tecnativa.com>
2022-06-27 16:11:37 +0200

    [IMP] delivery_tnt_oca: Add tnt_use_packages_from_picking field in carrier to use (or not) package from pickings. TT37523

--- delivery_tnt_oca/models/tnt_request.py
+++ delivery_tnt_oca/models/tnt_request.py
@@ -28,6 +28,7 @@ class TntRequest(object):
         self.password = self.carrier.tnt_oca_ws_password
         self.account = self.carrier.tnt_oca_ws_account
         self.default_packaging_id = self.carrier.tnt_default_packaging_id
+        self.use_packages_from_picking = self.carrier.tnt_use_packages_from_picking
         self.url = "https://express.tnt.com"
         auth_encoding = "%s:%s" % (self.username, self.password)
         self.authorization = base64.b64encode(auth_encoding.encode("utf-8")).decode(
@@ -200,7 +201,7 @@ class TntRequest(object):
 
     def _get_data_total_shipping(self):
         weight = self.record.shipping_weight
-        if self.record.package_ids:
+        if self.use_packages_from_picking and self.record.package_ids:
             height = sum(self.record.package_ids.mapped("height"))
             width = sum(self.record.package_ids.mapped("width"))
             p_length = sum(self.record.package_ids.mapped("length"))
