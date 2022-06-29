9f6d3549c4a99b3abd401620903a6f71674fb6a0
cubells <vicent@vcubells.net>
2022-06-29 18:05:53 +0200

    [IMP] stock_picking_delivery_info_computation: black, isort, prettier

--- stock_picking_delivery_info_computation/models/sale_order.py
+++ stock_picking_delivery_info_computation/models/sale_order.py
@@ -4,10 +4,10 @@ from odoo import models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     def action_confirm(self):
         """Recompute picking volume when confirming sales order."""
         res = super(SaleOrder, self).action_confirm()
-        self.mapped('picking_ids').action_calculate_volume()
+        self.mapped("picking_ids").action_calculate_volume()
         return res
