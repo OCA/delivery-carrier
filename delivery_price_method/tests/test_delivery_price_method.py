0631aa93854f8ddb4a0cd4484532c7589b528678
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-11 18:08:10 +0200

    [MIG] delivery_price_method: Migration to 14.0 TT36947
    [UPD] Update delivery_price_method.pot
    
    [UPD] README.rst

--- delivery_price_method/tests/test_delivery_price_method.py
+++ delivery_price_method/tests/test_delivery_price_method.py
@@ -30,7 +30,17 @@ class TestDeliveryPriceMethod(SavepointCase):
         self.pricelist = self.env["product.pricelist"].create(
             {
                 "name": "Test pricelist",
-                "item_ids": [(0, 0, {"applied_on": "3_global", "base": "list_price"})],
+                "item_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "applied_on": "3_global",
+                            "compute_price": "formula",
+                            "base": "list_price",
+                        },
+                    )
+                ],
             }
         )
         self.product = self.env.ref("product.product_delivery_01")
@@ -62,16 +72,16 @@ class TestDeliveryPriceMethod(SavepointCase):
         delivery_lines = sale.order_line.filtered(lambda r: r.is_delivery)
         delivery_price = sum(delivery_lines.mapped("price_unit"))
         self.assertEqual(float_compare(delivery_price, 99.99, precision_digits=2), 0)
-        self.assertEquals(len(delivery_lines), 1)
+        self.assertEqual(len(delivery_lines), 1)
         sale.action_confirm()
         picking = sale.picking_ids[0]
-        self.assertEquals(len(picking.move_lines), 1)
-        self.assertEquals(picking.carrier_id, self.carrier)
+        self.assertEqual(len(picking.move_lines), 1)
+        self.assertEqual(picking.carrier_id, self.carrier)
         picking.action_confirm()
         picking.action_assign()
         self.assertFalse(picking.carrier_price)
         picking.send_to_shipper()
-        self.assertEquals(picking.carrier_price, 99.99)
+        self.assertEqual(picking.carrier_price, 99.99)
 
     def test_delivery_price_method(self):
         self.carrier.write({"price_method": "fixed", "fixed_price": 99.99})
@@ -80,12 +90,12 @@ class TestDeliveryPriceMethod(SavepointCase):
         delivery_lines = sale.order_line.filtered(lambda r: r.is_delivery)
         delivery_price = sum(delivery_lines.mapped("price_unit"))
         self.assertEqual(float_compare(delivery_price, 99.99, precision_digits=2), 0)
-        self.assertEquals(len(delivery_lines), 1)
+        self.assertEqual(len(delivery_lines), 1)
         self.carrier.write({"price_method": "fixed", "fixed_price": 5})
         self._add_delivery()
         delivery_lines = sale.order_line.filtered(lambda r: r.is_delivery)
         delivery_price = sum(delivery_lines.mapped("price_unit"))
-        self.assertEquals(delivery_price, 5)
+        self.assertEqual(delivery_price, 5)
         self.carrier.write(
             {
                 "price_method": "base_on_rule",
@@ -106,4 +116,4 @@ class TestDeliveryPriceMethod(SavepointCase):
         self._add_delivery()
         delivery_lines = sale.order_line.filtered(lambda r: r.is_delivery)
         delivery_price = sum(delivery_lines.mapped("price_unit"))
-        self.assertEquals(delivery_price, 11.11)
+        self.assertEqual(delivery_price, 11.11)
