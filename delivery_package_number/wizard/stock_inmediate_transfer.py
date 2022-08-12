492d854ed127e3c03b9a29babbc7bd94f018ea53
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-12 11:23:19 +0200

    [IMP] delivery_package_number: Hide Number of packages in Immediate Transfer wizard if picking have not carrier. TT38549

--- delivery_package_number/wizard/stock_inmediate_transfer.py
+++ delivery_package_number/wizard/stock_inmediate_transfer.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockImmediateTransfer(models.TransientModel):
@@ -9,6 +9,12 @@ class StockImmediateTransfer(models.TransientModel):
     number_of_packages = fields.Integer(
         help="Set the number of packages for this picking(s)",
     )
+    ask_number_of_packages = fields.Boolean(compute="_compute_ask_number_of_packages")
+
+    @api.depends("pick_ids")
+    def _compute_ask_number_of_packages(self):
+        for item in self:
+            item.ask_number_of_packages = bool(item.pick_ids.carrier_id)
 
     def process(self):
         if self.number_of_packages:
