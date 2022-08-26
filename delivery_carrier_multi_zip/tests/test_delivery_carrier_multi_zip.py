079d651d1ee09682e20b8ae95f87c8fe121f0fff
Philipp Zimmer <philipp.zimmer@mta-consulting.de>
2022-08-27 01:29:03 +0200

    [MIG] delivery_carrier_multi_zip: Migration to 14.0
    [UPD] Update delivery_carrier_multi_zip.pot
    
    [UPD] README.rst

--- delivery_carrier_multi_zip/tests/test_delivery_carrier_multi_zip.py
+++ delivery_carrier_multi_zip/tests/test_delivery_carrier_multi_zip.py
@@ -29,26 +29,26 @@ class TestDeliveryCarrierMultiZip(common.SavepointCase):
         )
 
     def test_delivery_multi_zip_write_create(self):
-        self.assertEqual(len(self.carrier.zip_ids), 1)
-        self.assertEqual(self.carrier.zip_ids.zip_from, "0001")
-        self.assertEqual(self.carrier.zip_ids.zip_to, "0001")
+        self.assertEqual(len(self.carrier.zip_range_ids), 1)
+        self.assertEqual(self.carrier.zip_range_ids.zip_from, "0001")
+        self.assertEqual(self.carrier.zip_range_ids.zip_to, "0001")
         self.carrier.write({"zip_to": "0002"})
-        self.assertEqual(len(self.carrier.zip_ids), 2)
-        self.assertEqual(self.carrier.zip_ids[1].zip_from, "0")
-        self.assertEqual(self.carrier.zip_ids[1].zip_to, "0002")
+        self.assertEqual(len(self.carrier.zip_range_ids), 2)
+        self.assertEqual(self.carrier.zip_range_ids[1].zip_from, "0")
+        self.assertEqual(self.carrier.zip_range_ids[1].zip_to, "0002")
 
     def test_post_init_hook(self):
-        self.carrier.zip_ids.unlink()
+        self.carrier.zip_range_ids.unlink()
         self.carrier.with_context(bypass_multi_zip=True).write({"zip_from": "0002"})
         post_init_hook(self.env.cr, None)
-        self.assertEqual(len(self.carrier.zip_ids), 1)
-        self.assertEqual(self.carrier.zip_ids.zip_from, "0002")
-        self.assertEqual(self.carrier.zip_ids.zip_to, "z")
+        self.assertEqual(len(self.carrier.zip_range_ids), 1)
+        self.assertEqual(self.carrier.zip_range_ids.zip_from, "0002")
+        self.assertEqual(self.carrier.zip_range_ids.zip_to, "z")
         self.carrier.refresh()
         self.assertFalse(self.carrier.zip_from)
 
     def test_available_carriers(self):
         self.assertIn(self.carrier, self.carrier.available_carriers(self.partner_1))
         self.assertNotIn(self.carrier, self.carrier.available_carriers(self.partner_2))
-        self.carrier.zip_ids = [(0, 0, {"zip_from": "0002", "zip_to": "0020"})]
+        self.carrier.zip_range_ids = [(0, 0, {"zip_from": "0002", "zip_to": "0020"})]
         self.assertIn(self.carrier, self.carrier.available_carriers(self.partner_2))
