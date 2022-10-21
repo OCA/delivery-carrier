1b6cde37de26e1952a00fb6e002741c975d9372b
Víctor Martínez <victor.martinez@tecnativa.com>
2022-10-21 17:29:56 +0200

    [MIG] partner_delivery_schedule: Migration to 15.0
    TT36479

--- partner_delivery_schedule/__manifest__.py
+++ partner_delivery_schedule/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Partner Delivery Schedule",
     "summary": "Set on partners a schedule for delivery goods",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
--- partner_delivery_schedule/tests/test_partner_delivery_schedule.py
+++ partner_delivery_schedule/tests/test_partner_delivery_schedule.py
@@ -1,13 +1,10 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.exceptions import ValidationError
-from odoo.tests import SavepointCase
+from odoo.tests import Form, TransactionCase
 
 
-class TestStockPikingReturnRefundOption(SavepointCase):
-    at_install = False
-    post_install = True
-
+class TestStockPikingReturnRefundOption(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -41,6 +38,18 @@ class TestStockPikingReturnRefundOption(SavepointCase):
                 "delivery_schedule_ids": [(6, 0, cls.schedule.ids + cls.schedule2.ids)],
             }
         )
+        cls.report_model = cls.env["ir.actions.report"]
+        cls.product = cls.env["product.product"].create({"name": "Test product"})
+        cls.order = cls._create_sale_order(cls)
+        cls.order.action_confirm()
+        cls.picking = cls.order.picking_ids[0]
+
+    def _create_sale_order(self):
+        order_form = Form(self.env["sale.order"])
+        order_form.partner_id = self.partner
+        with order_form.order_line.new() as line_form:
+            line_form.product_id = self.product
+        return order_form.save()
 
     def test_partner_schedule_name(self):
         self.assertEqual(self.schedule.name_get()[0][1], "08:00-10:00 (Mo, Tu)")
@@ -72,3 +81,17 @@ class TestStockPikingReturnRefundOption(SavepointCase):
             day_update[day[0]] = True
         self.schedule.update(day_update)
         self.assertTrue(self.partner.allow_delivery_date("2018-09-09 09:00:00"))
+
+    def test_report_picking(self):
+        res = self.report_model._get_report_from_name(
+            "stock.report_picking"
+        )._render_qweb_text(self.picking.ids, False)
+        self.assertRegex(str(res[0]), "08:00-10:00")
+        self.assertRegex(str(res[0]), "10:00-12:00")
+
+    def test_report_deliveryslip(self):
+        res = self.report_model._get_report_from_name(
+            "stock.report_deliveryslip"
+        )._render_qweb_text(self.picking.ids, False)
+        self.assertRegex(str(res[0]), "08:00-10:00")
+        self.assertRegex(str(res[0]), "10:00-12:00")
