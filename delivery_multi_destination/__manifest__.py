b547b5a3cfd8a231ed78ed318ef87d30a619fddb
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-03 12:23:41 +0100

    [MIG] delivery_multi_destination: Migration to 15.0
    TT40225

--- delivery_multi_destination/__manifest__.py
+++ delivery_multi_destination/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2016-2019 Tecnativa - Pedro M. Baeza
-# Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
+# Copyright 2017 Tecnativa - Luis M. Ontalba
 # Copyright 2021 Gianmarco Conte <gconte@dinamicheaziendali.it>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Multiple destinations for the same delivery method",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["delivery"],
