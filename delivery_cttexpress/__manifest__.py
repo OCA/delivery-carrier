191f0e5e08900d8e39855fed707676aa2162f5b5
David <david.vidal@tecnativa.com>
2022-11-08 10:47:45 +0100

    [MIG] delivery_cttexpress: migration to 13.0
    TT39099

--- delivery_cttexpress/__manifest__.py
+++ delivery_cttexpress/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Delivery CTT Express",
     "summary": "Delivery Carrier implementation for CTT Express API",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
     "author": "Tecnativa, Odoo Community Association (OCA)",
