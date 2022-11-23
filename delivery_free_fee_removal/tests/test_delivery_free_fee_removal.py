f9a6d4f02554f4173dfcef4cf5c41536c32ca5e9
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-23 15:46:38 +0100

    [MIG] delivery_free_fee_removal: Migration to 15.0
    TT36945

--- delivery_free_fee_removal/tests/test_delivery_free_fee_removal.py
+++ delivery_free_fee_removal/tests/test_delivery_free_fee_removal.py
@@ -1,24 +1,27 @@
-# Copyright 2019 Vicent Cubells <vicent.cubells@tecnativa.com>
+# Copyright 2019 Tecnativa - Vicent Cubells
+# Copyright 2022 Tecnativa - Víctor Martínez
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestDeliveryFreeFeeRemoval(SavepointCase):
+class TestDeliveryFreeFeeRemoval(TransactionCase):
     @classmethod
     def setUpClass(cls):
-        super(TestDeliveryFreeFeeRemoval, cls).setUpClass()
-
+        super().setUpClass()
         product = cls.env["product.product"].create(
-            {"name": "Product", "type": "product"}
+            {"name": "Product", "detailed_type": "product"}
         )
         product_delivery = cls.env["product.product"].create(
-            {"name": "Delivery Product", "type": "service"}
+            {
+                "name": "Delivery Product",
+                "detailed_type": "service",
+                "invoice_policy": "delivery",
+            }
         )
-
         cls.delivery = cls.env["delivery.carrier"].create(
             {
-                "name": "Delivery",
+                "name": "Test Delivery",
                 "delivery_type": "fixed",
                 "fixed_price": 10,
                 "free_over": True,
@@ -43,6 +46,7 @@ class TestDeliveryFreeFeeRemoval(SavepointCase):
                 ],
             }
         )
+        cls.report_obj = cls.env["ir.actions.report"]
 
     def test_delivery_free_fee_removal_with_fee(self):
         self.delivery.product_id.write({"invoice_policy": "order"})
@@ -60,6 +64,10 @@ class TestDeliveryFreeFeeRemoval(SavepointCase):
                 }
             ],
         )
+        res = self.report_obj._get_report_from_name(
+            "sale.report_saleorder"
+        )._render_qweb_text(self.sale.ids, False)
+        self.assertRegex(str(res[0]), "Test Delivery")
 
     def test_delivery_free_fee_removal_with_fee_invoice_policy_delivery(self):
         self.sale.set_delivery_line(self.delivery, 100)
@@ -77,6 +85,10 @@ class TestDeliveryFreeFeeRemoval(SavepointCase):
                 }
             ],
         )
+        res = self.report_obj._get_report_from_name(
+            "sale.report_saleorder"
+        )._render_qweb_text(self.sale.ids, False)
+        self.assertRegex(str(res[0]), "Test Delivery")
 
     def test_delivery_free_fee_removal_free_fee(self):
         self.sale.set_delivery_line(self.delivery, 0)
@@ -93,6 +105,10 @@ class TestDeliveryFreeFeeRemoval(SavepointCase):
                 }
             ],
         )
+        res = self.report_obj._get_report_from_name(
+            "sale.report_saleorder"
+        )._render_qweb_text(self.sale.ids, False)
+        self.assertNotRegex(str(res[0]), "Test Delivery")
 
     def test_delivery_free_fee_removal_free_fee_invoice_policy_order(self):
         self.delivery.product_id.write({"invoice_policy": "order"})
@@ -110,3 +126,7 @@ class TestDeliveryFreeFeeRemoval(SavepointCase):
                 }
             ],
         )
+        res = self.report_obj._get_report_from_name(
+            "sale.report_saleorder"
+        )._render_qweb_text(self.sale.ids, False)
+        self.assertNotRegex(str(res[0]), "Test Delivery")
