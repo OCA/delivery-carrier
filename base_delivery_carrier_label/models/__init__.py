84090726332f95832c8c4edd1b830a85d13a3782
Florian da Costa <florian.dacosta@akretion.com>
2022-11-29 14:45:27 +0100

    [16.0][MIG] Extract delivery_carrier_account from base_delivery_carrier_label and migrate base_delivery_carrier_label to 16

--- base_delivery_carrier_label/models/__init__.py
+++ base_delivery_carrier_label/models/__init__.py
@@ -5,4 +5,3 @@ from . import stock_move_line
 from . import stock_picking
 from . import stock_quant_package
 from . import shipping_label
-from . import carrier_account
--- base_delivery_carrier_label/tests/test_helper_functions.py
+++ base_delivery_carrier_label/tests/test_helper_functions.py
@@ -8,53 +8,11 @@ from odoo.tests.common import TransactionCase
 class TestHelperFunctions(TransactionCase):
     """Test convenience functions on stock.picking"""
 
-    def test_get_carrier_account(self):
-        """Test finding the correct account for a picking"""
-        account_without_company = self.env["carrier.account"].create(
-            {
-                "name": "Account without company",
-                "account": "account",
-                "password": "password",
-                "delivery_type": "fixed",
-                "sequence": 1,
-            }
-        )
-        account_with_company = account_without_company.copy(
-            default=dict(
-                name="Account with company",
-                company_id=self.env.user.company_id.id,
-                sequence=2,
-            ),
-        )
-        second_account_with_company = account_with_company.copy(
-            default=dict(name="2. Account with company", sequence=-1),
-        )
-        carrier_id = self.env.ref("delivery.normal_delivery_carrier").id
-        pick = self.env["stock.picking"].new(
-            dict(
-                carrier_id=carrier_id,
-                company_id=self.env.user.company_id.id,
-            )
-        )
-
-        account = pick._get_carrier_account()
-        self.assertEqual(account, second_account_with_company)
-
-        # restrict second_account_with_company to another delivery method
-        free_method = self.env.ref("delivery.free_delivery_carrier")
-        second_account_with_company.write({"carrier_ids": [(6, 0, [free_method.id])]})
-        account = pick._get_carrier_account()
-        self.assertEqual(account, account_with_company)
-
-        second_account_with_company.write({"carrier_ids": [(4, carrier_id, 0)]})
-        account = pick._get_carrier_account()
-        self.assertEqual(account, second_account_with_company)
-
     def test_attach_shipping_label(self):
         """Test if attaching labels works correctly"""
         picking = self.env["stock.picking"].new(
             dict(
-                carrier_id=self.env.ref("delivery.normal_delivery_carrier").id,
+                carrier_id=self.env.ref("delivery.delivery_carrier").id,
                 company_id=self.env.user.company_id.id,
             )
         )
