96a1af821e9ee9059579e93a3d732819fe019f67
Florian da Costa <florian.dacosta@akretion.com>
2022-12-11 20:18:09 +0100

    [16][MIG] Migrate carrier_account_environment to v16

--- carrier_account_environment/__manifest__.py
+++ carrier_account_environment/__manifest__.py
@@ -1,9 +1,8 @@
-# Copyright 2019 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Carrier accounts configuration with server_environment",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Tools",
     "summary": "Configure carriers with server_environment_files",
     "maintainers": ["florian-dacosta"],
@@ -12,6 +11,6 @@
     "website": "https://github.com/OCA/delivery-carrier",
     "depends": [
         "server_environment",
-        "base_delivery_carrier_label",
+        "delivery_carrier_account",
     ],
 }
