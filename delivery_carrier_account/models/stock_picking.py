254d2707fce13860f128e7935fabbca431eee265
Florian da Costa <florian.dacosta@akretion.com>
2022-12-16 13:43:02 +0100

    [REF] Refactore delivery_carrier_account so simplify UI

--- delivery_carrier_account/models/stock_picking.py
+++ delivery_carrier_account/models/stock_picking.py
@@ -7,22 +7,13 @@ from odoo import models
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def _get_carrier_account_domain(self):
-        return [
-            ("delivery_type", "in", self.mapped("delivery_type")),
-            "|",
-            ("company_id", "=", False),
-            ("company_id", "in", self.mapped("company_id.id")),
-            "|",
-            ("carrier_ids", "=", False),
-            ("carrier_ids", "in", self.mapped("carrier_id").ids),
-        ]
-
+    # All carrier module implementation should use this method in order to get the
+    # carrier account, instead of doing directly "picking.carrier_id.carrier_account_id"
+    # this way, it leaves the possibility to override globally (for all carriers)
+    # the way to get the carrier account.
+    # For instance, if a case requires to have multiple carrier account per delivery
+    # method, using a method leaves the possibility to implements the multiple
+    # account logic and make it work for all carrier modules installed.
     def _get_carrier_account(self):
-        """Return a carrier suitable for the current picking"""
-        domain = self._get_carrier_account_domain()
-        return self.env["carrier.account"].search(
-            domain,
-            limit=1,
-            order="company_id asc, sequence asc",
-        )
+        self.ensure_one()
+        return self.carrier_id.carrier_account_id
