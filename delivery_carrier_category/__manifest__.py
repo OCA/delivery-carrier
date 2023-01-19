924edbf34c8c8d2a95e00c160be2168e713ffd11
Pere Martínez <84047887+pere-aquarian@users.noreply.github.com>
2023-01-19 07:52:09 +0100

    Version fix

--- delivery_carrier_category/__manifest__.py
+++ delivery_carrier_category/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """
         Adds a category to delivery carriers in order to help users
         classifying them""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/delivery-carrier",
