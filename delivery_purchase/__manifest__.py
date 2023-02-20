d24792a09d595b7e6d31224841f685e20fefd05f
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-20 10:05:54 +0100

    [MIG] delivery_purchase: Migration to 15.0
    TT41587

--- delivery_purchase/__manifest__.py
+++ delivery_purchase/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Delivery costs in purchases",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Operations/Purchase",
     "website": "https://github.com/OCA/delivery-carrier",
