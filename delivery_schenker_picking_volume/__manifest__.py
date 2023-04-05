450291ddc8c75b6e4adbfca6b78dde64850e58d2
Michael Tietz <mtietz@mt-software.de>
2023-04-05 16:23:11 +0200

    [ADD] delivery_schenker_picking_volume: glue module between delivery_schenker and stock_picking_volume

---
+++ delivery_schenker_picking_volume/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Delivery Schenker Picking Volume",
+    "summary": "Glue module between delivery_schenker and stock_picking_volume"
+    "With this module the transmitted volume is changed,"
+    "it uses the computed volume from stock_picking_volume",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "MT Software, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["delivery_schenker", "stock_picking_volume"],
+    "auto_install": True,
+}
---
+++ delivery_schenker_picking_volume/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This is a glue module between delivery_schenker and stock_picking_volume
+With this module the transmitted volume is changed, it uses the computed volume from stock_picking_volume
