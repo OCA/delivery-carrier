0e0fa7e5521fa4de9c444db650426bca4a886b2e
ferran-73 <ferran@studio73.es>
2023-04-18 10:44:24 +0200

    [16.0][MIG] delivery_schenker

--- delivery_schenker/__manifest__.py
+++ delivery_schenker/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright 2021 Tecnativa - David Vidal
 # Copyright 2021 Studio73 - Ethan Hildick <ethan@studio73.es>
+# Copyright 2023 Studio73 - Ferran Mora <ferran@studio73.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Delivery Schenker",
     "summary": "Delivery Carrier implementation for DB Schenker API",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/delivery-carrier",
     "author": "Tecnativa, Studio73, Odoo Community Association (OCA)",
