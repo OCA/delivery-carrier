bdc0d3424b1940c5f7b7634655aedb6a7716eee8
Weblate <noreply@weblate.org>
2023-06-23 19:24:32 +0000

    Update translation files
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: delivery-carrier-14.0/delivery-carrier-14.0-delivery_schenker
    Translate-URL: https://translation.odoo-community.org/projects/delivery-carrier-14-0/delivery-carrier-14-0-delivery_schenker/

---
+++ delivery_carrier_default_tracking_url/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Delivery Carrier Default Tracking Url",
+    "summary": """
+        Adds the default tracking url on delivery carrier""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "maintainers": ["rousseldenis"],
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "depends": ["delivery"],
+    "data": ["views/delivery_carrier.xml", "views/stock_picking.xml"],
+}
---
+++ delivery_carrier_default_tracking_url/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to define the URL for the default tracking page of the
+carrier.
