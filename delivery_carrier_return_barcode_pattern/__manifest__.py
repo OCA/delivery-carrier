1228f683a2f14489fc5730a75178e4c801c94b08
Mmequignon <matthieu@fwzte.xyz>
2023-07-03 17:07:46 +0200

    Add delivery_carrier_return_barcode_pattern

---
+++ delivery_carrier_return_barcode_pattern/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Delivery Carrier Return Barcode Pattern",
+    "summary": "Allow to define a return barcode pattern on delivery methods",
+    "version": "14.0.1.0.0",
+    "category": "Delivery",
+    "website": "https://github.com/OCA/delivery-carrier",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "maintainers": ["mmequignon"],
+    "license": "AGPL-3",
+    "auto_install": False,
+    "depends": ["delivery"],
+    "data": ["views/delivery_carrier.xml"],
+}
---
+++ delivery_carrier_return_barcode_pattern/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Adds a `return_barcode_pattern` field on carriers, allowing to parse the origin from the provided barcode.
