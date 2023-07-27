68db8badfe99107922b5f84dca1716e364ba5336
Michael Tietz <mtietz@mt-software.de>
2023-07-27 20:42:57 +0200

    [ADD] delivery_packaging_archive: Add an active field on delivery packaging

---
+++ delivery_packaging_archive/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Delivery Packaging Archive",
+    "summary": """
+        This module allows archiving a delivery packaging""",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "MT Software, Odoo Community Association (OCA)",
+    "maintainers": ["mt-software-de"],
+    "depends": ["delivery"],
+    "data": [
+        "views/product_packaging_views.xml",
+    ],
+    "website": "https://github.com/OCA/delivery-carrier",
+}
---
+++ delivery_packaging_archive/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows archiving a delivery packaging.
