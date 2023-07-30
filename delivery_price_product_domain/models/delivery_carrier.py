9c4865fb607ce26e6a36fced19f381d09ed55fef
Andrey <andrejsolodovnikov699@gmail.com>
2023-07-30 15:12:31 +0300

    [14.0][IMP] delivery_price_product_domain: add order_id field in delivery.carrier model
    Add new field and rewrite code for this change

--- delivery_price_product_domain/models/delivery_carrier.py
+++ delivery_price_product_domain/models/delivery_carrier.py
@@ -1,16 +1,19 @@
 import ast
 
-from odoo import models
+from odoo import fields, models
 from odoo.tools.safe_eval import safe_eval
 
 
 class DeliveryPriceRule(models.Model):
     _inherit = "delivery.carrier"
 
-    def recompute_price_available(self, order, exclude_product):
+    order_id = fields.Many2one("sale.order", string="Sale Order")
+
+    def recompute_price_available(self, exclude_product):
         """This method recompute total parameters of order without exclude products"""
         self.ensure_one()
         self = self.sudo()
+        order = self.order_id
         total = weight = volume = quantity = 0
         total_delivery = 0.0
         for line in order.order_line:
@@ -35,9 +38,7 @@ class DeliveryPriceRule(models.Model):
         price_dict = self._get_price_dict(total, weight, volume, quantity)
         return price_dict
 
-    def _get_price_from_picking(
-        self, total, weight, volume, quantity, order_line=False
-    ):
+    def _get_price_from_picking(self, total, weight, volume, quantity):
         price_dict = self._get_price_dict(total, weight, volume, quantity)
         if self.free_over and total >= self.amount:
             return 0
@@ -47,13 +48,11 @@ class DeliveryPriceRule(models.Model):
             )
             if test:
                 exclude_product_domain_char = line.exclude_product_domain
-                if exclude_product_domain_char and order_line:
-                    exclude_product = order_line.product_id.search(
+                if exclude_product_domain_char and self.order_id:
+                    exclude_product = self.order_id.order_line.product_id.search(
                         ast.literal_eval(exclude_product_domain_char)
                     )
-                    price_dict = self.recompute_price_available(
-                        order_line.order_id, exclude_product
-                    )
+                    price_dict = self.recompute_price_available(exclude_product)
                 break
 
         return super(DeliveryPriceRule, self)._get_price_from_picking(
@@ -64,30 +63,5 @@ class DeliveryPriceRule(models.Model):
         )
 
     def _get_price_available(self, order):
-        self.ensure_one()
-        self = self.sudo()
-        order = order.sudo()
-        total = weight = volume = quantity = 0
-        total_delivery = 0.0
-        for line in order.order_line:
-            if line.state == "cancel":
-                continue
-            if line.is_delivery:
-                total_delivery += line.price_total
-            if not line.product_id or line.is_delivery:
-                continue
-            if line.product_id.type == "service":
-                continue
-            qty = line.product_uom._compute_quantity(
-                line.product_uom_qty, line.product_id.uom_id
-            )
-            weight += (line.product_id.weight or 0.0) * qty
-            volume += (line.product_id.volume or 0.0) * qty
-            quantity += qty
-        total = (order.amount_total or 0.0) - total_delivery
-
-        total = self._compute_currency(order, total, "pricelist_to_company")
-
-        return self._get_price_from_picking(
-            total, weight, volume, quantity, order.order_line
-        )
+        self.order_id = order
+        return super(DeliveryPriceRule, self)._get_price_available(order)
