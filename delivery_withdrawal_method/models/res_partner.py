574b066267886954ebda43fb036fa3a9e09ee270
Romain <73525560+Rom10811@users.noreply.github.com>
2023-08-23 23:20:26 +0200

    [IMP] Add res partner street2

--- delivery_withdrawal_method/models/res_partner.py
+++ delivery_withdrawal_method/models/res_partner.py
@@ -8,9 +8,9 @@ class ResPartnerWithdrawal(models.Model):
     _inherit = 'res.partner'
     @api.model
     def _withdrawal_search_or_create(self, data):
+        print("data create", data)
         partner = self.search([
             ('id', 'child_of', self.commercial_partner_id.ids),
-            # fast check that address always the same
             ('city', '=', data['city']),
             ('street', '=', data['street']),
             ('zip', '=', data['zip']),
@@ -19,10 +19,13 @@ class ResPartnerWithdrawal(models.Model):
             partner = self.create({
                 'name': data['name'],
                 'street': data['street'],
+                'street2': data['street2'],
                 'zip': data['zip'],
                 'city': data['city'],
                 'country_id': self.env['res.country'].search([('id', '=', data['country_id'])]).id,
                 'type': 'delivery',
                 'parent_id': self.id,
             })
+        else:
+            partner.street2 = data['street2']
         return partner
