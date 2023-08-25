23442972d3fcd3c8d9e62529c0bd0124582907de
Sébastien Alix <sebastien.alix@camptocamp.com>
2023-08-25 17:41:15 +0200

    delivery_postlogistics: improve rendering of error messages

--- delivery_postlogistics/models/stock_picking.py
+++ delivery_postlogistics/models/stock_picking.py
@@ -3,7 +3,9 @@
 import base64
 from operator import attrgetter
 
-from odoo import _, exceptions, fields, models
+import lxml.html
+
+from odoo import _, api, exceptions, fields, models
 
 from ..postlogistics.web_service import PostlogisticsWebService
 
@@ -231,9 +233,21 @@ class StockPicking(models.Model):
             # Commit the change to save the changes,
             # This ensures the label pushed recored correctly in Odoo
             self._cr.commit()  # pylint: disable=invalid-commit
-            error_message = "\n".join(label["errors"] for label in failed_label_results)
-            raise exceptions.UserError(error_message)
+            error_message = "\n".join(
+                self._cleanup_error_message(label["errors"])
+                for label in failed_label_results
+            )
+            raise exceptions.UserError(
+                _("PostLogistics error:") + "\n\n" + error_message
+            )
         return labels
+
+    @api.model
+    def _cleanup_error_message(self, error_message):
+        """Cleanup HTML error message to be readable by users."""
+        texts_no_html = lxml.html.fromstring(error_message).text_content()
+        texts = [text for text in texts_no_html.split("\n") if text]
+        return "\n".join(texts)
 
     def generate_postlogistics_shipping_labels(self, package_ids=None):
         """Add label generation for PostLogistics"""
