8a648d6a3767b930ff26d8d9d55a76d96c54c87b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-09-07 08:44:57 +0200

    [FIX] delivery_package_number: Don't reset number on backorder
    Fixes #676

--- delivery_package_number/models/stock_picking.py
+++ delivery_package_number/models/stock_picking.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Tecnativa - David Vidal
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
 
@@ -11,9 +12,12 @@ class StockPicking(models.Model):
         compute="_compute_number_of_packages",
         readonly=False,
         store=True,
+        default=0,
+        copy=False,
     )
 
     @api.depends("package_ids")
     def _compute_number_of_packages(self):
         for picking in self:
-            picking.number_of_packages = len(picking.package_ids) or 1
+            if picking.package_ids:
+                picking.number_of_packages = len(picking.package_ids)
