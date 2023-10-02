e9bedd655ac17d5cfd80e2dcf4f2172320743b00
angel <angel.garcia@sygel.com>
2023-10-02 15:22:00 +0200

    [FIX] delivery_package_number: inhibit auto-create packets from base_delivery_carrier_label module

--- delivery_package_number/wizard/stock_backorder_confirmation.py
+++ delivery_package_number/wizard/stock_backorder_confirmation.py
@@ -15,9 +15,16 @@ class StockBackorderConfirmation(models.TransientModel):
     @api.depends("pick_ids")
     def _compute_ask_number_of_packages(self):
         for item in self:
-            item.ask_number_of_packages = bool(item.pick_ids.carrier_id)
+            # we use ._origin because if not, a NewId is used for the checks and the returned
+            # value of package_ids is wrong.
+            item.ask_number_of_packages = bool(
+                item.pick_ids.carrier_id and not item.pick_ids._origin.package_ids
+            )
 
     def process(self):
         if self.number_of_packages:
             self.pick_ids.write({"number_of_packages": self.number_of_packages})
-        return super().process()
+        # put context key for avoiding `base_delivery_carrier_label` auto-packaging feature
+        return super(
+            StockBackorderConfirmation, self.with_context(set_default_package=False)
+        ).process()
