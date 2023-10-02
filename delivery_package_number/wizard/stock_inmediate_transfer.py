9a852d10d00e275ba93c00efc50589bbcafcce0c
angel <angel.garcia@sygel.com>
2023-10-02 15:22:00 +0200

    [FIX] delivery_package_number: inhibit auto-create packets from base_delivery_carrier_label module

--- delivery_package_number/wizard/stock_inmediate_transfer.py
+++ delivery_package_number/wizard/stock_inmediate_transfer.py
@@ -14,9 +14,16 @@ class StockImmediateTransfer(models.TransientModel):
     @api.depends("pick_ids")
     def _compute_ask_number_of_packages(self):
         for item in self:
-            item.ask_number_of_packages = bool(item.pick_ids.carrier_id)
+            # we use ._origin because if not, a NewId is used for the checks and the returned
+            # value of package_ids is wrong.
+            item.ask_number_of_packages = bool(
+                item.pick_ids.carrier_id and not item.pick_ids._origin.package_ids
+            )
 
     def process(self):
         if self.number_of_packages:
             self.pick_ids.write({"number_of_packages": self.number_of_packages})
-        return super().process()
+        # put context key for avoiding `base_delivery_carrier_label` auto-packaging feature
+        return super(
+            StockImmediateTransfer, self.with_context(set_default_package=False)
+        ).process()
