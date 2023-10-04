47b193049f7a0ae6cb76f0980fa833956daff3b2
Florian da Costa <florian.dacosta@akretion.com>
2023-10-04 12:30:03 +0200

    [FIX] Disable unwanted pylint warning

--- delivery_roulier/tests/models.py
+++ delivery_roulier/tests/models.py
@@ -1,6 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
+# pylint: disable=consider-merging-classes-inherited
+
 
 class FakeDeliveryCarrier(models.Model):
     _inherit = "delivery.carrier"
