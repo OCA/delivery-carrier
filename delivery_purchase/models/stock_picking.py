8b169f7e7dda17166e8977ac281f9d98c30f01cd
Víctor Martínez <victor.martinez@tecnativa.com>
2023-10-30 17:08:02 +0100

    [IMP] delivery_purchase: Add delivery purchase order lines from picking
    Auto-create (delivery) order lines when picking are marked as done
    
    TT45656

--- delivery_purchase/models/stock_picking.py
+++ delivery_purchase/models/stock_picking.py
@@ -45,14 +45,13 @@ class StockPicking(models.Model):
 
     def _add_delivery_cost_to_po(self):
         self.ensure_one()
-        if (
-            self.purchase_id
-            and self.carrier_id.invoice_policy == "real"
-            and self.carrier_price
-        ):
-            carrier_price = self.carrier_price * (
-                1.0 + (float(self.carrier_id.margin) / 100.0)
-            )
-            self.purchase_id.write(
-                {"carrier_id": self.carrier_id, "delivery_price": carrier_price}
-            )
+        if self.purchase_id and self.carrier_price:
+            carrier_price = self.carrier_price
+            # Re-set carrier price
+            if self.carrier_id.invoice_policy == "real":
+                carrier_price = self.carrier_price * (
+                    1.0 + (float(self.carrier_id.margin) / 100.0)
+                )
+            # Create delivery line allways
+            self.purchase_id._create_delivery_line(self.carrier_id, carrier_price)
+            self.purchase_id.write({"carrier_id": self.carrier_id})
