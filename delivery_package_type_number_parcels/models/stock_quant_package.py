95606ffb0f721ceef07b71e4b0506248da6daa97
sbejaoui <souheil.bejaoui@acsone.eu>
2024-01-08 21:55:39 +0100

    [FIX] delivery_package_type_number_parcels: make number of parcels computed
    avoid related for this field as we don't want to change the pakcage type
    value when it changes in the package

--- delivery_package_type_number_parcels/models/stock_quant_package.py
+++ delivery_package_type_number_parcels/models/stock_quant_package.py
@@ -1,13 +1,17 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockQuantPackage(models.Model):
     _inherit = "stock.quant.package"
 
     number_of_parcels = fields.Integer(
-        related="package_type_id.number_of_parcels",
-        store=True,
+        compute="_compute_number_of_parcels", store=True, readonly=False
     )
+
+    @api.depends("package_type_id")
+    def _compute_number_of_parcels(self):
+        for rec in self:
+            rec.number_of_parcels = rec.package_type_id.number_of_parcels
