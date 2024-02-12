ae9e18176406b0066ce7266f445a0d7856732b95
Víctor Martínez <victor.martinez@tecnativa.com>
2024-02-12 13:26:53 +0100

    [MIG] partner_delivery_schedule: Migration to 16.0
    TT47628

--- partner_delivery_schedule/tests/test_partner_delivery_schedule.py
+++ partner_delivery_schedule/tests/test_partner_delivery_schedule.py
@@ -1,23 +1,15 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.exceptions import ValidationError
-from odoo.tests import Form, TransactionCase
+from odoo.tests import Form
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestStockPikingReturnRefundOption(TransactionCase):
+class TestStockPikingReturnRefundOption(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.env = cls.env(
-            context=dict(
-                cls.env.context,
-                mail_create_nolog=True,
-                mail_create_nosubscribe=True,
-                mail_notrack=True,
-                no_reset_password=True,
-                tracking_disable=True,
-            )
-        )
         cls.schedule = cls.env["delivery.schedule"].create(
             {
                 "name": "test",
@@ -66,16 +58,13 @@ class TestStockPikingReturnRefundOption(TransactionCase):
 
     def test_partner_schedule_name(self):
         self.assertEqual(self.schedule.name_get()[0][1], "08:00-10:00 (Mo, Tu)")
-
         day_update = {}
         for day in self.schedule._days_of_week():
             day_update[day[0]] = True
         self.schedule.update(day_update)
         self.assertEqual(self.schedule.name_get()[0][1], "08:00-10:00 (All days)")
-
         with self.assertRaises(ValidationError):
             self.schedule.update({"hour_from": 0, "hour_to": 25})
-
         day_update = {}
         for day in self.schedule._days_of_week():
             day_update[day[0]] = False
@@ -87,7 +76,6 @@ class TestStockPikingReturnRefundOption(TransactionCase):
         self.assertTrue(self.partner.allow_delivery_date("2018-09-04 09:00:00"))
         self.assertFalse(self.partner.allow_delivery_date("2018-09-04 12:01:00"))
         self.assertFalse(self.partner.allow_delivery_date("2018-09-05 10:01:00"))
-
         # Allow delivery in all days
         day_update = {}
         for day in self.schedule._days_of_week():
@@ -96,15 +84,15 @@ class TestStockPikingReturnRefundOption(TransactionCase):
         self.assertTrue(self.partner.allow_delivery_date("2018-09-09 09:00:00"))
 
     def test_report_picking(self):
-        res = self.report_model._get_report_from_name(
-            "stock.report_picking"
-        )._render_qweb_text(self.picking.ids, False)
+        res = self.report_model._render_qweb_html(
+            "stock.report_picking", self.picking.ids, False
+        )
         self.assertRegex(str(res[0]), "08:00-10:00")
         self.assertRegex(str(res[0]), "10:00-12:00")
 
     def test_report_deliveryslip(self):
-        res = self.report_model._get_report_from_name(
-            "stock.report_deliveryslip"
-        )._render_qweb_text(self.picking.ids, False)
+        res = self.report_model._render_qweb_html(
+            "stock.report_deliveryslip", self.picking.ids, False
+        )
         self.assertRegex(str(res[0]), "08:00-10:00")
         self.assertRegex(str(res[0]), "10:00-12:00")
