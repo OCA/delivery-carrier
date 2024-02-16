2c53fe8e184fa17a33333ed8ef9afa8780afb472
chafique.delli <chafique.delli@akretion.com>
2024-02-16 16:25:39 +0100

    [IMP] delivery_dropoff_site: black, isort, prettier

--- delivery_dropoff_site/models/dropoff_site.py
+++ delivery_dropoff_site/models/dropoff_site.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # Copyright (C) 2014 - Today: Akretion (http://www.akretion.com)
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # @author Aymeric Lecomte <aymeric.lecomte@akretion.com>
@@ -10,39 +9,47 @@ from odoo import api, fields, models
 
 
 class DropoffSite(models.Model):
-    _name = 'dropoff.site'
-    _inherits = {'res.partner': 'partner_id'}
+    _name = "dropoff.site"
+    _inherits = {"res.partner": "partner_id"}
     _order = "code, name"
 
-    code = fields.Char(string='Code')
+    code = fields.Char(string="Code")
 
     partner_id = fields.Many2one(
-        comodel_name='res.partner', string='Partner', required=True,
-        ondelete='cascade')
+        comodel_name="res.partner", string="Partner", required=True, ondelete="cascade"
+    )
 
     carrier_id = fields.Many2one(
-        comodel_name='delivery.carrier', string='Delivery Method',
-        required=True, domain="[('with_dropoff_site', '=', True)]")
+        comodel_name="delivery.carrier",
+        string="Delivery Method",
+        required=True,
+        domain="[('with_dropoff_site', '=', True)]",
+    )
 
     calendar_id = fields.Many2one(
-        comodel_name='resource.calendar', string='Calendar', copy=False)
+        comodel_name="resource.calendar", string="Calendar", copy=False
+    )
 
     attendance_ids = fields.One2many(
-        'resource.calendar.attendance', related='calendar_id.attendance_ids',
-        string='Working Time')
+        "resource.calendar.attendance",
+        related="calendar_id.attendance_ids",
+        string="Working Time",
+    )
 
     # Inherit part
     @api.model
     def create(self, vals):
-        vals.update({
-            'customer': False,
-            'supplier': False,
-        })
+        vals.update(
+            {
+                "customer": False,
+                "supplier": False,
+            }
+        )
         return super(DropoffSite, self).create(vals)
 
     @api.multi
     def unlink(self):
-        self.mapped('calendar_id').unlink()
+        self.mapped("calendar_id").unlink()
         return super(DropoffSite, self).unlink()
 
     # Action Part
@@ -50,12 +57,11 @@ class DropoffSite(models.Model):
     def action_enable_calendar(self):
         for site in self:
             if not site.calendar_id:
-                site.calendar_id = site.calendar_id.create(
-                    site._prepare_calendar_id())
+                site.calendar_id = site.calendar_id.create(site._prepare_calendar_id())
 
     @api.multi
     def action_disable_calendar(self):
-        self.mapped('calendar_id').unlink()
+        self.mapped("calendar_id").unlink()
 
     @api.multi
     def geo_localize(self):
@@ -66,4 +72,4 @@ class DropoffSite(models.Model):
     @api.multi
     def _prepare_calendar_id(self):
         self.ensure_one()
-        return {'name': self.name}
+        return {"name": self.name}
