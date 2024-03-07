94430fd9f118ee0aabb50a8c5684539a3ccfbbce
Florian Mounier <paradoxxx.zero@gmail.com>
2024-03-07 09:01:57 +0100

    [IMP] delivery_carrier_label_gls: black, isort, prettier

--- delivery_carrier_label_gls/__manifest__.py
+++ delivery_carrier_label_gls/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Delivery Carrier Label GLS",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Acsone,Akretion,Odoo Community Association (OCA)",
     "maintener": "Akretion",
     "category": "Warehouse",
