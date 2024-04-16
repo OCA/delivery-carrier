97a66ec22a4f2f5e2db4afa412e5c7d9d90abec8
Jacques-Etienne Baudoux <je@bcim.be>
2024-04-16 23:01:23 +0200

    delivery_auto_refresh: set carrier

--- delivery_auto_refresh/models/res_config_settings.py
+++ delivery_auto_refresh/models/res_config_settings.py
@@ -7,16 +7,12 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    set_default_carrier = fields.Boolean(
-        "Set Default Carrier Automatically",
-        config_parameter="delivery_auto_refresh.set_default_carrier",
-    )
     auto_add_delivery_line = fields.Boolean(
-        "Add Delivery Line Automatically",
+        "Refresh shipping cost line automatically",
         config_parameter="delivery_auto_refresh.auto_add_delivery_line",
     )
     refresh_after_picking = fields.Boolean(
-        "Refresh After Picking Automatically",
+        "Refresh after picking automatically",
         config_parameter="delivery_auto_refresh.refresh_after_picking",
     )
     auto_void_delivery_line = fields.Boolean(
