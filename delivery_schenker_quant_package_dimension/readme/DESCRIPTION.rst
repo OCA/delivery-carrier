69a165d5ab422b2125607e65a380eb8041574e69
Michael Tietz <mtietz@mt-software.de>
2024-04-22 02:04:39 +0200

    [ADD] delivery_schenker_quant_package_dimension: Glue addon between delivery_schenker and stock_quant_package_dimension Using the package.volume and all dimension to transmit to schenker

---
+++ delivery_schenker_quant_package_dimension/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This addon is a glue module between delivery_schenker and stock_quant_package_dimension.
+It is changing the transmitted package volume, it uses the computed volume from stock_quant_package_dimension.
+Also the dimensions length, width and height of a package will be added to the request.
---
+++ delivery_schenker_quant_package_dimension/tests/test_delivery_schenker.py
@@ -0,0 +1,31 @@
+# Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo.addons.delivery_schenker.tests.common import TestDeliverySchenkerCommon
+
+
+class TestDeliverySchenker(TestDeliverySchenkerCommon):
+    def test_delivery_schenker(self):
+        _sale_order, picking = self._create_sale_order()
+        self._process_picking(picking, with_package=True)
+        length = 14.0
+        width = 13.0
+        height = 12.0
+        volume = length * width * height
+        package = picking.package_ids
+        package.pack_length = length
+        package.width = width
+        package.height = height
+        expected_vals = self._prepare_schenker_shipping(picking)
+        expected_vals["shippingInformation"]["shipmentPosition"][0].update(
+            {
+                "cargoDesc": " / ".join([picking.name, package.name]),
+                "length": length,
+                "width": width,
+                "height": height,
+                "volume": volume,
+            }
+        )
+        expected_vals["shippingInformation"]["volume"] = volume
+        expected_vals["measureUnitVolume"] = volume
+        vals = self.carrier._prepare_schenker_shipping(picking)
+        self.assertDictEqual(expected_vals, vals)
