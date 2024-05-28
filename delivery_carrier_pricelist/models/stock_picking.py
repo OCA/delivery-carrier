a6f3e4e97b8f96c606067c6b52b5d5008a53db14
Zina Rasoamanana <zina.rasoamanana@acsone.eu>
2024-05-28 17:58:22 +0200

    [16.0][MIG] delivery_carrier_pricelist: migration to version 16.0

--- delivery_carrier_pricelist/models/stock_picking.py
+++ delivery_carrier_pricelist/models/stock_picking.py
@@ -1,39 +1,36 @@
 # Copyright 2020 Camptocamp
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from lxml import etree
 
-from odoo import models
+from odoo import api, models
 from odoo.osv import expression
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    def fields_view_get(
-        self, view_id=None, view_type="form", toolbar=False, submenu=False
-    ):
-        result = super().fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+    @api.model
+    def _get_view(self, view_id=None, view_type="form", **options):
+        arch, view = super()._get_view(
+            view_id=view_id, view_type=view_type, options=options
         )
-        if result.get("name") == "stock.picking.form":
-            result["arch"] = self._fields_view_get_adapt_attrs(result["arch"])
-        return result
+        if view.name == "stock.picking.form":
+            arch = self._fields_view_get_adapt_attrs(arch)
+        return arch, view
 
     def _fields_view_get_adapt_attrs(self, view_arch):
-        doc = etree.XML(view_arch)
         # hide all these fields and buttons for delivery providers which have already
         # an attrs with a domain we can't extend...
         self.env["delivery.carrier"]._add_pricelist_domain(
-            doc, "//button[@name='cancel_shipment']", "invisible"
+            view_arch, "//button[@name='cancel_shipment']", "invisible"
         )
         self.env["delivery.carrier"]._add_pricelist_domain(
-            doc, "//button[@name='send_to_shipper']", "invisible"
+            view_arch, "//button[@name='send_to_shipper']", "invisible"
         )
         self.env["delivery.carrier"]._add_pricelist_domain(
-            doc,
+            view_arch,
             "//field[@name='partner_id']",
             "required",
             domain_operator=expression.AND,
             field_operator="!=",
         )
-        return etree.tostring(doc, encoding="unicode")
+        return view_arch
