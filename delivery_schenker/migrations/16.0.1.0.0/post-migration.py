702cbb60a9dc84ba17572b1903001ce3a036787f
pablo-marin-s73 <pablo.marin@studio73.es>
2024-06-25 16:54:01 +0200

    [IMP] delivery_schenker: pre-commit auto fixes

--- delivery_schenker/migrations/16.0.1.0.0/post-migration.py
+++ delivery_schenker/migrations/16.0.1.0.0/post-migration.py
@@ -7,12 +7,10 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     for i in range(1, 23):
-        xml_id = "delivery_schenker.schenker_packaging_{}".format(str(i).zfill(2))
+        xml_id = f"delivery_schenker.schenker_packaging_{str(i).zfill(2)}"
         pack = env.ref(xml_id, raise_if_not_found=False)
         if pack:
-            xml_id = "delivery_schenker.schenker_package_type_{}".format(
-                str(i).zfill(2)
-            )
+            xml_id = f"delivery_schenker.schenker_package_type_{str(i).zfill(2)}"
             package_type = env.ref(xml_id, raise_if_not_found=False)
             if package_type:
                 env.cr.execute(
