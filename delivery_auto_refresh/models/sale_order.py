4785c77d8d28b207cd74085e03ef2336e610ea32
Odoo SA <odoo@odoo.com>
2024-07-08 15:28:41 +0200

    delivery_auto_refresh: method copied from 16.0

--- delivery_auto_refresh/models/sale_order.py
+++ delivery_auto_refresh/models/sale_order.py
@@ -3,7 +3,7 @@
 # Copyright 2024 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class SaleOrder(models.Model):
@@ -78,6 +78,46 @@ class SaleOrder(models.Model):
         if self.env.context.get("website_id"):
             return False
         return self.company_id.sale_auto_add_delivery_line
+
+    def _prepare_delivery_line_vals(self, carrier, price_unit):
+        context = {}
+        if self.partner_id:
+            # set delivery detail in the customer language
+            context['lang'] = self.partner_id.lang
+            carrier = carrier.with_context(lang=self.partner_id.lang)
+
+        # Apply fiscal position
+        taxes = carrier.product_id.taxes_id.filtered(lambda t: t.company_id.id == self.company_id.id)
+        taxes_ids = taxes.ids
+        if self.partner_id and self.fiscal_position_id:
+            taxes_ids = self.fiscal_position_id.map_tax(taxes).ids
+
+        # Create the sales order line
+
+        if carrier.product_id.description_sale:
+            so_description = '%s: %s' % (carrier.name, carrier.product_id.description_sale)
+        else:
+            so_description = carrier.name
+        values = {
+            'order_id': self.id,
+            'name': so_description,
+            'product_uom_qty': 1,
+            'product_uom': carrier.product_id.uom_id.id,
+            'product_id': carrier.product_id.id,
+            'tax_id': [(6, 0, taxes_ids)],
+            'is_delivery': True,
+        }
+        if carrier.invoice_policy == 'real':
+            values['price_unit'] = 0
+            values['name'] += _(' (Estimated Cost: %s )', self._format_currency_amount(price_unit))
+        else:
+            values['price_unit'] = price_unit
+        if carrier.free_over and self.currency_id.is_zero(price_unit) :
+            values['name'] += '\n' + _('Free Shipping')
+        if self.order_line:
+            values['sequence'] = self.order_line[-1].sequence + 1
+        del context
+        return values
 
     def _update_delivery_line(self, delivery_line, price_unit):
         """Update the existing delivery line"""
