854b903ce6f9405a3a02a9f828fb5153e5e575a7
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2024-07-31 08:51:46 -0400

    [14.0][ADD] delivery_easypost_oca

--- delivery_easypost_oca/models/sale_order.py
+++ delivery_easypost_oca/models/sale_order.py
@@ -9,19 +9,8 @@ class SaleOrder(models.Model):
     easypost_oca_carrier_name = fields.Char(tracking=True, copy=False)
 
     def _create_delivery_line(self, carrier, price_unit):
-        sol = super()._create_delivery_line(carrier=carrier, price_unit=price_unit)
-        self.write(
-            {
-                "easypost_oca_rate_id": self.env.context.get(
-                    "easypost_oca_rate_id", False
-                ),
-                "easypost_oca_shipment_id": self.env.context.get(
-                    "easypost_oca_shipment_id", False
-                ),
-                "easypost_oca_carrier_name": self.env.context.get(
-                    "easypost_oca_carrier_name", False
-                ),
-            }
+        sol = super()._create_delivery_line(
+            carrier=carrier, price_unit=price_unit
         )
         carrier_name = (
             f" - {self.easypost_oca_carrier_name}"
