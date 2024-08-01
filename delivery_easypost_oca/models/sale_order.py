d72af7c5d69a63e9dde560b199298f91fd26e396
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2024-08-01 12:29:30 -0400

    [ADD] delivery_oca_easypost

--- delivery_easypost_oca/models/sale_order.py
+++ delivery_easypost_oca/models/sale_order.py
@@ -1,8 +1,12 @@
-from odoo import models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    easypost_oca_shipment_id = fields.Char(tracking=False, copy=False)
+    easypost_oca_rate_id = fields.Char(tracking=False, copy=False)
+    easypost_oca_carrier_name = fields.Char(tracking=True, copy=False)
 
     def _create_delivery_line(self, carrier, price_unit):
         sol = super()._create_delivery_line(carrier=carrier, price_unit=price_unit)
