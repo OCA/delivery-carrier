70ca6e0db189af875dd4ba020f1e331224afe341
Florian Mounier <paradoxxx.zero@gmail.com>
2024-09-17 11:43:52 +0200

    [IMP] delivery_roulier_dpd_fr: Backport fixes from 16.0 mig and use new dpd_fr instead of dpd_fr_soap

--- delivery_roulier_dpd_fr/models/carrier_account.py
+++ delivery_roulier_dpd_fr/models/carrier_account.py
@@ -3,11 +3,23 @@
 
 from odoo import fields, models
 
+try:
+    from roulier.carriers.dpd_fr.schema import Format
+except ImportError:
+    Format = None
+
 
 class CarrierAccount(models.Model):
     _inherit = "carrier.account"
 
-    dpd_fr_soap_customer_country = fields.Char()
-    dpd_fr_soap_customer_id = fields.Char()
-    dpd_fr_soap_agency_id = fields.Char()
-    dpd_fr_soap_file_format = fields.Selection([("ZPL", "ZPL"), ("PDF", "PDF")])
+    dpd_fr_customer_country = fields.Char(string="DPD Customer Country")
+    dpd_fr_customer_id = fields.Char(string="DPD Customer ID")
+    dpd_fr_agency_id = fields.Char(string="DPD Agency ID")
+    dpd_fr_file_format = fields.Selection(
+        (
+            [(format.value, format.value) for format in Format]
+            if Format
+            else [("ZPL", "ZPL"), ("PDF", "PDF")]
+        ),
+        string="DPD file format",
+    )
