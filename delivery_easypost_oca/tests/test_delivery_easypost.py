5108e98700bf4539ee45aa99d342114182cf3696
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2025-01-30 11:21:01 -0500

    [ADD][14.0] delivery_easypost_oca

--- delivery_easypost_oca/tests/test_delivery_easypost.py
+++ delivery_easypost_oca/tests/test_delivery_easypost.py
@@ -91,3 +91,57 @@ class TestDeliveryEasypost(EasypostTestBaseCase):
             )
         except UserError:
             self.assertTrue(1)
+
+    def test_easypost_oca_carrier_services(self):
+        """Test carrier services method returns False by default"""
+        SaleOrder = self._create_sale_order(10)
+        Picking = SaleOrder.picking_ids[0]
+
+        self.assertFalse(self.carrier._get_easypost_carrier_services())
+        self.assertFalse(self.carrier._get_easypost_carrier_services(Picking))
+
+    def test_easypost_oca_multiple_packages_shipping(self):
+        """Test shipping with multiple packages"""
+        SaleOrder = self._create_sale_order(10)
+        Picking = SaleOrder.picking_ids[0]
+        Picking.action_assign()
+
+        # Create two packages
+        Picking.move_lines[0].write({"quantity_done": 5})
+        self._put_in_pack(Picking)
+        Picking.move_lines[
+            0
+        ].move_line_ids.result_package_id.packaging_id = self.default_packaging.id
+        Picking.move_lines[0].move_line_ids.result_package_id.shipping_weight = 5.0
+
+        Picking.move_lines[0].write({"quantity_done": 5})
+        self._put_in_pack(Picking)
+        Picking.move_lines[
+            0
+        ].move_line_ids.result_package_id.packaging_id = self.default_packaging.id
+        Picking.move_lines[0].move_line_ids.result_package_id.shipping_weight = 5.0
+
+        self.assertEqual(len(Picking.package_ids), 2, "Should have created 2 packages")
+
+        try:
+            Picking._action_done()
+            self.assertGreater(
+                Picking.carrier_price,
+                0.0,
+                "Easypost carrying price should be positive for multiple packages",
+            )
+            self.assertTrue(
+                Picking.carrier_tracking_ref, "Should have tracking reference"
+            )
+        except UserError:
+            self.assertTrue(1)
+
+    def test_easypost_oca_shipping_error_handling(self):
+        """Test error handling during shipping"""
+        SaleOrder = self._create_sale_order(1)
+        Picking = SaleOrder.picking_ids[0]
+        # Force an error by setting invalid API key
+        self.carrier.easypost_oca_test_api_key = "invalid_key"
+
+        with self.assertRaises(UserError):
+            Picking._action_done()
