c0b4742ca6da2caab14dc75991f5c83c5f415d6d
Bhavesh Heliconia <oca@heliconia.in>
2025-02-07 15:28:54 +0530

    [IMP] delivery_dropoff_site: pre-commit auto fixes

--- delivery_dropoff_site/models/stock_rule.py
+++ delivery_dropoff_site/models/stock_rule.py
@@ -14,6 +14,6 @@ class StockRule(models.Model):
         """The purpose of this method is to be override in order to easily add
         fields from procurement 'values' argument to move data.
         """
-        ret = super(StockRule, self)._get_custom_move_fields()
+        ret = super()._get_custom_move_fields()
         ret.append("final_shipping_partner_id")
         return ret
