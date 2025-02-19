a3fa0a9ba9eafaba96667cfac5108f92330a9056
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-02-19 12:33:15 +0100

    [MIG] stock_picking_delivery_link: Migration to 18.0

--- stock_picking_delivery_link/__manifest__.py
+++ stock_picking_delivery_link/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Stock Picking Delivery Link",
     "summary": "Adds link to the delivery on all intermediate operations.",
-    "version": "16.0.1.1.4",
+    "version": "18.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/delivery-carrier",
     "category": "Warehouse Management",
-    "depends": ["stock", "delivery"],
+    "depends": ["stock_delivery"],
     "data": ["views/stock_picking.xml", "views/stock_picking_type.xml"],
     "installable": True,
     "license": "AGPL-3",
