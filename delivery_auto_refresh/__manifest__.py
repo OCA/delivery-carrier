fd98fed3bc2aafea0519240b6a5077dbad77d68b
Víctor Martínez <victor.martinez@tecnativa.com>
2025-02-25 14:42:13 +0100

    [FIX] delivery_auto_refresh: Fallback to load CoA Since odoo/odoo@d0342c8, the default existing company is not getting a CoA automatically, provoking than the current tests fail with the error:
    odoo.exceptions.UserError: No journal could be found in company My Company (San Francisco) for any of those types: sale
    
    Thus, we put tests post-install for being sure localization modules are
    installed, the same as AccountTestInvoicingCommon does, but we don't
    inherit from it, as it creates an overhead creating 2 new companies and
    loading their CoA and some more stuff, while we don't need all of that.
    
    Besides, if you don't have `l10n_generic_coa` installed, you can't use
    another CoA (like `l10n_es`) easily, so we put little code to select the
    first available CoA.
    
    [BOT] post-merge updates

--- delivery_auto_refresh/__manifest__.py
+++ delivery_auto_refresh/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Auto-refresh delivery",
     "summary": "Auto-refresh delivery price in sales orders",
-    "version": "16.0.2.0.2",
+    "version": "16.0.2.0.3",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
     "author": "Tecnativa, Odoo Community Association (OCA)",
