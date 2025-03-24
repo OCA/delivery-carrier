e8d9514a28e9c8a109af588916060d390fae459b
Denis Leemann <denis.leemann@camptocamp.com>
2025-03-24 14:41:57 +0100

    [fix] delivery_postlogistics: v18 migration scripts

--- delivery_postlogistics/migrations/18.0.1.0.0/pre-migration.py
+++ delivery_postlogistics/migrations/18.0.1.0.0/pre-migration.py
@@ -1,60 +1,38 @@
 # Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
 
 from openupgradelib import openupgrade
 
-_logger = logging.getLogger(__name__)
 
-
-MODEL_TO_RENAMED_FIELDS = {
-    "res.partner": [
-        ("postlogistics_option_ids", "delivery_carrier_template_option_ids"),
-    ],
-    "delivery.carrier.template.option": [
-        ("postlogistics_type", "type"),
-    ],
-}
-
-
-def _migrate_models(env):
-    xmlids_spec = [
-        (
-            "delivery_postlogistics.postlogistics_delivery_carrier_template_option",
-            "delivery_carrier_option.delivery_carrier_template_option",
-        )
-    ]
-    openupgrade.rename_xmlids(env.cr, xmlids_spec)
-
-
-def _rename_models(env):
-    models_spec = [
-        (
-            "postlogistics.delivery.carrier.template.option",
-            "delivery.carrier.template.option",
-        )
-    ]
-    openupgrade.rename_models(env.cr, models_spec)
-
-
-def _rename_fields(env):
-    openupgrade.rename_fields(
-        env,
-        [
-            (
-                model_name,
-                env[model_name]._table,
-                field_spec[0],
-                field_spec[1],
-            )
-            for model_name, field_specs in MODEL_TO_RENAMED_FIELDS.items()
-            for field_spec in field_specs
-        ],
+def _move_postlogistics_delivery_carrier_template_option(cr):
+    query = """
+        ALTER TABLE delivery_carrier_template_option
+        ADD COLUMN IF NOT EXISTS
+        old_postlogistics_delivery_carrier_template_option integer
+    """
+    openupgrade.logged_query(cr, query)
+    # Insert data from old model to new
+    query = """
+        INSERT INTO delivery_carrier_template_option
+        (name,type,partner_id,code,old_postlogistics_delivery_carrier_template_option)
+            (SELECT name, postlogistics_type, partner_id, code,id
+            FROM postlogistics_delivery_carrier_template_option pdcto)
+            RETURNING id, old_postlogistics_delivery_carrier_template_option
+    """
+    openupgrade.logged_query(cr, query)
+    openupgrade.merge_models(
+        cr,
+        "postlogistics.delivery.carrier.template.option",
+        "delivery.carrier.template.option",
+        "old_postlogistics_delivery_carrier_template_option",
     )
+    query = """
+        UPDATE ir_model_data
+            SET model = 'delivery.carrier.template.option'
+            WHERE model = 'postlogistics.delivery.carrier.template.option'
+    """
 
 
-def migrate(env, version):
-    _rename_models(env)
-    _migrate_models(env)
-    _rename_fields(env)
+def migrate(cr, version):
+    _move_postlogistics_delivery_carrier_template_option(cr)
