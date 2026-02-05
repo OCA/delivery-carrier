3429b00de32306f715fd7ff3f666dfbe5d32d074
Nils Coenen <nils.coenen@nico-solutions.de>
2026-02-05 13:09:32 +0100

    [MIG] partner_delivery_schedule: Migration to 19.0

--- partner_delivery_schedule/__manifest__.py
+++ partner_delivery_schedule/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Partner Delivery Schedule",
     "summary": "Set on partners a schedule for delivery goods",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Delivery",
     "website": "https://github.com/OCA/delivery-carrier",
