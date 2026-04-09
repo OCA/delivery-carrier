644410b6fbdbdb3bea9a06ade3ae6f2a79430eac
fanha99@hotmail.com <fanha99@hotmail.com>
2026-04-09 13:56:11 +0700

    [MIG] delivery_free_fee_removal: Migration to 19.0

--- delivery_free_fee_removal/tests/test_delivery_free_fee_removal.py
+++ delivery_free_fee_removal/tests/test_delivery_free_fee_removal.py
@@ -39,7 +39,7 @@ class TestDeliveryFreeFeeRemoval(TransactionCase):
                         {
                             "product_id": product.id,
                             "product_uom_qty": 1,
-                            "product_uom": product.uom_id.id,
+                            "product_uom_id": product.uom_id.id,
                             "price_unit": 3.0,
                         },
                     )
