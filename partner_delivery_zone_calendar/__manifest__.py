632039b993d3557bf49613c7886f35f7c9c941ef
hda <hughes.damry@acsone.eu>
2026-05-19 17:47:14 +0200

    [MIG] partner_delivery_zone_calendar: Migration to 19.0

--- partner_delivery_zone_calendar/__manifest__.py
+++ partner_delivery_zone_calendar/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Partner Delivery Zone Calendar",
     "summary": """
         This module allows to define a calendar on a delivery zone""",
-    "version": "14.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/delivery-carrier",
