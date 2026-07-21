dcd711e2f922c16d09e33b72ed2e7d3c880553c8
oca-ci <oca-ci@odoo-community.org>
2026-07-21 15:59:46 +0000

    [UPD] Update delivery_carrier_max_weight_constraint.pot

--- delivery_carrier_max_weight_constraint/__manifest__.py
+++ delivery_carrier_max_weight_constraint/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Constrain package maximum weight",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "category": "Delivery",
     "depends": ["stock_delivery"],
