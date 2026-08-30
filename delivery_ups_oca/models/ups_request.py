Nitrokey/odoo-delivery-carrier

[IMP] delivery_ups_oca: add paperless invoice
Automatically upload paperless invoice and other documents to UPS for
international shipments.

delivery_ups_oca/models/ups_request.py
@@ -3,7 +3,9 @@
 # Copyright 2024 Sygel - Manuel Regidor
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import ast
 import datetime
+import json
 import logging
 from urllib.parse import urlencode
 
@@ -296,9 +298,33 @@ class UpsRequest:
                     }
                 },
             )
+        # Add paperless invoice if a document id has been retrieved
+        if picking.ups_document_id and picking.ups_document_id.strip():
+            try:
+                document_id = (
+                    ast.literal_eval(picking.ups_document_id)
+                    if len(picking.ups_document_id) > 30
+                    else picking.ups_document_id
+                )
+            except (ValueError, SyntaxError):
+                document_id = picking.ups_document_id
+            shipment = vals["ShipmentRequest"]["Shipment"]
+            shipment.setdefault("ShipmentServiceOptions", {})["InternationalForms"] = {
+                "FormType": "07",
+                "UserCreatedForm": {"DocumentID": document_id},
+            }
         return vals
 
     def _send_shipping(self, picking):
+        # Send the paperless invoice first so its document ID is included
+        # in the shipment request.
+        if picking.ups_paperless_auto_send and not picking.ups_document_id:
+            try:
+                self.carrier.ups_paperless_invoice_provider(picking)
+            except Exception as e:
+                error_msg = _("Failed to send paperless invoice: %s") % str(e)
+                _logger.error(error_msg)
+                raise UserError(error_msg) from e
         status = self._process_reply(
             url=f"{self.url}/api/shipments/v1/ship",
             json=self._prepare_create_shipping(picking),
@@ -497,3 +523,62 @@ class UpsRequest:
             "tracking_state": tracking_state,
             "pod": pod,
         }
+
+    def send_paperless_invoice(self, picking, paperless_document_data):
+        """Send paperless invoice documents to UPS"""
+        if not paperless_document_data:
+            raise UserError(picking.env._("No documents to send!"))
+
+        request_data = {
+            "UploadRequest": {
+                "Request": {"TransactionReference": {"CustomerContext": ""}},
+                "ShipperNumber": self.shipper_number,
+                "UserCreatedForm": paperless_document_data,
+            }
+        }
+        headers = {
+            "ShipperNumber": self.shipper_number,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer {self.token}",
+        }
+        url = f"{self.url}/api/paperlessdocuments/v1/upload"
+        # Mask the document files before logging the request
+        debug_request = json.loads(json.dumps(request_data))
+        for doc in debug_request.get("UploadRequest", {}).get("UserCreatedForm", []):
+            if "UserCreatedFormFile" in doc:
+                doc["UserCreatedFormFile"] = "***MASKED***"
+        _logger.debug(
+            "UPS Paperless Invoice Request: URL=%s, Headers=%s, Data=%s",
+            url,
+            headers,
+            debug_request,
+        )
+        try:
+            response = self._send_request(
+                url,
+                data=json.dumps(request_data),
+                headers=headers,
+                timeout=10,
+            )
+            _logger.debug(
+                "UPS Paperless Invoice Response: Status=%s, Content=%s",
+                response.status_code,
+                response.text,
+            )
+            if response.status_code in [200, 201]:
+                invoice_response = response.json()
+                upload_response = invoice_response.get("UploadResponse") or {}
+                forms_history = upload_response.get("FormsHistoryDocumentID") or {}
+                document_id = forms_history.get("DocumentID")
+                picking.ups_document_id = document_id
+                return document_id
+            errors = (json.loads(response.text).get("response") or {}).get("errors")
+            error_message = errors[0].get("message") if errors else response.text
+            raise UserError(
+                picking.env._("Paperless Invoice: %s") % error_message
+            ) from None
+        except UserError:
+            raise
+        except Exception as e:
+            raise UserError(str(e)) from e

delivery_ups_oca/tests/test_delivery_ups.py
@@ -2,6 +2,7 @@
 
 import base64
 import io
+import json
 from datetime import datetime, timedelta
 from unittest import mock
 
@@ -1000,3 +1001,227 @@ class TestResidentialAddress(TestDeliveryUpsBase):
             shipping_data["Address"],
             "Commercial address should not contain ResidentialAddressIndicator",
         )
+
+
+class TestSendPaperlessInvoice(TestDeliveryUpsBase):
+    def setUp(self):
+        super().setUp()
+        self.picking = self.sale.picking_ids[0]
+        self.picking.move_ids.quantity = 10
+        self.picking.action_assign()
+        # Create a dummy invoice PDF
+        self.dummy_pdf = base64.b64encode(b"%PDF-1.4\n%Fake PDF Content\n%%EOF")
+        self.invoice = self.sale._create_invoices()
+        self.invoice.action_post()
+
+    def test_prepare_paperless_invoice_provider_adds_missing_docs(self):
+        result = self.carrier.prepare_paperless_invoice_provider(self.picking)
+        doc_types = [doc["UserCreatedFormDocumentType"] for doc in result]
+        self.assertIn("002", doc_types, "Invoice should be added if missing")
+        self.assertIn("010", doc_types, "Packing list should be added if missing")
+
+    def test_ups_paperless_invoice_raises_if_document_id_exists(self):
+        """Should raise UserError when a document ID already exists"""
+        self.picking.ups_document_id = "DUMMY_ID"
+        with self.assertRaises(UserError):
+            self.carrier.ups_paperless_invoice_provider(self.picking)
+
+    def test_prepare_paperless_invoice_raises_if_invoice_missing(self):
+        self.picking.sale_id.invoice_ids = False
+        with self.assertRaises(UserError):
+            self.carrier.ups_paperless_invoice_provider(self.picking)
+
+    def test_send_paperless_invoice_data(self):
+        self.picking.ups_paperless_auto_send = True
+        self.picking.ups_paperless_document = [
+            (
+                0,
+                0,
+                {
+                    "file_name": "Paperless Invoice - 001",
+                    "ups_document_type": "003",
+                    "ups_paperless_file": self.dummy_pdf,
+                },
+            ),
+            (
+                0,
+                0,
+                {
+                    "file_name": "Paperless Invoice - 002",
+                    "ups_document_type": "013",
+                    "ups_paperless_file": self.dummy_pdf,
+                },
+            ),
+        ]
+        with mock.patch(
+            _provider_class + ".send_paperless_invoice", return_value="DOC123456789"
+        ):
+            result = self.carrier.ups_paperless_invoice_provider(self.picking)
+            self.assertIsNotNone(result)
+
+    def test_send_paperless_invoice_success(self):
+        """A successful upload stores the returned document ID on the picking."""
+        ups_request = UpsRequest(self.carrier)
+        documents = [
+            {
+                "UserCreatedFormFileName": "commercial_invoice.pdf",
+                "UserCreatedFormFileFormat": "pdf",
+                "UserCreatedFormDocumentType": "002",
+                "UserCreatedFormFile": self.dummy_pdf.decode("utf-8"),
+            }
+        ]
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "UploadResponse": {"FormsHistoryDocumentID": {"DocumentID": "DOC123"}}
+        }
+        with mock.patch.object(
+            ups_request, "_send_request", return_value=mock_response
+        ):
+            result = ups_request.send_paperless_invoice(self.picking, documents)
+        self.assertEqual(result, "DOC123")
+        self.assertEqual(self.picking.ups_document_id, "DOC123")
+
+    def test_send_paperless_invoice_api_error(self):
+        """An error response is raised as a UserError with the UPS message."""
+        ups_request = UpsRequest(self.carrier)
+        documents = [
+            {
+                "UserCreatedFormFileName": "commercial_invoice.pdf",
+                "UserCreatedFormFileFormat": "pdf",
+                "UserCreatedFormDocumentType": "002",
+                "UserCreatedFormFile": self.dummy_pdf.decode("utf-8"),
+            }
+        ]
+        mock_response = mock.Mock()
+        mock_response.status_code = 400
+        mock_response.text = json.dumps(
+            {"response": {"errors": [{"message": "Invalid shipper number"}]}}
+        )
+        with mock.patch.object(
+            ups_request, "_send_request", return_value=mock_response
+        ):
+            with self.assertRaises(UserError) as cm:
+                ups_request.send_paperless_invoice(self.picking, documents)
+        self.assertIn("Invalid shipper number", str(cm.exception))
+
+    def test_prepare_create_shipping_adds_international_forms(self):
+        """A retrieved document ID is added as InternationalForms to the request."""
+        self.picking.ups_document_id = "DOC123"
+        self.picking.shipping_weight = 10.0
+        self.picking.number_of_packages = 1
+        ups_request = UpsRequest(self.carrier)
+        vals = ups_request._prepare_create_shipping(self.picking)
+        service_options = vals["ShipmentRequest"]["Shipment"]["ShipmentServiceOptions"]
+        self.assertEqual(
+            service_options["InternationalForms"]["UserCreatedForm"]["DocumentID"],
+            "DOC123",
+        )
+
+    def test_send_shipping_triggers_paperless_invoice(self):
+        """_send_shipping sends the paperless invoice before shipping."""
+        self.picking.ups_paperless_auto_send = True
+        self.picking.shipping_weight = 10.0
+        self.picking.number_of_packages = 1
+        ups_request = UpsRequest(self.carrier)
+        shipment_response = {
+            "ShipmentResponse": {
+                "ShipmentResults": {
+                    "ShipmentCharges": {
+                        "TotalCharges": {
+                            "CurrencyCode": "USD",
+                            "MonetaryValue": "10.0",
+                        }
+                    },
+                    "ShipmentIdentificationNumber": "123456",
+                    "PackageResults": {
+                        "TrackingNumber": "123456",
+                        "ShippingLabel": {
+                            "ImageFormat": {"Code": "GIF"},
+                            "GraphicImage": base64.b64encode(self.label),
+                        },
+                    },
+                }
+            }
+        }
+        with mock.patch.object(
+            type(self.carrier), "ups_paperless_invoice_provider"
+        ) as mock_provider:
+            with mock.patch.object(
+                ups_request, "_process_reply", return_value=shipment_response
+            ):
+                with mock.patch.object(ups_request, "_raise_for_status"):
+                    ups_request._send_shipping(self.picking)
+        mock_provider.assert_called_once_with(self.picking)
+
+    def test_button_validate_triggers_paperless_invoice(self):
+        """Validating an auto-send picking triggers the paperless invoice."""
+        self.picking.ups_paperless_auto_send = True
+        self.picking.ups_paperless_document = [
+            (
+                0,
+                0,
+                {
+                    "file_name": "Paperless Invoice - 001",
+                    "ups_document_type": "003",
+                    "ups_paperless_file": self.dummy_pdf,
+                },
+            )
+        ]
+        with mock.patch(
+            _provider_class + "._send_shipping",
+            return_value={
+                "price": {"CurrencyCode": "USD", "MonetaryValue": "10.0"},
+                "ShipmentIdentificationNumber": "123456",
+                "labels": [
+                    {
+                        "tracking_ref": "123456",
+                        "format_code": "GIF",
+                        "datas": base64.b64encode(self.label),
+                    }
+                ],
+            },
+        ):
+            with mock.patch.object(
+                type(self.carrier), "ups_paperless_invoice_provider"
+            ) as mock_provider:
+                self.picking.button_validate()
+        mock_provider.assert_called_once_with(self.picking)
+
+    def test_ups_paperless_file_download(self):
+        """The download action returns a URL pointing to the stored file."""
+        document = self.env["ups.paperless.document"].create(
+            {
+                "file_name": "invoice.pdf",
+                "ups_document_type": "002",
+                "ups_paperless_file": self.dummy_pdf,
+                "ups_stock_picking_id": self.picking.id,
+            }
+        )
+        action = document.ups_paperless_file_download()
+        self.assertEqual(action["type"], "ir.actions.act_url")
+        self.assertIn(str(document.id), action["url"])
+        self.assertIn("invoice.pdf", action["url"])
+
+    def test_onchange_sets_auto_send(self):
+        """Auto-send is toggled based on the carrier's country groups."""
+        country_group = self.env["res.country.group"].create(
+            {
+                "name": "UPS Paperless Test Group",
+                "country_ids": [(6, 0, [self.picking.partner_id.country_id.id])],
+            }
+        )
+        # Country in the carrier's groups -> auto-send enabled
+        self.carrier.country_groups = [(6, 0, [country_group.id])]
+        self.picking._onchange_ups_paperless_auto_send()
+        self.assertTrue(self.picking.ups_paperless_auto_send)
+        # No matching country group -> auto-send disabled
+        self.carrier.country_groups = [(5, 0, 0)]
+        self.picking._onchange_ups_paperless_auto_send()
+        self.assertFalse(self.picking.ups_paperless_auto_send)
+
+    def test_send_paperless_invoice_no_documents(self):
+        """Sending without any document raises a UserError."""
+        ups_request = UpsRequest(self.carrier)
+        with self.assertRaises(UserError):
+            ups_request.send_paperless_invoice(self.picking, [])

OCA/delivery-carrier

delivery_ups_oca/models/ups_request.py
@@ -3,7 +3,9 @@
 # Copyright 2024 Sygel - Manuel Regidor
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import ast
 import datetime
+import json
 import logging
 from urllib.parse import urlencode
 
@@ -311,9 +313,33 @@ class UpsRequest:
                     }
                 },
             )
+        # Add paperless invoice if a document id has been retrieved
+        if picking.ups_document_identifier and picking.ups_document_identifier.strip():
+            try:
+                document_id = (
+                    ast.literal_eval(picking.ups_document_identifier)
+                    if len(picking.ups_document_identifier) > 30
+                    else picking.ups_document_identifier
+                )
+            except (ValueError, SyntaxError):
+                document_id = picking.ups_document_identifier
+            shipment = vals["ShipmentRequest"]["Shipment"]
+            shipment.setdefault("ShipmentServiceOptions", {})["InternationalForms"] = {
+                "FormType": "07",
+                "UserCreatedForm": {"DocumentID": document_id},
+            }
         return vals
 
     def _send_shipping(self, picking):
+        # Send the paperless invoice first so its document ID is included
+        # in the shipment request.
+        if picking.ups_paperless_auto_send and not picking.ups_document_identifier:
+            try:
+                self.carrier.send_ups_paperless_invoice(picking)
+            except Exception as e:
+                error_msg = _("Failed to send paperless invoice: %s") % str(e)
+                _logger.error(error_msg)
+                raise UserError(error_msg) from e
         status = self._process_reply(
             url=f"{self.url}/api/shipments/v1/ship",
             json=self._prepare_create_shipping(picking),
@@ -524,3 +550,62 @@ class UpsRequest:
             "tracking_state": tracking_state,
             "pod": pod,
         }
+
+    def send_paperless_invoice(self, picking, paperless_document_data):
+        """Send paperless invoice documents to UPS"""
+        if not paperless_document_data:
+            raise UserError(picking.env._("No documents to send!"))
+
+        request_data = {
+            "UploadRequest": {
+                "Request": {"TransactionReference": {"CustomerContext": ""}},
+                "ShipperNumber": self.shipper_number,
+                "UserCreatedForm": paperless_document_data,
+            }
+        }
+        headers = {
+            "ShipperNumber": self.shipper_number,
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": f"Bearer {self.token}",
+        }
+        url = f"{self.url}/api/paperlessdocuments/v1/upload"
+        # Mask the document files before logging the request
+        debug_request = json.loads(json.dumps(request_data))
+        for doc in debug_request.get("UploadRequest", {}).get("UserCreatedForm", []):
+            if "UserCreatedFormFile" in doc:
+                doc["UserCreatedFormFile"] = "***MASKED***"
+        _logger.debug(
+            "UPS Paperless Invoice Request: URL=%s, Headers=%s, Data=%s",
+            url,
+            headers,
+            debug_request,
+        )
+        try:
+            response = self._send_request(
+                url,
+                data=json.dumps(request_data),
+                headers=headers,
+                timeout=10,
+            )
+            _logger.debug(
+                "UPS Paperless Invoice Response: Status=%s, Content=%s",
+                response.status_code,
+                response.text,
+            )
+            if response.status_code in [200, 201]:
+                invoice_response = response.json()
+                upload_response = invoice_response.get("UploadResponse") or {}
+                forms_history = upload_response.get("FormsHistoryDocumentID") or {}
+                document_id = forms_history.get("DocumentID")
+                picking.ups_document_identifier = document_id
+                return document_id
+            errors = (json.loads(response.text).get("response") or {}).get("errors")
+            error_message = errors[0].get("message") if errors else response.text
+            raise UserError(
+                picking.env._("Paperless Invoice: %s") % error_message
+            ) from None
+        except UserError:
+            raise
+        except Exception as e:
+            raise UserError(str(e)) from e

delivery_ups_oca/tests/test_delivery_ups.py
@@ -2,6 +2,7 @@
 
 import base64
 import io
+import json
 from datetime import datetime, timedelta
 from unittest import mock
 
@@ -1225,3 +1226,227 @@ class TestUpsNegotiatedRates(TestDeliveryUpsBase):
                 ],
             )
             self.assertEqual(self.picking.carrier_tracking_ref, "1ZXXXXXXXXXXXXXXXX")
+
+
+class TestSendPaperlessInvoice(TestDeliveryUpsBase):
+    def setUp(self):
+        super().setUp()
+        self.picking = self.sale.picking_ids[0]
+        self.picking.move_ids.quantity = 10
+        self.picking.action_assign()
+        # Create a dummy invoice PDF
+        self.dummy_pdf = base64.b64encode(b"%PDF-1.4\n%Fake PDF Content\n%%EOF")
+        self.invoice = self.sale._create_invoices()
+        self.invoice.action_post()
+
+    def test_prepare_ups_paperless_invoice_adds_missing_docs(self):
+        result = self.carrier.prepare_ups_paperless_invoice(self.picking)
+        doc_types = [doc["UserCreatedFormDocumentType"] for doc in result]
+        self.assertIn("002", doc_types, "Invoice should be added if missing")
+        self.assertIn("010", doc_types, "Packing list should be added if missing")
+
+    def test_ups_paperless_invoice_raises_if_document_id_exists(self):
+        """Should raise UserError when a document ID already exists"""
+        self.picking.ups_document_identifier = "DUMMY_ID"
+        with self.assertRaises(UserError):
+            self.carrier.send_ups_paperless_invoice(self.picking)
+
+    def test_prepare_paperless_invoice_raises_if_invoice_missing(self):
+        self.picking.sale_id.invoice_ids = False
+        with self.assertRaises(UserError):
+            self.carrier.send_ups_paperless_invoice(self.picking)
+
+    def test_send_paperless_invoice_data(self):
+        self.picking.ups_paperless_auto_send = True
+        self.picking.ups_paperless_document_ids = [
+            (
+                0,
+                0,
+                {
+                    "file_name": "Paperless Invoice - 001",
+                    "ups_document_type": "003",
+                    "ups_paperless_file": self.dummy_pdf,
+                },
+            ),
+            (
+                0,
+                0,
+                {
+                    "file_name": "Paperless Invoice - 002",
+                    "ups_document_type": "013",
+                    "ups_paperless_file": self.dummy_pdf,
+                },
+            ),
+        ]
+        with mock.patch(
+            _provider_class + ".send_paperless_invoice", return_value="DOC123456789"
+        ):
+            result = self.carrier.send_ups_paperless_invoice(self.picking)
+            self.assertIsNotNone(result)
+
+    def test_send_paperless_invoice_success(self):
+        """A successful upload stores the returned document ID on the picking."""
+        ups_request = UpsRequest(self.carrier)
+        documents = [
+            {
+                "UserCreatedFormFileName": "commercial_invoice.pdf",
+                "UserCreatedFormFileFormat": "pdf",
+                "UserCreatedFormDocumentType": "002",
+                "UserCreatedFormFile": self.dummy_pdf.decode("utf-8"),
+            }
+        ]
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.json.return_value = {
+            "UploadResponse": {"FormsHistoryDocumentID": {"DocumentID": "DOC123"}}
+        }
+        with mock.patch.object(
+            ups_request, "_send_request", return_value=mock_response
+        ):
+            result = ups_request.send_paperless_invoice(self.picking, documents)
+        self.assertEqual(result, "DOC123")
+        self.assertEqual(self.picking.ups_document_identifier, "DOC123")
+
+    def test_send_paperless_invoice_api_error(self):
+        """An error response is raised as a UserError with the UPS message."""
+        ups_request = UpsRequest(self.carrier)
+        documents = [
+            {
+                "UserCreatedFormFileName": "commercial_invoice.pdf",
+                "UserCreatedFormFileFormat": "pdf",
+                "UserCreatedFormDocumentType": "002",
+                "UserCreatedFormFile": self.dummy_pdf.decode("utf-8"),
+            }
+        ]
+        mock_response = mock.Mock()
+        mock_response.status_code = 400
+        mock_response.text = json.dumps(
+            {"response": {"errors": [{"message": "Invalid shipper number"}]}}
+        )
+        with mock.patch.object(
+            ups_request, "_send_request", return_value=mock_response
+        ):
+            with self.assertRaises(UserError) as cm:
+                ups_request.send_paperless_invoice(self.picking, documents)
+        self.assertIn("Invalid shipper number", str(cm.exception))
+
+    def test_prepare_create_shipping_adds_international_forms(self):
+        """A retrieved document ID is added as InternationalForms to the request."""
+        self.picking.ups_document_identifier = "DOC123"
+        self.picking.shipping_weight = 10.0
+        self.picking.number_of_packages = 1
+        ups_request = UpsRequest(self.carrier)
+        vals = ups_request._prepare_create_shipping(self.picking)
+        service_options = vals["ShipmentRequest"]["Shipment"]["ShipmentServiceOptions"]
+        self.assertEqual(
+            service_options["InternationalForms"]["UserCreatedForm"]["DocumentID"],
+            "DOC123",
+        )
+
+    def test_send_shipping_triggers_paperless_invoice(self):
+        """_send_shipping sends the paperless invoice before shipping."""
+        self.picking.ups_paperless_auto_send = True
+        self.picking.shipping_weight = 10.0
+        self.picking.number_of_packages = 1
+        ups_request = UpsRequest(self.carrier)
+        shipment_response = {
+            "ShipmentResponse": {
+                "ShipmentResults": {
+                    "ShipmentCharges": {
+                        "TotalCharges": {
+                            "CurrencyCode": "USD",
+                            "MonetaryValue": "10.0",
+                        }
+                    },
+                    "ShipmentIdentificationNumber": "123456",
+                    "PackageResults": {
+                        "TrackingNumber": "123456",
+                        "ShippingLabel": {
+                            "ImageFormat": {"Code": "GIF"},
+                            "GraphicImage": base64.b64encode(self.label),
+                        },
+                    },
+                }
+            }
+        }
+        with mock.patch.object(
+            type(self.carrier), "send_ups_paperless_invoice"
+        ) as mock_provider:
+            with mock.patch.object(
+                ups_request, "_process_reply", return_value=shipment_response
+            ):
+                with mock.patch.object(ups_request, "_raise_for_status"):
+                    ups_request._send_shipping(self.picking)
+        mock_provider.assert_called_once_with(self.picking)
+
+    def test_button_validate_triggers_paperless_invoice(self):
+        """Validating an auto-send picking triggers the paperless invoice."""
+        self.picking.ups_paperless_auto_send = True
+        self.picking.ups_paperless_document_ids = [
+            (
+                0,
+                0,
+                {
+                    "file_name": "Paperless Invoice - 001",
+                    "ups_document_type": "003",
+                    "ups_paperless_file": self.dummy_pdf,
+                },
+            )
+        ]
+        with mock.patch(
+            _provider_class + "._send_shipping",
+            return_value={
+                "price": {"CurrencyCode": "USD", "MonetaryValue": "10.0"},
+                "ShipmentIdentificationNumber": "123456",
+                "labels": [
+                    {
+                        "tracking_ref": "123456",
+                        "format_code": "GIF",
+                        "datas": base64.b64encode(self.label),
+                    }
+                ],
+            },
+        ):
+            with mock.patch.object(
+                type(self.carrier), "send_ups_paperless_invoice"
+            ) as mock_provider:
+                self.picking.button_validate()
+        mock_provider.assert_called_once_with(self.picking)
+
+    def test_download_ups_paperless_file(self):
+        """The download action returns a URL pointing to the stored file."""
+        document = self.env["ups.paperless.document"].create(
+            {
+                "file_name": "invoice.pdf",
+                "ups_document_type": "002",
+                "ups_paperless_file": self.dummy_pdf,
+                "ups_stock_picking_id": self.picking.id,
+            }
+        )
+        action = document.download_ups_paperless_file()
+        self.assertEqual(action["type"], "ir.actions.act_url")
+        self.assertIn(str(document.id), action["url"])
+        self.assertIn("invoice.pdf", action["url"])
+
+    def test_onchange_sets_auto_send(self):
+        """Auto-send is toggled based on the carrier's country groups."""
+        country_group = self.env["res.country.group"].create(
+            {
+                "name": "UPS Paperless Test Group",
+                "country_ids": [(6, 0, [self.picking.partner_id.country_id.id])],
+            }
+        )
+        # Country in the carrier's groups -> auto-send enabled
+        self.carrier.ups_paperless_country_group_ids = [(6, 0, [country_group.id])]
+        self.picking._onchange_ups_paperless_auto_send()
+        self.assertTrue(self.picking.ups_paperless_auto_send)
+        # No matching country group -> auto-send disabled
+        self.carrier.ups_paperless_country_group_ids = [(5, 0, 0)]
+        self.picking._onchange_ups_paperless_auto_send()
+        self.assertFalse(self.picking.ups_paperless_auto_send)
+
+    def test_send_paperless_invoice_no_documents(self):
+        """Sending without any document raises a UserError."""
+        ups_request = UpsRequest(self.carrier)
+        with self.assertRaises(UserError):
+            ups_request.send_paperless_invoice(self.picking, [])