80bae3ad099b1964d4eeaa620809f8896d5320ec
Florian Mounier <paradoxxx.zero@gmail.com>
2026-09-21 16:34:20 +0200

    [ADD] delivery_discounted_free_over

---
+++ delivery_discounted_free_over/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Akretion (http://www.akretion.com).
+# @author Florian Mounier <florian.mounier@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Delivery Discounted Free Over",
+    "version": "18.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "summary": "Set free over shipping as 100% shipment discount",
+    "category": "Sales/Delivery",
+    "depends": ["delivery"],
+    "website": "https://github.com/OCA/delivery-carrier",
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+    "maintainers": ["paradoxxxzero"],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ delivery_discounted_free_over/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+Consider free over shipping as a discount from the carrier price.
